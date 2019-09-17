7f5eb198d576ada8701b092a893fddfa3478b1bc
Kevin Le <kevin_le@sfu.ca>
2019-09-16 21:47:30 -0700

    add User model

---
+++ server/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+# To add a table to db, make a new class
+# create a migration: flask db migrate
+# apply the migration: flask db upgrade
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
