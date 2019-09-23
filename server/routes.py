d2ff3e1d21cef986713066c7de6b28cc415103e9
Jeffrey Leung <jleung513@gmail.com>
2019-09-23 11:26:33 -0700

    [#10] Add forgotten route change
    Whoops.

--- server/routes.py
+++ server/routes.py
@@ -11,4 +11,4 @@ from Api_endpoints.PatientApi import *
 def init(api):
     api.add_resource(HelloWorld, '/', '/home', '/api/hello-world')
     api.add_resource(Multi, '/multi/<int:num>')
-    api.add_resource(Patient, '/patient', '/patient/<string:id>')
+    api.add_resource(Patient, '/patient', '/patient/<string:patient_id>')
