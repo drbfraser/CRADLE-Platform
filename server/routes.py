226ad0ece94de74e3ec73d8af1885220dc8065de
kev-le <kevin_le@sfu.ca>
2019-09-23 12:24:32 -0700

    fix route merge again

--- server/routes.py
+++ server/routes.py
@@ -11,7 +11,6 @@ from Api_endpoints.ReferralsController import ReferralApi, ReferralInfo
 def init(api):
     api.add_resource(HelloWorld, '/', '/home', '/api/hello-world')
     api.add_resource(Multi, '/multi/<int:num>')
-<<<<<<< HEAD
 
     api.add_resource(PatientApi, '/patient') # [GET, POST]
     api.add_resource(PatientInfo, '/patient/<int:id>') # [GET]
@@ -19,6 +18,3 @@ def init(api):
     api.add_resource(ReferralApi, '/referral') # [GET, POST]
     api.add_resource(ReferralInfo, '/referral/<int:id>') # [GET]
 
-=======
-    api.add_resource(Patient, '/patient', '/patient/<string:patient_id>')
->>>>>>> 005ecc403f8c40c396a6b8b9c4943d3fab6c91b3
