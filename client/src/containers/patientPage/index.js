8b4d4ae5792e89433c46f595ce320938324e3c53
Kevin Le <kevin_le@sfu.ca>
2019-09-24 01:03:40 -0700

    add patients page (only logged in users can see)

---
+++ client/src/containers/patientPage/index.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import { push } from 'connected-react-router'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getCurrentUser } from '../../actions/users';
+
+class PatientPage extends Component {
+  state = {
+    email: "",
+  }
+
+  componentDidMount = () => {
+    this.props.getCurrentUser()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Patients Page</h1>
+        <p> only logged in users are allowed to see this </p>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ user }) => ({
+  user : user.currentUser,
+})
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getCurrentUser
+    },
+    dispatch
+  )
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PatientPage)
