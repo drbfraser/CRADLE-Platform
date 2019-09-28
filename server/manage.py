ed4da650d09b1a5cb7b63799423846e102d0865a
Kevin Le <kevin_le@sfu.ca>
2019-09-27 20:19:23 -0700

    Fix user seeding

--- server/manage.py
+++ server/manage.py
@@ -2,7 +2,7 @@
 # this seeds the database with seed command below
 
 from flask_script import Manager
-from config import app, db
+from config import app, db, flask_bcrypt
 from models import *
 
 manager = Manager(app)
@@ -16,15 +16,21 @@ def seed():
 
     print('Seeding roles...')
     db.session.add_all([role1,role2,role3])
+    db.session.commit()
 
     # NOTE: users are automatically added to 'HCW' role
-    u1 = User(email="a@a.com", password="123456")
-    u2 = User(email="b@b.com", password="123456")
+    role_hcw = Role.query.filter_by(name='HCW').first()
+
+    user_schema = UserSchema()
+    u1 = { 'email' : 'a@a.com', 'password': flask_bcrypt.generate_password_hash('123456') }
+    u2 = { 'email' : 'b@b.com', 'password': flask_bcrypt.generate_password_hash('123456') }
+    role_hcw.users.append(user_schema.load(u1, session=db.session))
+    role_hcw.users.append(user_schema.load(u2, session=db.session))
 
     print('Seeding users...')
-    db.session.add_all([u1,u2])
-
+    db.session.add(role_hcw)
     db.session.commit()
+
     print('Complete!')
 
 if __name__ == "__main__":
