8b5156d5613acf29786dac847608146fe51dbd89
Kevin Le <kevin_le@sfu.ca>
2019-09-28 20:09:38 -0700

    Add view for single patient using modals

--- client/src/containers/patientPage/index.js
+++ client/src/containers/patientPage/index.js
@@ -7,23 +7,45 @@ import { getPatients } from '../../actions/patients';
 import { getCurrentUser } from '../../actions/users';
 import { getPosts } from '../../actions/posts';
 
+import { Button,
+  Header, Image, Modal,
+  Divider, Form, Select,
+  Input, TextArea
+} from 'semantic-ui-react'
+
+const sexOptions = [
+  { key: 'm', text: 'Male', value: 'M' },
+  { key: 'f', text: 'Female', value: 'F' },
+  { key: 'o', text: 'Other', value: 'I' },
+]
+
+const pregOptions = [
+  { key: 'y', text: 'Yes', value: true },
+  { key: 'n', text: 'No', value: false },
+]
 
 class PatientPage extends Component {
   state = {
     columns: [
       { title: 'Patient ID', field: 'patientId', defaultSort: 'asc' },
-      { title: 'Patient Name', field: 'patientName' },
-      { title: 'Patient Sex', field: 'patientSex' },
-      // { title: 'Last Reading', render: rowData => <p>Oct 01, 2019 10:59 AM</p> },
-      { title: 'Readings', render: rowData => <button>View Readings</button> },
-      { title: 'Pregant', field: 'isPregnant', type: 'boolean' },
-      // { title: 'Referred', field: 'isReferred', type: 'boolean' },
+      { title: 'Patient Name', field: 'patientName',
+        render: rowData => 
+          <div>
+            <p>{rowData.patientName}</p>
+            <a onClick={() => this.openModal({rowData})}>View Info</a>
+          </div>  
+      },
+      { title: 'Readings', render: rowData => <a onClick={() => console.log(`clicked ${rowData}`)}>View Readings</a>},
       { title: 'Follow Up Status', field: 'followUp',
         render: rowData => rowData.isPregnant ? (<button>Follow Up</button>) : (<p>Not needed</p>) },
     ],
     data: [],
+    selectedPatient: { patientId: '', patientName: 'Test', 
+                       patientSex: 'F', medicalHistory: '',
+                       drugHistory: '', villageNumber:''
+                      },
+    displayModal: false
   }
-
 
   componentDidMount = () => {
     console.log('componentDidMount')
@@ -34,6 +56,37 @@ class PatientPage extends Component {
       }
       this.props.getPatients()
     })
+  }
+
+  handleSelectChange = (e, value) => {
+    console.log(value)
+    this.setState({'selectedPatient': { ...this.state.selectedPatient, [value.name] : value.value } })
+  }
+
+  openModal = (patientData) => {
+    console.log(patientData)
+    console.log('open modal')
+    this.setState({ displayModal: true, selectedPatient: patientData['rowData'] })
+  }
+
+  closeModal = () => {
+    console.log('close modal')
+    this.setState({ displayModal: false })
+  }
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let patientData = this.state.selectedPatient
+
+    // delete any unnecessary fields
+    delete patientData.readings
+    delete patientData.tableData
+    delete patientData.patientId
+
+    let patientJSON = JSON.stringify({ 'patient' : patientData });
+    console.log(patientJSON)
+
+    // TODO: make request to update patient record
   }
 
   render() {
@@ -80,6 +133,81 @@ class PatientPage extends Component {
                 }),
             }}
           />
+
+          <Modal closeIcon onClose={this.closeModal} open={this.state.displayModal}>
+            <Modal.Content scrolling>
+              <Modal.Description>
+                <Header>Patient #{this.state.selectedPatient.patientId}</Header>
+                <Divider />
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group widths='equal'>
+                    <Form.Field
+                      name="patientName"
+                      value={this.state.selectedPatient.patientName}
+                      control={Input}
+                      label='Name'
+                      placeholder='Patient Name'
+                      onChange={this.handleSelectChange}
+                    />
+                    <Form.Field
+                      name="patientAge"
+                      value={this.state.selectedPatient.patientAge}
+                      control={Input}
+                      label='Age'
+                      placeholder='Patient age'
+                      onChange={this.handleSelectChange}
+                    />
+                    <Form.Field
+                      name="patientSex"
+                      control={Select}
+                      value={this.state.selectedPatient.patientSex}
+                      label='Gender'
+                      options={sexOptions}
+                      placeholder='Gender'
+                      onChange={this.handleSelectChange}
+                    />
+                  </Form.Group>
+                  <Form.Group widths='equal'>
+                    <Form.Field
+                      name='villageNumber'
+                      value={this.state.selectedPatient.villageNumber}
+                      control={Input}
+                      label='Village Number'
+                      placeholder='Village Number'
+                      onChange={this.handleSelectChange}
+                    />
+                    <Form.Field
+                      name='isPregnant'
+                      value={this.state.selectedPatient.isPregnant}
+                      control={Select}
+                      label='Pregant'
+                      options={pregOptions}
+                      onChange={this.handleSelectChange}
+                    />
+                  </Form.Group>
+                  <Form.Field
+                    name="drugHistory"
+                    value={this.state.selectedPatient.drugHistory || ''}
+                    control={TextArea}
+                    label='Drug History'
+                    placeholder="Patient's drug history..."
+                    onChange={this.handleSelectChange}
+                  />
+                  <Form.Field
+                    name="medicalHistory"
+                    value={this.state.selectedPatient.medicalHistory || ''}
+                    control={TextArea}
+                    label='Medical History'
+                    placeholder="Patient's medical history..."
+                    onChange={this.handleSelectChange}
+                  />
+                  <Form.Field control={Button}>Submit</Form.Field>
+                </Form>
+
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+
         </div>
 
     )
