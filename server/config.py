e909ee8612c0fc82e9a0c610db097cf73df1d5b0
vinsonl <vinsonl@sfu.ca>
2019-09-30 01:00:50 -0700

    added /api to the beginning of every rest api endpoint, refactored client to work with new enpoints, flask app now serves react app

--- server/config.py
+++ server/config.py
@@ -55,13 +55,13 @@ FLASK_DEBUG=1
 app = Flask(__name__, static_folder='../client/build')
 
 # Serve React App
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def serve(path):
-#     if path != "" and os.path.exists(app.static_folder + '/' + path):
-#         return send_from_directory(app.static_folder, path)
-#     else:
-#         return send_from_directory(app.static_folder, 'index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 CORS(app)
 api = Api(app)
