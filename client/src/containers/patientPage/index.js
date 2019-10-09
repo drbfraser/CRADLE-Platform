f7f80347c0eac10d9240ef32513ef7cd786120e3
Kevin Le <kevin_le@sfu.ca>
2019-10-09 02:31:08 -0700

    Separate patients table from patients page

--- client/src/containers/patientPage/index.js
+++ client/src/containers/patientPage/index.js
@@ -1,105 +1,41 @@
 import React, {Component} from 'react';
-import MaterialTable from 'material-table';
-import { push } from 'connected-react-router'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { getPatients } from '../../actions/patients';
-import { getCurrentUser } from '../../actions/users';
-import { getPosts } from '../../actions/posts';
+import { getPatients } from '../../actions/patients'
+import { getCurrentUser } from '../../actions/users'
+import PatientTable from './patientTable'
+import PatientSummary from './patientSummary'
 
-import { Button,
-  Header, Image, Modal,
-  Divider, Form, Select,
-  Input, TextArea
-} from 'semantic-ui-react'
-
-const sexOptions = [
-  { key: 'm', text: 'Male', value: 'M' },
-  { key: 'f', text: 'Female', value: 'F' },
-  { key: 'o', text: 'Other', value: 'I' },
-]
-
-const pregOptions = [
-  { key: 'y', text: 'Yes', value: true },
-  { key: 'n', text: 'No', value: false },
-]
 
 class PatientPage extends Component {
   state = {
-    columns: [
-      { title: 'Patient ID', field: 'patientId', defaultSort: 'asc' },
-      { title: 'Patient Name', field: 'patientName',
-        render: rowData => 
-          <div>
-            <p>{rowData.patientName}</p>
-            <a onClick={() => this.openPatientModal({rowData})}>View Info</a>
-          </div>  
-      },
-      { title: 'Readings', render: rowData => <a onClick={() => this.openReadingModal({rowData})}>View Reading</a> },
-      { title: 'Follow Up Status', field: 'followUp',
-        render: rowData => rowData.isPregnant ? (<button>Follow Up</button>) : (<p>Not needed</p>) },
-    ],
-    data: [],
     selectedPatient: { patientId: '', patientName: 'Test', 
                        patientSex: 'F', medicalHistory: '',
                        drugHistory: '', villageNumber:'', readings: []
                       },
-    displayPatientModal: false,
-    displayReadingModal: false
+    showSelectedPatient : false,
   }
 
   componentDidMount = () => {
-    console.log('componentDidMount')
     this.props.getCurrentUser().then((err) => {
       if (err !== undefined) {
         // error from getCurrentUser(), don't get patients
         return
       }
+      
       this.props.getPatients()
     })
   }
 
-  handleSelectChange = (e, value) => {
-    console.log(value)
-    this.setState({'selectedPatient': { ...this.state.selectedPatient, [value.name] : value.value } })
+  patientCallback = (selectedPatient) => {
+    console.log('Received callback: ')
+    this.setState({'selectedPatient': selectedPatient, 'showSelectedPatient': true })
   }
 
-  openPatientModal = (patientData) => {
-    console.log(patientData)
-    console.log('open patient modal')
-    this.setState({ displayPatientModal: true, selectedPatient: patientData['rowData'] })
+  backBtnCallback = (status) => {
+    this.setState({ 'showSelectedPatient' : false })
   }
 
-  closePatientModal = () => {
-    console.log('close patient modal')
-    this.setState({ displayPatientModal: false })
-  }
-
-  openReadingModal = (patientData) => {
-    console.log(patientData)
-    console.log('open reading modal')
-    this.setState({ displayReadingModal: true, selectedPatient: patientData['rowData'] })
-  }
-
-  closeReadingModal = () => {
-    console.log('close reading modal')
-    this.setState({ displayReadingModal: false })
-  }
-  
-  handleSubmit = (event) => {
-    event.preventDefault();
-    let patientData = this.state.selectedPatient
-
-    // delete any unnecessary fields
-    delete patientData.readings
-    delete patientData.tableData
-    delete patientData.patientId
-
-    let patientJSON = JSON.stringify({ 'patient' : patientData });
-    console.log(patientJSON)
-
-    // TODO: make request to update patient record
-  }
 
   render() {
     // don't render page if user is not logged in
@@ -107,162 +43,25 @@ class PatientPage extends Component {
       return <div />
     }
 
-    let readings = [];
-    if (this.state.selectedPatient.readings.length > 0) {
-      for (var i = 0; i < this.state.selectedPatient.readings.length; i++) {
-        readings.push(<p>Reading {i+1}</p>)
-        readings.push(<p>Date Referred: {this.state.selectedPatient.readings[i]['dateReferred']}</p>)
-        readings.push(<p>BP Diastolic: {this.state.selectedPatient.readings[i]['bpDiastolic']}</p>)
-        readings.push(<p>BP Systolic: {this.state.selectedPatient.readings[i]['bpSystolic']}</p>)
-
-        if (this.state.selectedPatient.readings[i]['comment']) { // has a referral attached!
-          readings.push(<p>Referral Status: Referred! : {this.state.selectedPatient.readings[i]['comment']}</p>)
-          readings.push(<Button>Follow Up</Button>)
-        }else {
-          readings.push(<p>Referral Status: Not Referred!</p>)
-        }
-        readings.push(<Divider />)
-      }
-    }
-
-
     return (
-        <div >
-          <MaterialTable
-            title="Patients Table"
-            columns={this.state.columns}
-            data={this.props.data}
-            options={{
-              rowStyle: {
-                height: '150px',
-              }
-            }}
-            editable={{
-              onRowAdd: newData =>
-                new Promise(resolve => {
-                  setTimeout(() => {
-                    resolve();
-                    const data = [...this.state.data];
-                    data.push(newData);
-                    this.setState({ ...this.state, data });
-                  }, 600);
-                }),
-              onRowUpdate: (newData, oldData) =>
-                new Promise(resolve => {
-                  setTimeout(() => {
-                    resolve();
-                    const data = [...this.state.data];
-                    data[data.indexOf(oldData)] = newData;
-                    this.setState({ ...this.state, data });
-                  }, 600);
-                }),
-              onRowDelete: oldData =>
-                new Promise(resolve => {
-                  setTimeout(() => {
-                    resolve();
-                    const data = [...this.state.data];
-                    data.splice(data.indexOf(oldData), 1);
-                    this.setState({ ...this.state, data });
-                  }, 600);
-                }),
-            }}
-          />
-
-          <Modal closeIcon onClose={this.closePatientModal} open={this.state.displayPatientModal}>
-            <Modal.Header>Patient Readings</Modal.Header>
-            <Modal.Content scrolling>
-              <Modal.Description>
-                <Header>Patient Readings for ID #{this.state.selectedPatient.patientId}</Header>
-                <Divider />
-                <Form onSubmit={this.handleSubmit}>
-                  <Form.Group widths='equal'>
-                    <Form.Field
-                      name="patientName"
-                      value={this.state.selectedPatient.patientName}
-                      control={Input}
-                      label='Name'
-                      placeholder='Patient Name'
-                      onChange={this.handleSelectChange}
-                    />
-                    <Form.Field
-                      name="patientAge"
-                      value={this.state.selectedPatient.patientAge}
-                      control={Input}
-                      label='Age'
-                      placeholder='Patient age'
-                      onChange={this.handleSelectChange}
-                    />
-                    <Form.Field
-                      name="patientSex"
-                      control={Select}
-                      value={this.state.selectedPatient.patientSex}
-                      label='Gender'
-                      options={sexOptions}
-                      placeholder='Gender'
-                      onChange={this.handleSelectChange}
-                    />
-                  </Form.Group>
-                  <Form.Group widths='equal'>
-                    <Form.Field
-                      name='villageNumber'
-                      value={this.state.selectedPatient.villageNumber}
-                      control={Input}
-                      label='Village Number'
-                      placeholder='Village Number'
-                      onChange={this.handleSelectChange}
-                    />
-                    <Form.Field
-                      name='isPregnant'
-                      value={this.state.selectedPatient.isPregnant}
-                      control={Select}
-                      label='Pregant'
-                      options={pregOptions}
-                      onChange={this.handleSelectChange}
-                    />
-                  </Form.Group>
-                  <Form.Field
-                    name="drugHistory"
-                    value={this.state.selectedPatient.drugHistory || ''}
-                    control={TextArea}
-                    label='Drug History'
-                    placeholder="Patient's drug history..."
-                    onChange={this.handleSelectChange}
-                  />
-                  <Form.Field
-                    name="medicalHistory"
-                    value={this.state.selectedPatient.medicalHistory || ''}
-                    control={TextArea}
-                    label='Medical History'
-                    placeholder="Patient's medical history..."
-                    onChange={this.handleSelectChange}
-                  />
-                  <Form.Field control={Button}>Submit</Form.Field>
-                </Form>
-
-              </Modal.Description>
-            </Modal.Content>
-          </Modal>
-
-          <Modal closeIcon onClose={this.closeReadingModal} open={this.state.displayReadingModal}>
-            <Modal.Header>Patient Information</Modal.Header>
-            <Modal.Content scrolling>
-              <Modal.Description>
-                <Header>Patient ID #{this.state.selectedPatient.patientId}</Header>
-                <Divider />
-                <p>{readings}</p>
-              </Modal.Description>
-            </Modal.Content>
-          </Modal>
-
-        </div>
-
+      <div>
+        {this.state.showSelectedPatient ? (
+          // <div>
+          //   <Button onClick={() => this.setState({ 'showSelectedPatient' : false }) }>Back</Button>
+          //   <h1>Patient Summary</h1>
+          //   <h2>{this.state.selectedPatient.patientName}</h2>
+          // </div>
+          <PatientSummary callbackFromParent={this.backBtnCallback} selectedPatient={this.state.selectedPatient}></PatientSummary>
+        ) : (
+          <PatientTable callbackFromParent={this.patientCallback} data={this.props.patients.patientsList}></PatientTable>
+        )}
+      </div>
     )
   }
 }
 
 const mapStateToProps = ({ patients, user }) => ({
   patients : patients,
-  data : patients.patientsList,
   user : user.currentUser
 })
 
@@ -271,7 +70,6 @@ const mapDispatchToProps = dispatch =>
     {
       getPatients,
       getCurrentUser,
-      getPosts
     },
     dispatch
   )
---
+++ client/src/containers/patientPage/patientTable.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import MaterialTable from 'material-table';
+
+// const useStyles = makeStyles(theme => ({
+//     initials: { fontSize : '62.5%'}
+// }));
+
+// const classes = useStyles();
+
+class PatientTable extends Component {
+    
+    state = {
+        columns: [
+        {   title: 'Patient Initials',
+            field: 'patientName',
+            render: rowData => 
+                <p key={"initials" + rowData.tableData.id}
+                   style={{"font-size": "200%", "fontWeight" : "bold", "textAlign" : "center"}}
+                >{rowData.patientName}</p>,
+            headerStyle : {
+                textAlign: "center"
+            }
+        },
+        {   title: 'Patient ID', field: 'patientId', defaultSort: 'asc' },
+        {   title: 'Village No.', field: 'villageNumber'},
+        {   title: 'Last Reading', field: 'followUp',
+                render: rowData => rowData.readings ? (<p>{rowData.readings[rowData.readings.length-1].dateTimeTaken}</p>) : (<p>Not needed</p>) },
+        ],
+        data: [],
+        selectedPatient: { patientId: '', patientName: 'Test', 
+                        patientSex: 'F', medicalHistory: '',
+                        drugHistory: '', villageNumber:'', readings: []
+                        }
+  }
+
+  render() {
+
+    return (
+        <MaterialTable
+            title="Patients Table"
+            columns={this.state.columns}
+            data={this.props.data}
+            options={{
+                rowStyle: rowData => {
+                    return {
+                        height: '75px',
+                        // backgroundColor: rowData.tableData.id % 2 === 0 ? "#FFF" : "#F0F0F0",
+                    }
+                }
+            }}
+            onRowClick={(e, rowData) => this.props.callbackFromParent(rowData)}
+        />
+    )
+  }
+}
+
+export default PatientTable
