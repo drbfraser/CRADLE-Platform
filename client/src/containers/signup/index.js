bcb895cb3a571a3e3ac2cdcb2cb9c0f09c8f5944
Kevin Le <kevin_le@sfu.ca>
2019-10-24 02:56:08 -0700

    restrict signup page to be only admin accessible

--- client/src/containers/signup/index.js
+++ client/src/containers/signup/index.js
@@ -4,11 +4,15 @@ import { Route, Link } from 'react-router-dom'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { userPostFetch } from '../../actions/users'
+import { getCurrentUser } from '../../actions/users'
+import { Divider, Message } from 'semantic-ui-react'
 
 class Signup extends Component {
   state = {
     email: "",
     password: "",
+    firstName: "",
+    role: ""
   }
 
   handleChange = event => {
@@ -23,17 +27,36 @@ class Signup extends Component {
     this.props.userPostFetch(this.state)
   }
 
+  componentDidMount = () => {
+    this.props.getCurrentUser()
+  }
+
   render() {
+    // only admins can see this page
+    if (this.props.user.role != 'ADMIN') {
+      return  <Message warning>
+                <Message.Header>Only Admins can enter this page</Message.Header>
+                <p>Please login with an Admin account</p>
+              </Message>
+    }
     return (
       <div className="loginWrapper">
         <form onSubmit={this.handleSubmit} className="content-box">
-          <h1>Sign Up</h1>
+          <h1>Create a User</h1>
 
           <label>Email</label>
           <input
             name='email'
             type='email'
             value={this.state.email}
+            onChange={this.handleChange}
+            />
+          <br/>
+          <label>First Name</label>
+          <input
+            name='firstName'
+            type='text'
+            value={this.state.firstName}
             onChange={this.handleChange}
             />
           <br/>
@@ -45,6 +68,13 @@ class Signup extends Component {
             onChange={this.handleChange}
             />
           <br/>
+          <label>Role</label>
+          <select onChange={this.handleChange} name='role'>
+            <option value="VHT">VHT</option>
+            <option value="HCW">HCW</option>
+            <option value="ADMIN">ADMIN</option>
+          </select>
+          <Divider />
           <div className="flexbox">
             <input className="contant-submit white" type='submit'/>
             <Link className="link" style={{ textDecoration: 'none' }} to="/login"> Login </Link>
@@ -55,15 +85,20 @@ class Signup extends Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  user : user.currentUser
+})
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      userPostFetch
+      userPostFetch,
+      getCurrentUser
     },
     dispatch
   )
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Signup)
