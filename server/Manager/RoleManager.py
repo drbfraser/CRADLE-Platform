f13485d55aa3df468c4f7ef4e7c1b482e2be0353
Kevin Le <kevin_le@sfu.ca>
2019-11-21 02:54:41 -0800

    add role endpoints to add vht to supervising list, and get role name list

--- server/Manager/RoleManager.py
+++ server/Manager/RoleManager.py
@@ -3,7 +3,15 @@ from Manager.Manager import Manager
 from config import db
 from models import Role, User
 
+
 class RoleManager():
+
+    def get_role_names(self, role_ids):
+        roles = []
+        for role_id in role_ids:
+            role = Role.query.filter_by(id=role_id).first()
+            roles.append(role.name.name)
+        return roles
 
     def add_user_to_role(self, user_id, role_ids):
         # remove all user roles
@@ -16,4 +24,20 @@ class RoleManager():
             role = Role.query.filter_by(id=role_id).first()
             role.users.append(user)
             db.session.add(role)
+        db.session.commit()
+
+    def add_vht_to_supervise(self, cho_id, vht_ids):
+        # find the cho
+        cho = User.query.filter_by(id=cho_id).first()
+
+        # check CHO role is actually CHO
+
+        cho.vhtList = []
+        db.session.commit()
+
+        # add vhts to CHO's vhtList
+        for vht_id in vht_ids:
+            vht = User.query.filter_by(id=vht_id).first()
+            cho.vhtList.append(vht)
+            db.session.add(cho)
         db.session.commit()
