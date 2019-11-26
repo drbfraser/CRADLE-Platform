3adb5010f33c5d32efdf2e5422618440615156cd
vinsonl <vinsonl@sfu.ca>
2019-11-26 02:58:51 -0800

    reverted base url change

--- client/src/serverUrl.js
+++ client/src/serverUrl.js
@@ -5,8 +5,7 @@ let BASE_URL;
 if(process.env.NODE_ENV == "production") {
     BASE_URL = "https://cmpt373.csil.sfu.ca:8048/api"
 } else {
-    // BASE_URL = "http://localhost:5000/api"
-    BASE_URL = "https://cmpt373.csil.sfu.ca:8048/api"
+    BASE_URL = "http://localhost:5000/api"
 }
 
 export default BASE_URL
