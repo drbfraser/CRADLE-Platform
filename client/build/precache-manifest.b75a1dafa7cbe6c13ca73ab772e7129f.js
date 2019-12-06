9b55d8a40f849ee4114d632698ce478c1152c59b
Jeffrey Leung <jleung513@gmail.com>
2019-12-06 00:19:18 -0800

    Try again to deploy build files

---
+++ client/build/precache-manifest.b75a1dafa7cbe6c13ca73ab772e7129f.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/client/build/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "ee7ecc1bc723ead05a7b",
+    "url": "/client/build/static/css/main.e8ff79c0.chunk.css"
+  },
+  {
+    "revision": "9388a4bf6ad820b9564c",
+    "url": "/client/build/static/js/runtime~main.0d323570.js"
+  },
+  {
+    "revision": "4959037de59abec64249397172c9934f",
+    "url": "/client/build/static/media/red.4959037d.svg"
+  },
+  {
+    "revision": "060d91fa3e97d2dcd3c8",
+    "url": "/client/build/static/js/2.63b6b33c.chunk.js"
+  },
+  {
+    "revision": "1577c44a049387f664631bd49a08bee9",
+    "url": "/client/build/static/media/splash_screen_4.1577c44a.png"
+  },
+  {
+    "revision": "acda751b17aa68378a193c9e2900bc6c",
+    "url": "/client/build/static/media/patients.acda751b.svg"
+  },
+  {
+    "revision": "3173bc1b9ca88662b0bfadd68c5bd568",
+    "url": "/client/build/static/media/referrals.3173bc1b.svg"
+  },
+  {
+    "revision": "32142613cf2f1688a66967291fabdafc",
+    "url": "/client/build/static/media/statistics.32142613.svg"
+  },
+  {
+    "revision": "3cbc5fc33a690b3337b677d24d5e5392",
+    "url": "/client/build/static/media/create.3cbc5fc3.svg"
+  },
+  {
+    "revision": "3abb1672bf3e95490bd9c3f22a2eb23d",
+    "url": "/client/build/static/media/video-solid.3abb1672.svg"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/client/build/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/client/build/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/client/build/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/client/build/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/client/build/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/client/build/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/client/build/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/client/build/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "ee7ecc1bc723ead05a7b",
+    "url": "/client/build/static/js/main.f0f3c49d.chunk.js"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/client/build/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/client/build/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/client/build/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/client/build/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/client/build/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/client/build/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/client/build/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/client/build/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/client/build/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/client/build/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/client/build/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/client/build/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/client/build/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/client/build/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/client/build/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "be827888e68ef2b8c7705b240544f7f3",
+    "url": "/client/build/static/media/green.be827888.svg"
+  },
+  {
+    "revision": "4d2f56819e2dd14c8701b8c00d30e9b7",
+    "url": "/client/build/static/media/yellow.4d2f5681.svg"
+  },
+  {
+    "revision": "060d91fa3e97d2dcd3c8",
+    "url": "/client/build/static/css/2.b02f2523.chunk.css"
+  },
+  {
+    "revision": "80bc5cd3d312cc0b200344289df8a6c6",
+    "url": "/client/build/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73de13b71ed391205e28ca2a295627e8.js"
+  "/client/build/precache-manifest.b75a1dafa7cbe6c13ca73ab772e7129f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/client/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
