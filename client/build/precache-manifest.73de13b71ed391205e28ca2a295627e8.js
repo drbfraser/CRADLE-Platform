c56386f76a6cbeca91b03c74a05b652194df3ecc
Jeffrey Leung <jleung513@gmail.com>
2019-12-06 00:05:37 -0800

    Change build files to use github deploy for yarn serve

--- client/build/precache-manifest.73de13b71ed391205e28ca2a295627e8.js
+++ client/build/precache-manifest.73de13b71ed391205e28ca2a295627e8.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
-    "url": "/static/media/roboto-latin-300.b00849e0.woff"
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/static/media/roboto-latin-100italic.f9e8e590.woff"
   },
   {
     "revision": "ee7ecc1bc723ead05a7b",
@@ -68,8 +68,8 @@ self.__precacheManifest = [
     "url": "/static/media/roboto-latin-900.9b3766ef.woff2"
   },
   {
-    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
-    "url": "/static/media/roboto-latin-100italic.f9e8e590.woff"
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/static/media/roboto-latin-100.5cb7edfc.woff"
   },
   {
     "revision": "f8b1df51ba843179fa1cc9b53d58127a",
@@ -80,8 +80,12 @@ self.__precacheManifest = [
     "url": "/static/js/main.f0f3c49d.chunk.js"
   },
   {
-    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
-    "url": "/static/media/roboto-latin-100.5cb7edfc.woff"
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/static/media/roboto-latin-300italic.4df32891.woff"
   },
   {
     "revision": "14286f3ba79c6627433572dfa925202e",
@@ -90,10 +94,6 @@ self.__precacheManifest = [
   {
     "revision": "60fa3c0614b8fb2f394fa29944c21540",
     "url": "/static/media/roboto-latin-400.60fa3c06.woff"
-  },
-  {
-    "revision": "4df32891a5f2f98a363314f595482e08",
-    "url": "/static/media/roboto-latin-300italic.4df32891.woff"
   },
   {
     "revision": "51521a2a8da71e50d871ac6fd2187e87",
@@ -108,6 +108,10 @@ self.__precacheManifest = [
     "url": "/static/media/roboto-latin-500.87284894.woff"
   },
   {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
     "revision": "db4a2a231f52e497c0191e8966b0ee58",
     "url": "/static/media/roboto-latin-500italic.db4a2a23.woff2"
   },
@@ -116,16 +120,12 @@ self.__precacheManifest = [
     "url": "/static/media/roboto-latin-700.adcde98f.woff"
   },
   {
-    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
-    "url": "/static/media/roboto-latin-500italic.288ad9c6.woff"
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/static/media/roboto-latin-700italic.da0e7178.woff2"
   },
   {
     "revision": "81f57861ed4ac74741f5671e1dff2fd9",
     "url": "/static/media/roboto-latin-700italic.81f57861.woff"
-  },
-  {
-    "revision": "da0e717829e033a69dec97f1e155ae42",
-    "url": "/static/media/roboto-latin-700italic.da0e7178.woff2"
   },
   {
     "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b09b7ce271ba11abd30cdf4b540c5225.js"
+  "/precache-manifest.73de13b71ed391205e28ca2a295627e8.js"
 );
 
 workbox.clientsClaim();
