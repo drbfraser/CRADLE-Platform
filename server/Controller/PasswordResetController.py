73d4732d94feb57a680be966c536de084412c7ed
ksim <ksim@sfu.ca>
2020-01-23 08:24:27 -0800

    fix errors and changed email sending library

--- server/Controller/PasswordResetController.py
+++ server/Controller/PasswordResetController.py
@@ -1,6 +1,10 @@
 import logging, json
 import datetime
 import yagmail
+import os
+import smtplib
+from email.message import EmailMessage
+
 from flask import request, jsonify
 from flask_restful import Resource, abort
 from flask import Flask, flash, render_template, request, redirect, jsonify, url_for, session
@@ -8,11 +12,14 @@ from models import validate_user, User, UserSchema, Role
 from config import db, flask_bcrypt
 from datetime import timedelta
 from flask_jwt_extended import (create_access_token, create_refresh_token,
-                                jwt_required, jwt_refresh_token_required, get_jwt_identity)
+                                jwt_required, jwt_refresh_token_required, get_jwt_identity, decode_token)
 from Manager.UserManager import UserManager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 userManager = UserManager()
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 
 # /auth/password_reset
@@ -27,112 +34,92 @@ class ForgotPassword(Resource):
     # todo: extract 'SECRET_KEY' and use better secret key?
     def post(self):
         logging.debug("Receive requestion: POST /forgot")
-        expires = datetime.timedelta(hours=24)
         url = request.host_url + 'reset/'
-        args = request.args.get('email')
-        print("args:____________ " + json.dumps(args, indent=2, sort_keys=True))
         try:
             # get email sent from client
             body = self._get_request_body()
             email = body.get('email')
 
-            # is error handling handled on the front-end?
-            user = UserManager.search(args)
+            # TODO: query for user in database
+            user = userManager.read("email", email)
+
+            if user is None:
+                abort(400, message=f'No user with email "{email}" exists.')
+
+            expires = datetime.timedelta(hours=24)
+            print(f'user with email "{email}" has requested for a password reset.')
+            logging.debug(f'user with email "{email}" has requested for a password reset.')
 
             # create reset token
-            reset_token = create_access_token(str(user.id), expires_delta=expires)
+            reset_token = create_access_token(str(email), expires_delta=expires)
 
             # todo: send email
-            contents = [
-                "Dear User,\n\tTo reset your password follow this link: {}".format(reset_token)
-            ]
+            msg = EmailMessage()
+            msg['Subject'] = 'Password Reset Requested'
+            msg['From'] = EMAIL_ADDRESS
+            msg['To'] = EMAIL_ADDRESS
+            msg.set_content(f'Dear User,\n\tTo reset your password follow this link: {reset_token} ' \
+                   f'If this was not requested by you, please ignore this message. ' \
+                   f'\n ' \
+                   f'Cradle Support')
+
+            return url+reset_token, 200
+            with smtplib.SMTP_SSL('smtp.gmail.com', 456) as smtp:
+                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                smtp.sendmail(msg)
+
             logging.debug('Sent link to reset email')
-            yagmail.SMTP('mmarty98@hotmail.com').send('kawaisim@hotmail.com', 'test', contents)
-            flash('If the email exists, you will be sent a link to reset your password', 'warning')
-            return redirect(url('auth'))
+            flash('If the email exists, you will be sent a link to reset your password', 'info')
+
         except Exception as e:
-            logging.debug('An error occurred' + e)
+            # logging.debug('An error occurred' + e)
             # TODO: proper exception handling
             return None
 
-    # def verify_reset_token(self, token):
-    #     s = Serializer(app.config['SECRET_KEY'])
-    #     try:
-    #         user_id = s.loads(token)['user_id']
-    #     except:
-    #         return None
-    #     # user = User.query.filter_by(email=data['email']).first()
-    #     return userManager.search(user_id)  # ???, search for the ID corresponding to the email
-
-
-#
-# class ForgotPassword(Resource):
-#     def post(self):
-#         url = request.host_url + 'reset/'
-#         try:
-#             body = request.get_json()
-#             email = body.get('email')
-#             if not email:
-#                 raise SchemaValidationError
-#
-#             user = User.objects.get(email=email)
-#             if not user:
-#                 raise EmailDoesnotExistsError
-#
-#             expires = datetime.timedelta(hours=24)
-#             reset_token = create_access_token(str(user.id), expires_delta=expires)
-#
-#             return send_email('[Cradle System] Reset Your Password',
-#                               sender='support@cradle.com', # change
-#                               recipients=[user.email],
-#                               text_body=render_template('email/reset_password.txt',
-#                                                         url=url + reset_token),
-#                               html_body=render_template('email/reset_password.html',
-#                                                         url=url + reset_token))
-#         except SchemaValidationError:
-#             raise SchemaValidationError
-#         except EmailDoesnotExistsError:
-#             raise EmailDoesnotExistsError
-#         except Exception as e:
-#             raise InternalServerError
-
 # verify is generated token is valid
-# class ResetPassword(Resource):
-#     def post(self):
-#         url = request.host_url + 'reset/'
-#         try:
-#             body = request.get_json()
-#             reset_token = body.get('reset_token')
-#             password = body.get('password')
-#
-#             if not reset_token or not password:
-#                 logging.debug('Token and password invalid')
-#                 abort(404, message="Invalid token")
-#
-#             # decode token
-#             user_id = decode_token(reset_token)['identity']
-#
-#             # query user id
-#             user = User.objects.get(id=user_id)
-#
-#             # update password
-#             user.modify(password=password)
-#             user.hash_password()
-#             user.save()
-#
-#             # respond with password change success
-#             return send_email('[Movie-bag] Password reset successful',
-#                               sender='support@movie-bag.com',
-#                               recipients=[user.email],
-#                               text_body='Password reset was successful',
-#                               html_body='<p>Password reset was successful</p>')
-#
-#         # except SchemaValidationError:
-#         #     raise SchemaValidationError
-#         # except ExpiredSignatureError:
-#         #     raise ExpiredTokenError
-#         # except (DecodeError, InvalidTokenError):
-#         #     raise BadTokenError
-#         logging.debug('An error occurred' + e)
-#         # TODO: proper exception handling
-#         return None
+class ResetPassword(Resource):
+    def post(self):
+        url = request.host_url + 'reset/'
+        try:
+
+            # retrieve token and newly entered password
+            body = request.get_json()
+            reset_token = body.get('reset_token')
+            password = body.get('password')
+
+            if not reset_token or not password:
+
+                # TODO: proper exception handling
+                logging.debug('Token and password invalid')
+                abort(404, message="Invalid token")
+
+            # decode token
+            user_email = decode_token(reset_token)['identity']
+
+            # TODO: query user with id
+            user_id = UserManager.search()
+
+            # update password
+            # TODO: update user password field
+            userManager.update()
+
+            # respond with password change success
+            contents = [
+                "Dear {}, \n\t your password has been reset. If this was you, "
+            ]
+            yagmail.SMTP('mmarty98@hotmail.com').send('kawaisim@hotmail.com', 'successfully reset', contents)
+            flash('Your email has been changed.', 'info')
+
+        except Exception as e:
+            logging.debug('An error occured' + e)
+
+
+        # except SchemaValidationError:
+        #     raise SchemaValidationError
+        # except ExpiredSignatureError:
+        #     raise ExpiredTokenError
+        # except (DecodeError, InvalidTokenError):
+        #     raise BadTokenError
+        logging.debug('An error occurred' + e)
+        # TODO: proper exception handling
+        return None
