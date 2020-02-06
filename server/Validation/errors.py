bfac24ed6c871275b8630e80f7c4339e930d9fa8
ksim <ksim@sfu.ca>
2020-02-05 18:43:22 -0800

    small fix

--- server/Validation/errors.py
+++ server/Validation/errors.py
@@ -6,22 +6,6 @@ class SchemaValidationError(Exception):
     pass
 
 
-class MovieAlreadyExistsError(Exception):
-    pass
-
-
-class UpdatingMovieError(Exception):
-    pass
-
-
-class DeletingMovieError(Exception):
-    pass
-
-
-class MovieNotExistsError(Exception):
-    pass
-
-
 class EmailAlreadyExistsError(Exception):
     pass
 
@@ -45,22 +29,6 @@ errors = {
     },
     "SchemaValidationError": {
         "message": "Request is missing required fields",
-        "status": 400
-    },
-    "MovieAlreadyExistsError": {
-        "message": "Movie with given name already exists",
-        "status": 400
-    },
-    "UpdatingMovieError": {
-        "message": "Updating movie added by other is forbidden",
-        "status": 403
-    },
-    "DeletingMovieError": {
-        "message": "Deleting movie added by other is forbidden",
-        "status": 403
-    },
-    "MovieNotExistsError": {
-        "message": "Movie with given id doesn't exists",
         "status": 400
     },
     "EmailAlreadyExistsError": {
