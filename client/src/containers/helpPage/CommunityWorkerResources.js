3c806f13f060e697a453c2d4dd9f32c9566a035d
Vinson Ly <vly@linkedin.com>
2020-02-09 19:45:41 -0800

    refactored healthworker and communityworker resource pages to use a react component template

--- client/src/containers/helpPage/CommunityWorkerResources.js
+++ client/src/containers/helpPage/CommunityWorkerResources.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import "./helpPage.css"
+import ResourceTabPage from "./ResourceTabPage"
 
-const COMMUNITY_WORKER_EDUCATION_VIDEO_LINK = "https://www.youtube.com/embed/WvS3L5P4P2c"
+const COMMUNITY_WORKER_POSTER = "images/education/community-poster.jpg"
 const COMMUNITY_PDF_POSTER_LINK = "https://d1b10bmlvqabco.cloudfront.net/attach/k50h6wwq90s2ds/k076kzpukaj5rk/k5vlxvf31tmz/Community_Poster.pdf"
+const COMMUNITY_WORKER_EDUCATION_VIDEO_LINK = "https://www.youtube.com/embed/WvS3L5P4P2c"
 
-export default function CommunityWorkerResources() {
-    return (
-        <div>
-            <img className="education-img" src="images/education/community-poster.jpg" />
-            <p><a href={COMMUNITY_PDF_POSTER_LINK} target="_blank">Click to view/download PDF</a></p>
-            <div className="centered-flexbox margin-top">
-                <iframe width="1080" height="730" src={COMMUNITY_WORKER_EDUCATION_VIDEO_LINK} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-        </div>
-    )
+export default function HealthWorkerResources() {
+    return <ResourceTabPage 
+        posterImgSrc={COMMUNITY_WORKER_POSTER}
+        posterImgUrl={COMMUNITY_PDF_POSTER_LINK}
+        videoUrl={COMMUNITY_WORKER_EDUCATION_VIDEO_LINK}
+    />
 }
--- client/src/containers/helpPage/HealthWorkerResources.js
+++ client/src/containers/helpPage/HealthWorkerResources.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import "./helpPage.css"
-import HEALTH_FACILITY_WORKER_EDUCATION_VIDEO_LINK from './index';
+import ResourceTabPage from "./ResourceTabPage"
 
+const HEALTH_WORKER_POSTER = "images/education/clinic-poster.jpg"
 const CLINIC_PDF_POSTER_LINK = "https://d1b10bmlvqabco.cloudfront.net/attach/k50h6wwq90s2ds/k076kzpukaj5rk/k5vly8uz8c2a/Clinic_Poster.pdf"
+const HEALTH_WORKER_EDUCATION_VIDEO = "https://www.youtube.com/embed/QainNBCHKAg"
+
 
 export default function HealthWorkerResources() {
-    return (
-        <div>
-            <img className="education-img" src="images/education/clinic-poster.jpg" />
-            <p><a href={CLINIC_PDF_POSTER_LINK} target="_blank">Click to view/download PDF</a></p>
-            <div className="centered-flexbox marginTop">
-                <iframe width="1080" height="730" src="https://www.youtube.com/embed/QainNBCHKAg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-        </div>
-    )
+    return <ResourceTabPage 
+        posterImgSrc={HEALTH_WORKER_POSTER}
+        posterImgUrl={CLINIC_PDF_POSTER_LINK}
+        videoUrl={HEALTH_WORKER_EDUCATION_VIDEO}
+    />
 }
