2e18992d3f412109082dbcc79f79cb9c585a999f
ksim <ksim@sfu.ca>
2020-03-11 18:42:09 -0700

    display frequency and instru on patient summary

--- client/src/containers/patientPage/followUpInfo.js
+++ client/src/containers/patientPage/followUpInfo.js
@@ -6,17 +6,23 @@ function FollowUpInfo(props) {
     if (props.followUp) {
         return (
             <Segment>
-                <Header size='small'>Follow Up Action:</Header>
-                <p>{props.followUp.followUpAction}</p>
-                <Divider />
-                <Header size='small'>Diagnosis:</Header>
-                <p>{props.followUp.diagnosis}</p>
-                <Divider />
-                <Header size='small'>Treatment:</Header>
-                <p>{props.followUp.treatment}</p>
-                <Divider />
-                <p><b>Assessed By:</b> Healthcare Worker {props.followUp.healthcareWorkerId}</p>
-                <p><b>Date Last Assessed:</b> {props.followUp.dateAssessed}</p>
+              <Header size='small'>Follow Up Action:</Header>
+              <p>{props.followUp.followUpAction}</p>
+              <Divider />
+              <Header size='small'>Diagnosis:</Header>
+              <p>{props.followUp.diagnosis}</p>
+              <Divider />
+              <Header size='small'>Treatment:</Header>
+              <p>{props.followUp.treatment}</p>
+              <Divider />
+              <Header size='small'>Frequency:</Header>
+              <p>Every {props.followUp.followupFrequencyValue} {props.followUp.followupFrequencyUnit.toLowerCase()} until {props.followUp.dateFollowupNeededTill}</p>
+              <Divider />
+              <Header size='small'>Instructions:</Header>
+              <p>{props.followUp.followupInstructions}</p>
+              <Divider />
+              <p><b>Assessed By:</b> Healthcare Worker {props.followUp.healthcareWorkerId}</p>
+              <p><b>Date Last Assessed:</b> {props.followUp.dateAssessed}</p>
             </Segment>
         )
     } else {
