d6c6f9efa1426e5f47c39bc68ca8bad433cb4c4e
Kisub Song <kisub_song@sfu.ca>
2020-03-17 20:27:06 -0700

    format patientUtils.js

--- client/src/containers/patientPage/patientUtils.js
+++ client/src/containers/patientPage/patientUtils.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import { ReactComponent as GreenTraffic } from './drawable/green.svg';
-import { ReactComponent as YellowTraffic } from './drawable/yellow.svg';
-import { ReactComponent as RedTraffic } from './drawable/red.svg';
+import React from 'react'
+import { ReactComponent as GreenTraffic } from './drawable/green.svg'
+import { ReactComponent as YellowTraffic } from './drawable/yellow.svg'
+import { ReactComponent as RedTraffic } from './drawable/red.svg'
 import { Icon } from 'semantic-ui-react'
 
-export const getTrafficIcon = (trafficLightStatus) => {
-    if (trafficLightStatus === "RED_DOWN") {
-      return <div>
-        <RedTraffic style={{ "height": "65px", "width": "65px" }} />
-        <Icon name="arrow down" size="huge" />
-      </div>
-    } else if (trafficLightStatus === "RED_UP") {
-      return <div>
-        <RedTraffic style={{ "height": "65px", "width": "65px" }} />
-        <Icon name="arrow up" size="huge" />
-      </div>
-    } else if (trafficLightStatus === "YELLOW_UP") {
-      return <div>
-        <YellowTraffic style={{ "height": "65px", "width": "65px" }} />
-        <Icon name="arrow up" size="huge" />
-      </div>
-    } else if (trafficLightStatus === "YELLOW_DOWN") {
-      return <div>
-        <YellowTraffic style={{ "height": "65px", "width": "65px" }} />
-        <Icon name="arrow down" size="huge" />
-      </div>
+export const getTrafficIcon = trafficLightStatus => {
+    if (trafficLightStatus === 'RED_DOWN') {
+        return (
+            <div>
+                <RedTraffic style={{ height: '65px', width: '65px' }} />
+                <Icon name="arrow down" size="huge" />
+            </div>
+        )
+    } else if (trafficLightStatus === 'RED_UP') {
+        return (
+            <div>
+                <RedTraffic style={{ height: '65px', width: '65px' }} />
+                <Icon name="arrow up" size="huge" />
+            </div>
+        )
+    } else if (trafficLightStatus === 'YELLOW_UP') {
+        return (
+            <div>
+                <YellowTraffic style={{ height: '65px', width: '65px' }} />
+                <Icon name="arrow up" size="huge" />
+            </div>
+        )
+    } else if (trafficLightStatus === 'YELLOW_DOWN') {
+        return (
+            <div>
+                <YellowTraffic style={{ height: '65px', width: '65px' }} />
+                <Icon name="arrow down" size="huge" />
+            </div>
+        )
     } else {
-      return <GreenTraffic style={{ "height": "65px", "width": "65px" }} />
+        return <GreenTraffic style={{ height: '65px', width: '65px' }} />
     }
-  }
+}
