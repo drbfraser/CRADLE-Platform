44fd53c774e8ddea912c5c81fe59aeb3e10758b5
Vinson FM Ly <vinsonly@Vinsons-MacBook-Pro.local>
2020-04-30 05:14:17 -0700

    configured gunicorn settings, set up gunicorn log volume and log files, run gunicorn using config file

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -5,22 +5,27 @@
 import os
 import multiprocessing
 
-_ROOT = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..'))
-_VAR = os.path.join(_ROOT, 'logs')
+port = os.environ.get('PORT')
+if port is None:
+    print('PORT environment variable not found. Using default (5000).')
+else:
+    print('PORT environment variable found:', port)
+
+_ROOT = '/'
+_VAR = os.path.join(_ROOT, 'var')
 
 loglevel = 'info'
-# errorlog = os.path.join(_VAR, 'log/cradle-error.log')
-# accesslog = os.path.join(_VAR, 'log/cradle-access.log')
-errorlog = "-"
-accesslog = "-"
+errorlog = os.path.join(_VAR, 'log/cradle-error.log')
+accesslog = os.path.join(_VAR, 'log/cradle-access.log')
+# errorlog = "-"
+# accesslog = "-"
 
 # bind = 'unix:%s' % os.path.join(_VAR, 'run/gunicorn.sock')
 bind = '0.0.0.0:5000'
 # workers = 3
 workers = multiprocessing.cpu_count() * 2 + 1
 
-timeout = 3 * 60  # 3 minutes
-keepalive = 24 * 60 * 60  # 1 day
+timeout = 30  # 30 s
+keepalive = 5 # 5 s
 
 capture_output = True
