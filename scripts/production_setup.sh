0bba079a680bcda112f251e57803d455365940d5
Vinson FM Ly <vinsonly@Vinsons-MacBook-Pro.local>
2020-04-30 23:17:41 -0700

    updated production_setup script

--- scripts/production_setup.sh
+++ scripts/production_setup.sh
@@ -4,9 +4,7 @@
 set -e
 
 echo "Starting Cradle Production Setup"
-
-echo "Please enter password to switch to root user."
-sudo su
+echo "This script must be run as a user with sudo privileges."
 
 echo "Updating packages..."
 apt-get update -y
@@ -16,7 +14,6 @@ apt-get remove docker docker-engine docker.io -y
 
 echo "Installing docker"
 apt install docker.io -y
-
 
 echo "Starting the docker service and setting docker to automatically start at boot"
 systemctl start docker
@@ -28,25 +25,13 @@ echo "Installing docker compose"
 curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-echo "Installing cradle-platform to /var/wwww"
-echo "Please enter your gitlab username and password"
-echo "WARNING: these git credentials will be saved on this server in ~/.git-credentials for future us by the CI/CD system and will be publically visible to other users that have root access to this machine."
-mkdir /var/www && cd /var/www
-git clone https://csil-git1.cs.surrey.sfu.ca/415-cradle/cradle-platform.git cradle-platform
-git config --global credential.helper store
-git pull
-
 echo "Spinning up docker containers..."
-cd /var/www/cradle-platform
-docker-compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.prod.yml up -d --build
 
 echo "Initializing database"
 docker exec cradle flask db init
 
 echo "Creating the tables..."
 docker exec cradle flask db upgrade
-
-echo "Seeding the initial user information..."
-python manage.py seed
 
 echo "Cradle Production Setup completed successfully! Navigate to port 80 to check out the app."
