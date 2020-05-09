8738872eceb482cd53f1e3a1df1d55d34eacfa1b
Vinson FM Ly <vinsonly@Vinsons-MacBook-Pro.local>
2020-05-09 02:12:06 -0700

    changed gunicorn workers to only 1

--- server/prod/gunicorn.conf.py
+++ server/prod/gunicorn.conf.py
@@ -21,7 +21,8 @@ accesslog = os.path.join(_VAR, 'log/cradle-access.log')
 
 # bind = 'unix:%s' % os.path.join(_VAR, 'run/gunicorn.sock')
 bind = '0.0.0.0:5000'
-# workers = 3
+# workers = 1
+# TODO: when upgrading server, uncoming the line below to replace the line above
 workers = multiprocessing.cpu_count() * 2 + 1
 
 timeout = 30  # 30 s
