d6df3b8e1be08343f97cb2ef926df31f3792feb7
Vinson FM Ly <vinsonly@Vinsons-MacBook-Pro.local>
2020-05-10 09:35:49 -0700

    moved logic to create network outside of condition that checks whether mysql container is running in start_db.sh

--- server/prod/start_db.sh
+++ server/prod/start_db.sh
@@ -8,12 +8,20 @@ echo $0
 db_env_file_path="$(dirname $(dirname $(realpath $0)))/.env.prod.db"
 echo $db_env_file_path
 
+if ! docker network inspect db_network;
+then
+    echo "creating network db_network"
+    docker network create db_network
+fi
+
+# create new network for to allow containers to connect to the database's exposing ports if not already existing
+docker network create db_network
+
 if ! nc -z 127.0.0.1 3307;
 then
-    # create new network for to allow containers to connect to the database's exposing ports if not already existing
-    docker network create db_network
     # remove any existing mysql container
     docker rm mysql
     # create and start mysql docker container 
+    echo "Running mysql container"
     docker run --name mysql -v mysql_data:/var/lib/mysql -p 127.0.0.1:3307:3306 --env-file $db_env_file_path --restart always -d mysql:latest
 fi
