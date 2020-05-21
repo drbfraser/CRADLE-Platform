d100c73984e7965da2b6a50269fd055abb14a82a
Waswa <wolunga@sfu.ca>
2020-05-20 20:14:07 -0700

    Add instructions

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,22 @@ import 'sanitize.css/sanitize.css';
 import './index.css';
 
 // * Using new structure
+// * Comment out lines 17 and 18 and uncomment 6 and 8
 import { history, reduxStore as store } from './newStructure/redux/store';
 
 import { App } from './newStructure/app';
-/*
-Using old structure
-import store, { history } from './oldStructure/store';
-import App from './oldStructure/containers/app';
-*/
 import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { render } from 'react-dom';
+
+/*
+// * Using old structure
+// * Comment out lines 5 and 7 and uncomment below
+import store, { history } from './oldStructure/store';
+import App from './oldStructure/containers/app';
+*/
+
 
 render(
   <ReduxProvider store={store}>
