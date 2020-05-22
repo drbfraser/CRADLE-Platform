57b0b57aec49502961c23379cdd9427bdd438f97
Waswa <wolunga@sfu.ca>
2020-05-22 11:18:05 -0700

    Update server config

--- client/src/newStructure/server/utils/index.ts
+++ client/src/newStructure/server/utils/index.ts
@@ -9,11 +9,11 @@ if (process.env.NODE_ENV == `production`) {
   URL = `https://cradle.eastus.cloudapp.azure.com/api`;
 }
 
-if (process.env.REACT_APP_USE_LEGACY_SERVER == 1) {
+if (parseInt(process.env.REACT_APP_USE_LEGACY_SERVER ?? ``) == 1) {
   URL = `https://cmpt373.csil.sfu.ca:8048/api`;
 }
 
-if (process.env.REACT_APP_USE_STAGING_SERVER == 1) {
+if (parseInt(process.env.REACT_APP_USE_STAGING_SERVER ?? ``) == 1) {
   URL = `https://cradle.eastus.cloudapp.azure.com:4443/api`;
 }
 
