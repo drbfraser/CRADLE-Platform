1ede02d313c627b4609b687d8cf00d34e173fed9
Waswa <wolunga@sfu.ca>
2020-05-23 11:15:07 -0700

    Add functional component boilerplate

--- client/src/newStructure/pages/patients/patientTable/index.tsx
+++ client/src/newStructure/pages/patients/patientTable/index.tsx
@@ -3,7 +3,7 @@ import {
   getLatestReadingDateTime,
   getPrettyDate,
   getTrafficIcon,
-  sortPatientsByLastReading,
+  sortPatientsByLastReading
 } from '../../../shared/utils';
 
 import MaterialTable from 'material-table';
@@ -18,140 +18,114 @@ interface IProps {
   isLoading: boolean;
 }
 
-interface IState {
-  columns: any;
-  data: any;
-  selectedPatient: any;
-  showReferredPatients: any;
-}
+export const PatientTable: React.FC<IProps> = ({
+  callbackFromParent,
+  data,
+  isLoading
+}) => {
+  const [showReferredPatients, setShowReferredPatients] = React.useState<
+    boolean
+  >(false);
 
-export class PatientTable extends React.Component<IProps, IState> {
-  state = {
-    columns: [
-      {
-        title: `Patient Initials`,
-        field: `patientName`,
-        render: (rowData: any) => (
-          <p
-            key={`initials` + rowData.tableData.id}
-            style={{
-              fontSize: `200%`,
-              fontWeight: `bold`,
-              textAlign: `center`,
-            }}>
-            {rowData.patientName}
-          </p>
-        ),
-        headerStyle: {
-          textAlign: `center` as TextAlignProperty,
-        },
-      },
-      {
-        title: `Patient ID`,
-        field: `patientId`,
-        customSort: (left: any, right: any) =>
-          Number(left.patientId) - Number(right.patientId),
-      },
-      { title: `Village`, field: `villageNumber` },
-      {
-        title: `Vital Sign`,
-        cellStyle: {
-          padding: `0px`,
-        },
-        render: (rowData: any) =>
-          getTrafficIcon(getLatestReading(rowData.readings).trafficLightStatus),
-        customSort: (left: any, right: any) => {
-          const leftIndex = trafficLights.indexOf(
-            left.readings[0].trafficLightStatus
-          );
-          const rightIndex = trafficLights.indexOf(
-            right.readings[0].trafficLightStatus
-          );
+  const handleSwitchChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => setShowReferredPatients(event.target.checked);
 
-          return leftIndex - rightIndex;
-        },
-      },
-      {
-        title: `Date of Last Reading`,
-        field: `lastReading`,
-        render: (rowData: any) => (
-          <p>{getPrettyDate(getLatestReadingDateTime(rowData.readings))}</p>
-        ),
-        customSort: (a: any, b: any) => sortPatientsByLastReading(a, b),
-        defaultSort: `asc` as `asc`,
-      },
-    ],
-    data: [],
-    selectedPatient: {
-      patientId: ``,
-      patientName: `Test`,
-      patientSex: `F`,
-      medicalHistory: ``,
-      drugHistory: ``,
-      villageNumber: ``,
-      readings: [],
-    },
-    showReferredPatients: false,
-  };
-
-  handleSwitchChange(event: any) {
-    this.setState({
-      showReferredPatients: event.target.checked,
+  const patientHasReferral = (readings: any): boolean =>
+    readings.some((reading: any) => {
+      return reading.dateReferred !== undefined;
     });
-  }
 
-  getPatientsToRender(showReffered: any) {
-    return this.props.data.filter((patient: any) => {
-      let hasReferral = this.patientHasReferral(patient.readings);
-      if (showReffered) {
-        return hasReferral;
-      } else {
-        return !hasReferral;
-      }
+  const getPatientsToRender = (): any =>
+    data.filter((patient: any): boolean => {
+      return showReferredPatients && patientHasReferral(patient.readings);
     });
-  }
 
-  patientHasReferral(readings: any) {
-    return readings.some((reading: any) => {
-      return reading.dateReferred != undefined;
-    });
-  }
-
-  render() {
-    let patientData = this.getPatientsToRender(this.state.showReferredPatients);
-    return (
-      <MaterialTable
-        title="Patients"
-        isLoading={this.props.isLoading}
-        columns={this.state.columns}
-        data={patientData}
-        options={{
-          pageSize: 10,
-          rowStyle: (rowData) => {
-            return {
-              height: `75px`,
-            };
+  return (
+    <MaterialTable
+      title="Patients"
+      isLoading={isLoading}
+      columns={[
+        {
+          title: `Patient Initials`,
+          field: `patientName`,
+          render: (rowData: any) => (
+            <p
+              key={`initials` + rowData.tableData.id}
+              style={{
+                fontSize: `200%`,
+                fontWeight: `bold`,
+                textAlign: `center`
+              }}>
+              {rowData.patientName}
+            </p>
+          ),
+          headerStyle: {
+            textAlign: `center` as TextAlignProperty
+          }
+        },
+        {
+          title: `Patient ID`,
+          field: `patientId`,
+          customSort: (left: any, right: any) =>
+            Number(left.patientId) - Number(right.patientId)
+        },
+        { title: `Village`, field: `villageNumber` },
+        {
+          title: `Vital Sign`,
+          cellStyle: {
+            padding: `0px`
           },
-          sorting: true,
-        }}
-        onRowClick={(_, rowData) => this.props.callbackFromParent(rowData)}
-        actions={[
-          {
-            icon: () => {
-              return (
-                <Switch
-                  onChange={this.handleSwitchChange.bind(this)}
-                  color="primary"
-                  checked={this.state.showReferredPatients}
-                />
-              );
-            },
-            tooltip: `Show referred patients only`,
-            isFreeAction: true,
-            onClick: () => { return; }
-          },
-        ]}
-      />
-    );
-  }
-}
+          render: (rowData: any) =>
+            getTrafficIcon(
+              getLatestReading(rowData.readings).trafficLightStatus
+            ),
+          customSort: (left: any, right: any) => {
+            const leftIndex = trafficLights.indexOf(
+              left.readings[0].trafficLightStatus
+            );
+            const rightIndex = trafficLights.indexOf(
+              right.readings[0].trafficLightStatus
+            );
+
+            return leftIndex - rightIndex;
+          }
+        },
+        {
+          title: `Date of Last Reading`,
+          field: `lastReading`,
+          render: (rowData: any) => (
+            <p>{getPrettyDate(getLatestReadingDateTime(rowData.readings))}</p>
+          ),
+          customSort: (a: any, b: any) => sortPatientsByLastReading(a, b),
+          defaultSort: `asc` as `asc`
+        }
+      ]}
+      data={getPatientsToRender()}
+      options={{
+        pageSize: 10,
+        rowStyle: (): React.CSSProperties => ({
+          height: 75
+        }),
+        sorting: true
+      }}
+      onRowClick={(_, rowData) => callbackFromParent(rowData)}
+      actions={[
+        {
+          icon: (): React.ReactElement => (
+            <Switch
+              onChange={handleSwitchChange}
+              color="primary"
+              checked={showReferredPatients}
+            />
+          ),
+          tooltip: `Show referred patients only`,
+          isFreeAction: true,
+          onClick: () => {
+            return;
+          }
+        }
+      ]}
+    />
+  );
+};
