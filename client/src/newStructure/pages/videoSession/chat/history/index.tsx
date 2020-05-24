0f5ec3d80813dd2a58431f73e9e8bc38556d21ec
Waswa <wolunga@sfu.ca>
2020-05-24 00:32:36 -0700

    Update Video Session Page to function components

--- client/src/newStructure/pages/videoSession/chat/history/index.tsx
+++ client/src/newStructure/pages/videoSession/chat/history/index.tsx
@@ -1,52 +1,37 @@
 import React from 'react';
-
-const chatAvatar = {
-  height: 50,
-  marginLeft: 10,
-  width: 50,
-  border: `1px solid`,
-  borderRadius: 10,
-};
+import classes from './styles.module.css';
 
 interface IProps {
   chatHistory: any;
 }
 
-export const ChatHistory: React.FC<IProps> = (props) => (
-  <div
-    id="chatHistory"
-    style={{
-      height: `calc(100% - 95px)`,
-      overflowY: `scroll`,
-      borderBottom: `2px solid black`,
-    }}>
-    {props.chatHistory.map((msg: any) => {
-      return (
+export const ChatHistory: React.FC<IProps> = props => (
+  <div id="chatHistory" className={classes.container}>
+    {props.chatHistory.map((msg: any) => (
+      <div
+        style={{
+          display: `flex`,
+          marginBlockStart: 5,
+          paddingBlockEnd: 5,
+          borderBottomStyle: `solid`,
+          borderWidth: 1,
+          borderColor: `black`
+        }}>
+        {msg.sender == `opener` ? (
+          <img src="/images/doctor.png" className={classes.chatAvatar} />
+        ) : (
+          <img src="/images/girl-doctor.png" className={classes.chatAvatar} />
+        )}
         <div
           style={{
             display: `flex`,
-            marginBlockStart: 5,
-            paddingBlockEnd: 5,
-            borderBottomStyle: `solid`,
-            borderWidth: 1,
-            borderColor: `black`,
+            flexDirection: `column`,
+            margin: `0 10px`
           }}>
-          {msg.sender == `opener` ? (
-            <img src="/images/doctor.png" style={chatAvatar} />
-          ) : (
-            <img src="/images/girl-doctor.png" style={chatAvatar} />
-          )}
-          <div
-            style={{
-              display: `flex`,
-              flexDirection: `column`,
-              margin: `0 10px`,
-            }}>
-            <span style={{ fontWeight: `bold` }}>{msg.senderName}</span>
-            <span>{msg.text}</span>
-          </div>
+          <span style={{ fontWeight: `bold` }}>{msg.senderName}</span>
+          <span>{msg.text}</span>
         </div>
-      );
-    })}
+      </div>
+    ))}
   </div>
 );
