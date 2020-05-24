38faab3440f7f0a82f7ac9b57b56f542dafd0c1c
Waswa <wolunga@sfu.ca>
2020-05-24 11:50:11 -0700

    Update all form components

--- client/src/newStructure/shared/components/form/symptom/index.tsx
+++ client/src/newStructure/shared/components/form/symptom/index.tsx
@@ -19,80 +19,78 @@ export const SymptomForm: React.FC<IProps> = ({
     <Header>
       <b>Symptoms</b>
     </Header>
-    <div>
+    <Form.Checkbox
+      checked={checkedItems.none}
+      value={checkedItems.none}
+      name="none"
+      label="None (patient healthy)"
+      onChange={onChange}
+    />
+    <Form.Group widths="equal">
       <Form.Checkbox
-        checked={checkedItems.none}
-        value={checkedItems.none}
-        name="none"
-        label="None (patient healthy)"
+        checked={checkedItems.headache}
+        value={checkedItems.headache}
+        name="headache"
+        label="Headache"
         onChange={onChange}
       />
-      <Form.Group widths="equal">
-        <Form.Checkbox
-          checked={checkedItems.headache}
-          value={checkedItems.headache}
-          name="headache"
-          label="Headache"
-          onChange={onChange}
-        />
-        <Form.Checkbox
-          checked={checkedItems.bleeding}
-          value={checkedItems.bleeding}
-          name="bleeding"
-          label="Bleeding"
-          onChange={onChange}
-        />
-      </Form.Group>
-      <Form.Group widths="equal">
-        <Form.Checkbox
-          checked={checkedItems.blurredVision}
-          value={checkedItems.blurredVision}
-          name="blurredVision"
-          label="Blurred vision"
-          onChange={onChange}
-        />
-        <Form.Checkbox
-          checked={checkedItems.feverish}
-          value={checkedItems.feverish}
-          name="feverish"
-          label="Feverish"
-          onChange={onChange}
-        />
-      </Form.Group>
-      <Form.Group widths="equal">
-        <Form.Checkbox
-          checked={checkedItems.abdominalPain}
-          value={checkedItems.abdominalPain}
-          name="abdominalPain"
-          label="Abdominal pain"
-          onChange={onChange}
-        />
-        <Form.Checkbox
-          checked={checkedItems.unwell}
-          value={checkedItems.unwell}
-          name="unwell"
-          label="Unwell"
-          onChange={onChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Checkbox
-          checked={checkedItems.other}
-          value={checkedItems.other}
-          widths="3"
-          name="other"
-          label="Other:"
-          onChange={onChange}
-        />
-        <Form.TextArea
-          checked={checkedItems.otherSymptoms}
-          widths="1"
-          name="otherSymptoms"
-          value={checkedItems.otherSymptoms}
-          onChange={onOtherChange}
-          disabled={!checkedItems.other}
-        />
-      </Form.Group>
-    </div>
+      <Form.Checkbox
+        checked={checkedItems.bleeding}
+        value={checkedItems.bleeding}
+        name="bleeding"
+        label="Bleeding"
+        onChange={onChange}
+      />
+    </Form.Group>
+    <Form.Group widths="equal">
+      <Form.Checkbox
+        checked={checkedItems.blurredVision}
+        value={checkedItems.blurredVision}
+        name="blurredVision"
+        label="Blurred vision"
+        onChange={onChange}
+      />
+      <Form.Checkbox
+        checked={checkedItems.feverish}
+        value={checkedItems.feverish}
+        name="feverish"
+        label="Feverish"
+        onChange={onChange}
+      />
+    </Form.Group>
+    <Form.Group widths="equal">
+      <Form.Checkbox
+        checked={checkedItems.abdominalPain}
+        value={checkedItems.abdominalPain}
+        name="abdominalPain"
+        label="Abdominal pain"
+        onChange={onChange}
+      />
+      <Form.Checkbox
+        checked={checkedItems.unwell}
+        value={checkedItems.unwell}
+        name="unwell"
+        label="Unwell"
+        onChange={onChange}
+      />
+    </Form.Group>
+    <Form.Group>
+      <Form.Checkbox
+        checked={checkedItems.other}
+        value={checkedItems.other}
+        widths="3"
+        name="other"
+        label="Other:"
+        onChange={onChange}
+      />
+      <Form.TextArea
+        checked={checkedItems.otherSymptoms}
+        widths="1"
+        name="otherSymptoms"
+        value={checkedItems.otherSymptoms}
+        onChange={onOtherChange}
+        disabled={!checkedItems.other}
+      />
+    </Form.Group>
   </Paper>
 );
