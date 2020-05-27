b5463e4c78b6605d5a600abfcd78049025ba8041
iamwaswa <wolunga@sfu.ca>
2020-05-27 11:15:35 -0700

    Update index.tsx to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,11 @@
 import 'sanitize.css/sanitize.css';
 import './index.css';
 
-import { history, reduxStore } from './newStructure/redux/store';
+// import { history, reduxStore } from './newStructure/redux/store';
+import reduxStore, { history }  from './oldStructure/store';
 
-import {App} from './newStructure/app';
+// import { App } from './newStructure/app';
+import App from './oldStructure/containers/app';
 import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
