b04e735c5430ff504530e73ca20db9d41955d78f
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2020-05-31 23:04:32 -0700

    #temp160 fix import issue

--- client/src/newStructure/pages/newReading/index.tsx
+++ client/src/newStructure/pages/newReading/index.tsx
@@ -9,11 +9,11 @@ import {
   // afterNewPatientAdded
 } from '../../shared/reducers/patients';
 import { addNewReading } from '../../shared/reducers/newReadingStatus';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogActions from '@material-ui/core/DialogActions';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogActions from '@material-ui/core/DialogActions';
 
 import { BpForm } from './bpForm';
 import SweetAlert from 'sweetalert2-react';
