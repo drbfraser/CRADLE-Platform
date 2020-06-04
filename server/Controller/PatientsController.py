84e7cb49479848eb0fdd94e8f19e1a3b52f93d78
Avneet Toor <avneett@sfu.ca>
2020-06-04 12:54:03 -0700

    Use black to reformat files touched in the merge request

--- server/Controller/PatientsController.py
+++ server/Controller/PatientsController.py
@@ -15,8 +15,13 @@ from Manager.UserManager import UserManager
 from Manager.PatientFacilityManager import PatientFacilityManager
 from Manager.urineTestManager import urineTestManager
 
-from flask_jwt_extended import (create_access_token, create_refresh_token,
-                                    jwt_required, jwt_refresh_token_required, get_jwt_identity)
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+)
 from flasgger import swag_from
 
 patientManager = PatientManagerNew()
@@ -25,7 +30,7 @@ userManager = UserManager()
 patientFacilityManager = PatientFacilityManager()
 
 urineTestManager = urineTestManager()
-decoding_error = 'The json body could not be decoded. Try enclosing appropriate fields with quotations, or ensuring that values are comma separated.'
+decoding_error = "The json body could not be decoded. Try enclosing appropriate fields with quotations, or ensuring that values are comma separated."
 
 
 def abort_if_body_empty(request_body):
@@ -53,32 +58,31 @@ def abort_if_patient_exists(patient_id):
 # output: patient data w/ age populated (int)
 def calculate_age_from_dob(patient_data):
     SECONDS_IN_YEAR = 31557600
-    age = (time.time() - patient_data['dob']) / SECONDS_IN_YEAR
-    patient_data['patientAge'] = int(age)
+    age = (time.time() - patient_data["dob"]) / SECONDS_IN_YEAR
+    patient_data["patientAge"] = int(age)
     return patient_data
 
 
 # URI: /api/patient [Get, Post]
 # [GET]: Get a list of patients
-# [POST]: Create a new patient 
+# [POST]: Create a new patient
 class PatientAll(Resource):
-
     @staticmethod
     def _get_request_body():
         raw_req_body = request.get_json(force=True)
-        if 'patient' in raw_req_body:
-            body = raw_req_body['patient']
+        if "patient" in raw_req_body:
+            body = raw_req_body["patient"]
         else:
             body = raw_req_body
-        print('Request body: ' + json.dumps(body, indent=2, sort_keys=True))
+        print("Request body: " + json.dumps(body, indent=2, sort_keys=True))
         return body
 
     # Get list of all patients
     @staticmethod
     @jwt_required
-    @swag_from('../specifications/patient-all.yml', methods=['GET'])
+    @swag_from("../specifications/patient-all.yml", methods=["GET"])
     def get():
-        logging.debug('Received request: GET /patient')
+        logging.debug("Received request: GET /patient")
 
         patients = patientManager.read_all()
         if patients is None:
@@ -87,27 +91,31 @@ class PatientAll(Resource):
 
     # Create a new patient
     @jwt_required
-    @swag_from('../specifications/patient-post.yml', methods=['POST'])
+    @swag_from("../specifications/patient-post.yml", methods=["POST"])
     def post(self):
-        logging.debug('Received request: POST /patient')
+        logging.debug("Received request: POST /patient")
         try:
             patient_data = self._get_request_body()
         except:
-            return {'HTTP 400': decoding_error}, 400
+            return {"HTTP 400": decoding_error}, 400
         patient_data = self._get_request_body()
 
-        if 'dob' in patient_data and patient_data['dob'] is not None:
-            patient_data['dob'] = int(patient_data['dob'])
+        if "dob" in patient_data and patient_data["dob"] is not None:
+            patient_data["dob"] = int(patient_data["dob"])
 
         # Ensure all data is valid
         abort_if_body_empty(patient_data)
-        abort_if_patient_exists(patient_data['patientId'])
+        abort_if_patient_exists(patient_data["patientId"])
         invalid = PatientValidation.check_patient_fields(patient_data)
         if invalid is not None:
             return invalid
 
         # if age is not provided, populate age using dob
-        if 'dob' in patient_data and patient_data['dob'] and patient_data['patientAge'] is None:
+        if (
+            "dob" in patient_data
+            and patient_data["dob"]
+            and patient_data["patientAge"] is None
+        ):
             patient_data = calculate_age_from_dob(patient_data)
         response_body = patientManager.create(patient_data)
         return response_body, 201
@@ -119,16 +127,16 @@ class PatientAll(Resource):
         return {}
 
 
-# URI: api/patient/<string:patient_id> 
+# URI: api/patient/<string:patient_id>
 # [GET]: Get a specific patient's information
 # [PUT]: Update a specific patient's information
 class PatientInfo(Resource):
 
     # Get a single patient
     @jwt_required
-    @swag_from('../specifications/patient-get.yml', methods=['GET'])
+    @swag_from("../specifications/patient-get.yml", methods=["GET"])
     def get(self, patient_id):
-        logging.debug('Received request: GET /patient/' + patient_id)
+        logging.debug("Received request: GET /patient/" + patient_id)
         patient = patientManager.read("patientId", patient_id)
 
         if patient is None:
@@ -137,9 +145,9 @@ class PatientInfo(Resource):
 
     # Update patient info
     @jwt_required
-    @swag_from('../specifications/patient-put.yml', methods=['PUT'])
+    @swag_from("../specifications/patient-put.yml", methods=["PUT"])
     def put(self, patient_id):
-        logging.debug('Received request: PUT /patient/' + patient_id)
+        logging.debug("Received request: PUT /patient/" + patient_id)
 
         data = _get_request_body()
 
@@ -155,19 +163,19 @@ class PatientInfo(Resource):
 
 # URI: api/patient/reading/:id [POST]
 # [GET]: Get a patient's information w/ reading information
-# [POST]: Create a new patient with a reading 
+# [POST]: Create a new patient with a reading
 class PatientReading(Resource):
     # Get a single patient
     def get(self, patient_id):
-        logging.debug('Received request: GET /patient/' + patient_id)
+        logging.debug("Received request: GET /patient/" + patient_id)
         patient = patientManager.read("patientId", patient_id)
 
         new_readings = []
-        for reading in patient['readings']:
+        for reading in patient["readings"]:
             new_reading = readingManager.read("readingId", reading)
             new_reading["urineTests"] = urineTestManager.read("readingId", reading)
             new_readings.append(new_reading)
-        patient['readings'] = new_readings
+        patient["readings"] = new_readings
 
         if patient is None:
             abort(404, message="Patient {} doesn't exist.".format(patient_id))
@@ -175,18 +183,22 @@ class PatientReading(Resource):
 
     # Create a new patient with a reading
     @jwt_required
-    @swag_from('../specifications/patient-reading-post.yml', methods=['POST'])
+    @swag_from("../specifications/patient-reading-post.yml", methods=["POST"])
     def post(self):
-        logging.debug('Received request: POST api/patient/reading')
+        logging.debug("Received request: POST api/patient/reading")
         try:
             patient_reading_data = _get_request_body()
         except:
-            return {'HTTP 400': decoding_error}, 400
+            return {"HTTP 400": decoding_error}, 400
         patient_reading_data = _get_request_body()
         # Ensure all data is valid
         abort_if_body_empty(patient_reading_data)
-        is_invalid_patient = PatientValidation.check_patient_fields(patient_reading_data['patient'])
-        is_invalid_reading = PatientValidation.check_reading_fields(patient_reading_data['reading'])
+        is_invalid_patient = PatientValidation.check_patient_fields(
+            patient_reading_data["patient"]
+        )
+        is_invalid_reading = PatientValidation.check_reading_fields(
+            patient_reading_data["reading"]
+        )
 
         if is_invalid_patient is not None:
             return is_invalid_patient
@@ -195,40 +207,46 @@ class PatientReading(Resource):
         if is_invalid_reading is not None:
             return is_invalid_reading
 
-        patient_data = patient_reading_data['patient']
-        if 'dob' in patient_data and patient_data['dob'] and patient_data['patientAge'] is None:
-            patient_reading_data['patient']['dob'] = int(patient_reading_data['patient']['dob'])
-            patient_reading_data['patient'] = calculate_age_from_dob(patient_data)
+        patient_data = patient_reading_data["patient"]
+        if (
+            "dob" in patient_data
+            and patient_data["dob"]
+            and patient_data["patientAge"] is None
+        ):
+            patient_reading_data["patient"]["dob"] = int(
+                patient_reading_data["patient"]["dob"]
+            )
+            patient_reading_data["patient"] = calculate_age_from_dob(patient_data)
 
         # create new reading (and patient if it does not already exist)
         reading_and_patient = readingManager.create_reading_and_patient(
-            patient_reading_data['patient']['patientId'],
-            patient_reading_data
+            patient_reading_data["patient"]["patientId"], patient_reading_data
         )
 
         # add patient to the facility of the user that took their reading
-        user = userManager.read("id", patient_reading_data['reading']['userId'])
-        userFacility = user['healthFacilityName']
+        user = userManager.read("id", patient_reading_data["reading"]["userId"])
+        userFacility = user["healthFacilityName"]
         patientFacilityManager.add_patient_facility_relationship(
-            patient_reading_data['patient']['patientId'],
-            userFacility
+            patient_reading_data["patient"]["patientId"], userFacility
         )
 
         # associate new reading with patient
-        reading_and_patient['message'] = 'Patient reading created successfully!'
+        reading_and_patient["message"] = "Patient reading created successfully!"
 
         return reading_and_patient, 201
 
 
-# URI: api/patient/allinfo 
-# [GET]: Get a list of ALL patients and their information (info, readings, referrals) 
+# URI: api/patient/allinfo
+# [GET]: Get a list of ALL patients and their information (info, readings, referrals)
 class PatientAllInformation(Resource):
 
     # get all patient information (patientinfo, readings, and referrals)
     @jwt_required
     def get(self):
         current_user = get_jwt_identity()
-        patients_readings_referrals = patientManager.get_patient_with_referral_and_reading(current_user)
+        patients_readings_referrals = patientManager.get_patient_with_referral_and_reading(
+            current_user
+        )
         # patients_readings_referrals = patientManager.get_patient_with_referral_and_reading()
 
         if not patients_readings_referrals:
--- server/SystemTests/test_patients.py
+++ server/SystemTests/test_patients.py
@@ -3,78 +3,74 @@ import requests
 import json
 import random
 import string
-import uuid 
+import uuid
 from datetime import datetime
 from manage import getRandomInitials
 
+
 def get_login_token():
-    url = 'http://localhost:5000/api/user/auth'
-    payload = {
-        'email': 'admin123@admin.com',
-        'password': 'admin123'
-    }
+    url = "http://localhost:5000/api/user/auth"
+    payload = {"email": "admin123@admin.com", "password": "admin123"}
 
     response = requests.post(url, json=payload)
     resp_json = response.json()
-    return resp_json['token']
+    return resp_json["token"]
+
 
 def get_bearer_token():
-    return 'Bearer {}'.format(get_login_token())
+    return "Bearer {}".format(get_login_token())
+
 
 def get_authorization_header():
-    auth = {
-        "Authorization": get_bearer_token()
-    } 
+    auth = {"Authorization": get_bearer_token()}
     return auth
 
-base_url = 'http://localhost:5000'
+
+base_url = "http://localhost:5000"
+
 
 def getRandomPatientId():
     return str(random.randint(48300000000, 48300099999))
 
+
 def getRandomGender():
-    GENDER_LIST = ['MALE', 'FEMALE']
-    return GENDER_LIST[random.randint(0,1)]
+    GENDER_LIST = ["MALE", "FEMALE"]
+    return GENDER_LIST[random.randint(0, 1)]
+
 
 def getRanomdUUID():
     return str(uuid.uuid4())
 
+
 def getRandomUserId():
-    return random.randint(1,4) 
+    return random.randint(1, 4)
+
 
 def getGreenTrafficLight():
     systolic = 126
     diastolic = 75
     hr = 84
-    return {
-        'bpSystolic': systolic,
-        'bpDiastolic': diastolic,
-        'hr': hr          
-    }
+    return {"bpSystolic": systolic, "bpDiastolic": diastolic, "hr": hr}
+
 
 def getYellowUpTrafficLight():
     systolic = 141
     diastolic = 83
     hr = 50
-    return {
-        'bpSystolic': systolic,
-        'bpDiastolic': diastolic,  
-        'hr': hr        
-    }
+    return {"bpSystolic": systolic, "bpDiastolic": diastolic, "hr": hr}
+
 
 def getRedUpTrafficLight():
     systolic = 112
     diastolic = 130
     hr = 99
-    return {
-        'bpSystolic': systolic,
-        'bpDiastolic': diastolic,  
-        'hr': hr        
-    }
+    return {"bpSystolic": systolic, "bpDiastolic": diastolic, "hr": hr}
+
 
 def getRandomSymtoms():
-    symptoms = ['headache', 'body pain', 'feverish', 'bleeding']
-    return symptoms[random.randint(0,3)]
+    symptoms = ["headache", "body pain", "feverish", "bleeding"]
+    return symptoms[random.randint(0, 3)]
+
 
 auth_header = get_authorization_header()
 ##############################################################
@@ -83,110 +79,110 @@ auth_header = get_authorization_header()
 def test_pass_create_patient():
     patientId = getRandomPatientId()
     patientName = getRandomInitials()
-    patientSex = 'FEMALE'
-    
-    url = base_url + '/api/patient'
+    patientSex = "FEMALE"
+
+    url = base_url + "/api/patient"
 
     data = {
         "patientId": patientId,
         "patientName": patientName,
-        "patientSex": patientSex
+        "patientSex": patientSex,
     }
 
     response = requests.post(url, json=data, headers=auth_header)
     response_body = response.json()
-    
+
     assert response.status_code == 201
-    assert response_body['patientId'] == patientId
-    assert response_body['patientName'] == patientName
-    assert response_body['patientSex'] == patientSex
+    assert response_body["patientId"] == patientId
+    assert response_body["patientName"] == patientName
+    assert response_body["patientSex"] == patientSex
 
 
 def test_pass_create_patient2():
     patientId = getRandomPatientId()
     patientName = getRandomInitials()
-    patientSex = 'MALE'
-    
-    url = base_url + '/api/patient'
+    patientSex = "MALE"
+
+    url = base_url + "/api/patient"
 
     data = {
         "patientId": patientId,
         "patientName": patientName,
-        "patientSex": patientSex
+        "patientSex": patientSex,
     }
 
     response = requests.post(url, json=data, headers=auth_header)
     response_body = json.loads(response.text)
-    
-    assert response.status_code == 201
-    assert response_body['patientId'] == patientId
-    assert response_body['patientName'] == patientName
-    assert response_body['patientSex'] == patientSex
 
-     
+    assert response.status_code == 201
+    assert response_body["patientId"] == patientId
+    assert response_body["patientName"] == patientName
+    assert response_body["patientSex"] == patientSex
+
+
 def test_pass_create_patient_reading():
     patientId = getRandomPatientId()
     patientName = getRandomInitials()
     patientSex = getRandomGender()
     readingId = getRanomdUUID()
-    
+
     vitals = getGreenTrafficLight()
-    bpSystolic = vitals['bpSystolic']
-    bpDiastolic = vitals['bpDiastolic']
-    hr = vitals['hr']
-    dateTimeTaken = int((datetime.now() - datetime(1970,1,1)).total_seconds())
+    bpSystolic = vitals["bpSystolic"]
+    bpDiastolic = vitals["bpDiastolic"]
+    hr = vitals["hr"]
+    dateTimeTaken = int((datetime.now() - datetime(1970, 1, 1)).total_seconds())
     userId = getRandomUserId()
-    
-    url = base_url + '/api/patient/reading'
+
+    url = base_url + "/api/patient/reading"
 
     patient = {
         "patientId": patientId,
         "patientName": patientName,
-        "patientSex": patientSex
+        "patientSex": patientSex,
     }
 
     reading = {
-        'readingId': readingId,
-        'bpSystolic': bpSystolic,
-        'bpDiastolic': bpDiastolic,
-        'heartRateBPM': hr,
-        'dateTimeTaken': dateTimeTaken,
-        'userId': userId,
-        'isFlaggedForFollowup': "false",
-        'symptoms': 'heache'
+        "readingId": readingId,
+        "bpSystolic": bpSystolic,
+        "bpDiastolic": bpDiastolic,
+        "heartRateBPM": hr,
+        "dateTimeTaken": dateTimeTaken,
+        "userId": userId,
+        "isFlaggedForFollowup": "false",
+        "symptoms": "heache",
     }
-    data = {
-        'patient': patient,
-        'reading': reading  
-    }
-    
+    data = {"patient": patient, "reading": reading}
+
     response = requests.post(url, json=data, headers=auth_header)
     response_body = json.loads(response.text)
-    
+
     assert response.status_code == 201
-    assert response_body['patient']['patientId'] == patientId
-    assert response_body['patient']['patientName'] == patientName
-    assert response_body['patient']['patientSex'] == patientSex
-    assert response_body['reading']['bpSystolic'] == bpSystolic
-    assert response_body['reading']['bpDiastolic'] == bpDiastolic
-    assert response_body['reading']['heartRateBPM'] == hr
+    assert response_body["patient"]["patientId"] == patientId
+    assert response_body["patient"]["patientName"] == patientName
+    assert response_body["patient"]["patientSex"] == patientSex
+    assert response_body["reading"]["bpSystolic"] == bpSystolic
+    assert response_body["reading"]["bpDiastolic"] == bpDiastolic
+    assert response_body["reading"]["heartRateBPM"] == hr
+
 
 def test_get_patient():
 
     # hardcoded id 204652 based on deterministic patient data seeded in test database
-    url = base_url + '/api/patient/204652'
+    url = base_url + "/api/patient/204652"
     response = requests.get(url, headers=auth_header)
     response_body = response.json()
-    
+
     # testing the 3 required fields of a patient
-    assert response_body['patientId'] == "204652"
-    assert response_body['patientName'] == "BB"
-    assert response_body['patientSex'] == 'FEMALE'
+    assert response_body["patientId"] == "204652"
+    assert response_body["patientName"] == "BB"
+    assert response_body["patientSex"] == "FEMALE"
     assert response.status_code == 200
+
 
 # ##############################################################
 #                      ERROR CODE 400 TESTS                    #
 # ##############################################################
+
 
 def test_fail_create_patient_reading():
     # should fail because missing some reading information
@@ -194,58 +190,54 @@ def test_fail_create_patient_reading():
     patientName = getRandomInitials()
     patientSex = getRandomGender()
     readingId = getRanomdUUID()
-    
-    url = base_url + '/api/patient/reading'
+
+    url = base_url + "/api/patient/reading"
 
     patient = {
         "patientId": patientId,
         "patientName": patientName,
-        "patientSex": patientSex
+        "patientSex": patientSex,
     }
 
     reading = {
-        'readingId': readingId,
+        "readingId": readingId,
     }
-    data = {
-        'patient': patient,
-        'reading': reading  
-    }
+    data = {"patient": patient, "reading": reading}
 
     response = requests.post(url, json=data, headers=auth_header)
     assert response.status_code == 400
+
 
 def test_fail_create_patient_duplicate():
     # should fail because we are creating a duplicate patient
 
-    patientId = getRandomPatientId() 
+    patientId = getRandomPatientId()
     patientName = getRandomInitials()
-    patientSex = 'MALE'
-    
-    url = base_url + '/api/patient'
+    patientSex = "MALE"
+
+    url = base_url + "/api/patient"
 
     data = {
         "patientId": patientId,
         "patientName": patientName,
-        "patientSex": patientSex
+        "patientSex": patientSex,
     }
 
     response = requests.post(url, json=data, headers=auth_header)
     response = requests.post(url, json=data, headers=auth_header)
 
     assert response.status_code == 400
+
 
 def test_fail_create_patient_missing_fields():
     # should fail because we are missing patientName
 
     patientId = getRandomPatientId()
-    patientSex = 'FEMALE'
-    
-    url = base_url + '/api/patient'
+    patientSex = "FEMALE"
 
-    data = {
-        "patientId": patientId,
-        "patientSex": patientSex
-    }
+    url = base_url + "/api/patient"
+
+    data = {"patientId": patientId, "patientSex": patientSex}
 
     response = requests.post(url, json=data, headers=auth_header)
     assert response.status_code == 400
