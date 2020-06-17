3851d9ff688566cb561f4be356dabebf1125f598
iamwaswa <wolunga@sfu.ca>
2020-06-16 23:39:06 -0700

    Enabled sorting on all fields for the referral table

--- client/src/newStructure/pages/referrals/referralTable/index.tsx
+++ client/src/newStructure/pages/referrals/referralTable/index.tsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
+import { assessment, dateReferred } from './utils';
+import { initials, patientId, village, vitalSign } from '../../../shared/components/table/columns';
+
 import MaterialTable from 'material-table';
-import { Icon } from 'semantic-ui-react';
-import { getLatestReferral, sortReferralsByDate } from './utils';
-import {
-  getTrafficIcon,
-  getLatestReading,
-  getPrettyDate,
-} from '../../../shared/utils';
+
 interface IProps {
   callbackFromParent: any;
   data: any;
@@ -18,60 +15,16 @@ interface IState {
   data: any;
   selectedPatient: any;
 }
+
 export class ReferralTable extends Component<IProps, IState> {
   state = {
     columns: [
-      {
-        title: 'Patient Initials',
-        field: 'patientName',
-        render: (rowData: any) => (
-          <p
-            key={'initials' + rowData.tableData.id}
-            style={{
-              fontSize: '200%',
-              fontWeight: 'bold',
-              textAlign: 'center',
-            }}>
-            {rowData.patientName}
-          </p>
-        ),
-        headerStyle: {
-          textAlign: 'center',
-        },
-        sorting: false,
-      },
-      { title: 'Patient ID', field: 'patientId' },
-      { title: 'Village', field: 'villageNumber' },
-      {
-        title: 'Vital Sign',
-        cellStyle: {
-          padding: '0px',
-        },
-        sorting: false,
-        render: (rowData: any) =>
-          getTrafficIcon(getLatestReading(rowData.readings).trafficLightStatus),
-      },
-      {
-        title: 'Date Referred',
-        render: (rowData: any) => (
-          <p>{getPrettyDate(getLatestReferral(rowData.readings))}</p>
-        ),
-        customSort: (a: any, b: any) => sortReferralsByDate(a, b),
-        defaultSort: 'asc',
-      },
-      {
-        title: 'Assessment',
-        render: (rowData: any) =>
-          rowData.needsAssessment ? (
-            <p>
-              <Icon name="clock outline" size="large" color="red" /> Pending
-            </p>
-          ) : (
-            <p>
-              <Icon name="checkmark" size="large" color="green" /> Complete
-            </p>
-          ),
-      },
+      initials,
+      patientId,
+      village,
+      vitalSign,
+      dateReferred,
+      assessment,
     ],
     data: [],
     selectedPatient: {
