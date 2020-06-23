efa3d607f038600fab65dc98f694f7976ef0029b
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2020-06-23 01:24:09 -0700

    CRDL-66 added pop up

--- client/src/newStructure/pages/statistics/covidCollection/index.tsx
+++ client/src/newStructure/pages/statistics/covidCollection/index.tsx
@@ -10,6 +10,13 @@ import {
   InputLabel,
   Input,
   Select,
+  Dialog ,
+  DialogActions,
+  DialogContent ,
+  DialogContentText ,
+  DialogTitle ,
+  TextField,
+  Button
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -33,16 +40,6 @@ const Page: React.FC<any> = (props) => {
     { title: 'Dob', field: 'birthYear', type: 'numeric' },
     { title: 'age', field: 'birthYear', type: 'numeric' },
     {
-      title: 'Transfer',
-      field: 'imageUrlTransfer',
-      render: (rowData: any) => (
-        <img
-          src={rowData.imageUrlTransfer}
-          style={{ width: 40, borderRadius: '50%' }}
-        />
-      ),
-    },
-    {
       title: 'Status',
       field: 'imageUrlStatus',
       render: (rowData: any) => (
@@ -54,14 +51,12 @@ const Page: React.FC<any> = (props) => {
     },
   ]);
 
-  const [data, setData] = React.useState([
+  const [data] = React.useState<any>([
     {
       id: '111222',
       name: 'Kabab I am Suspected',
       birthYear: 1987,
       birthCity: 63,
-      imageUrlTransfer:
-        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
       imageUrlStatus:
         'https://lh3.googleusercontent.com/proxy/iYC9_Nk1Qlh3y9zfTj6KpsD8Bi-zTTFvobKjjOH8XQglq1dNuTX12PkqsqIBHNGgkJgslpx5yjwR1lbl2p9cR7CMRcr5NKc',
     },
@@ -70,8 +65,6 @@ const Page: React.FC<any> = (props) => {
       name: 'Kabab I am Recovered',
       birthYear: 2017,
       birthCity: 34,
-      imageUrlTransfer:
-        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
       imageUrlStatus:
         'https://cdn3.vectorstock.com/i/1000x1000/09/82/check-icon-vector-10850982.jpg',
     },
@@ -80,8 +73,6 @@ const Page: React.FC<any> = (props) => {
       name: 'Kabab I am Dead',
       birthYear: 2017,
       birthCity: 34,
-      imageUrlTransfer:
-        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
       imageUrlStatus:
         'https://media.istockphoto.com/vectors/red-x-mark-icon-cross-symbol-vector-id692279886?k=6&m=692279886&s=170667a&w=0&h=PkTBYGVXGx0evEEjCMePs5suU12XTqUSlqaQdll87o0=',
     },
@@ -90,8 +81,6 @@ const Page: React.FC<any> = (props) => {
       name: 'Kabab I am Tested Positive',
       birthYear: 2017,
       birthCity: 34,
-      imageUrlTransfer:
-        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
       imageUrlStatus:
         'https://storage.needpix.com/rsynced_images/blood-2667009_1280.png',
     },
@@ -100,8 +89,6 @@ const Page: React.FC<any> = (props) => {
       name: 'Kabab I am Tested Negative',
       birthYear: 2017,
       birthCity: 34,
-      imageUrlTransfer:
-        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
       imageUrlStatus:
         'https://storage.needpix.com/rsynced_images/blood-2667006_1280.png',
     },
@@ -110,6 +97,15 @@ const Page: React.FC<any> = (props) => {
     event: React.ChangeEvent<{ value: unknown }>
   ) => {
     setHouseHoldNum(event.target.value as string);
+  };
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   const handleChangeTotalNumber = (
@@ -166,40 +162,58 @@ const Page: React.FC<any> = (props) => {
           title="People"
           columns={columns}
           data={data}
-          editable={{
-            onRowAdd: (newData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  setData([...data, newData]);
+          options={{
+            headerStyle: {
+              marginLeft: 'auto',
+              paddingRight: '0px !important',
+              paddingLeft: '0px !important',
+            },
+            actionsColumnIndex: -1
 
-                  resolve();
-                }, 1000);
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  const dataUpdate = [...data];
-                  const index = oldData?.id ?? 0;
-                  dataUpdate[+index] = newData;
-                  setData([...dataUpdate]);
-
-                  resolve();
-                }, 1000);
-              }),
-            onRowDelete: (oldData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  const dataDelete = [...data];
-                  const index = oldData?.id ?? 0;
-                  dataDelete.splice(+index, 1);
-                  setData([...dataDelete]);
-
-                  resolve();
-                }, 1000);
-              }),
           }}
+          actions={[
+            {
+              icon: 'edit',
+              tooltip: 'Save User',
+              onClick: (event, rowData) => handleClickOpen
+            },
+            {
+              icon: 'delete',
+              tooltip: 'Delete User',
+              onClick: (event, rowData) => {}
+            }
+          ]}
         />
+
       </Paper>
+      <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address
+              here. We will send updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
     </div>
   );
 };
