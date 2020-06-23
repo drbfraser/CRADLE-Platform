04824f830bc86b5a470d7838779ec515ea5d64bc
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2020-06-23 00:58:44 -0700

    CRDL-66 added list design one

--- client/src/newStructure/pages/statistics/covidCollection/index.tsx
+++ client/src/newStructure/pages/statistics/covidCollection/index.tsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { ReduxState } from '../../../redux/rootReducer';
 import MaterialTable from 'material-table';
-
 import {
   Divider,
   Paper,
@@ -28,7 +27,85 @@ const Page: React.FC<any> = (props) => {
   const classes = useStyles();
   const [houseHoldNum, setHouseHoldNum] = React.useState('');
   const [totalPeople, setTotalPeople] = React.useState('10');
+  const [columns] = React.useState<any>([
+    { title: 'Id', field: 'id' },
+    { title: 'Name', field: 'name' },
+    { title: 'Dob', field: 'birthYear', type: 'numeric' },
+    { title: 'age', field: 'birthYear', type: 'numeric' },
+    {
+      title: 'Transfer',
+      field: 'imageUrlTransfer',
+      render: (rowData: any) => (
+        <img
+          src={rowData.imageUrlTransfer}
+          style={{ width: 40, borderRadius: '50%' }}
+        />
+      ),
+    },
+    {
+      title: 'Status',
+      field: 'imageUrlStatus',
+      render: (rowData: any) => (
+        <img
+          src={rowData.imageUrlStatus}
+          style={{ width: 40, borderRadius: '50%' }}
+        />
+      ),
+    },
+  ]);
 
+  const [data, setData] = React.useState([
+    {
+      id: '111222',
+      name: 'Kabab I am Suspected',
+      birthYear: 1987,
+      birthCity: 63,
+      imageUrlTransfer:
+        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
+      imageUrlStatus:
+        'https://lh3.googleusercontent.com/proxy/iYC9_Nk1Qlh3y9zfTj6KpsD8Bi-zTTFvobKjjOH8XQglq1dNuTX12PkqsqIBHNGgkJgslpx5yjwR1lbl2p9cR7CMRcr5NKc',
+    },
+    {
+      id: '45454',
+      name: 'Kabab I am Recovered',
+      birthYear: 2017,
+      birthCity: 34,
+      imageUrlTransfer:
+        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
+      imageUrlStatus:
+        'https://cdn3.vectorstock.com/i/1000x1000/09/82/check-icon-vector-10850982.jpg',
+    },
+    {
+      id: '45454',
+      name: 'Kabab I am Dead',
+      birthYear: 2017,
+      birthCity: 34,
+      imageUrlTransfer:
+        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
+      imageUrlStatus:
+        'https://media.istockphoto.com/vectors/red-x-mark-icon-cross-symbol-vector-id692279886?k=6&m=692279886&s=170667a&w=0&h=PkTBYGVXGx0evEEjCMePs5suU12XTqUSlqaQdll87o0=',
+    },
+    {
+      id: '45454',
+      name: 'Kabab I am Tested Positive',
+      birthYear: 2017,
+      birthCity: 34,
+      imageUrlTransfer:
+        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
+      imageUrlStatus:
+        'https://storage.needpix.com/rsynced_images/blood-2667009_1280.png',
+    },
+    {
+      id: '45454',
+      name: 'Kabab I am Tested Negative',
+      birthYear: 2017,
+      birthCity: 34,
+      imageUrlTransfer:
+        'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
+      imageUrlStatus:
+        'https://storage.needpix.com/rsynced_images/blood-2667006_1280.png',
+    },
+  ]);
   const handleChangeHouseHold = (
     event: React.ChangeEvent<{ value: unknown }>
   ) => {
@@ -86,101 +163,41 @@ const Page: React.FC<any> = (props) => {
         </form>
 
         <MaterialTable
-          columns={[
-            { title: 'Id', field: 'id' },
-            { title: 'Name', field: 'name' },
-            { title: 'Dob', field: 'birthYear', type: 'numeric' },
-            { title: 'age', field: 'birthYear', type: 'numeric' },
-            {
-              title: 'Transfer',
-              field: 'imageUrlTransfer',
-              render: (rowData) => (
-                <img
-                  src={rowData.imageUrlTransfer}
-                  style={{ width: 40, borderRadius: '50%' }}
-                />
-              ),
-            },
-            {
-              title: 'Status',
-              field: 'imageUrlStatus',
-              render: (rowData) => (
-                <img
-                  src={rowData.imageUrlStatus}
-                  style={{ width: 40, borderRadius: '50%' }}
-                />
-              ),
-            },
-          ]}
-          actions={[
-            {
-              icon: 'save',
-              tooltip: 'Save User',
-              onClick: (event, rowData) => {alert("You saved " )}
-              
-            },
-            {
-              icon: 'save',
-              tooltip: 'Save User',
-              onClick: (event, rowData) => {alert("You saved " )}
-            },
-          ]}
-          options={{
-            actionsColumnIndex: -1
+          title="People"
+          columns={columns}
+          data={data}
+          editable={{
+            onRowAdd: (newData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  setData([...data, newData]);
+
+                  resolve();
+                }, 1000);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  const dataUpdate = [...data];
+                  const index = oldData?.id ?? 0;
+                  dataUpdate[+index] = newData;
+                  setData([...dataUpdate]);
+
+                  resolve();
+                }, 1000);
+              }),
+            onRowDelete: (oldData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  const dataDelete = [...data];
+                  const index = oldData?.id ?? 0;
+                  dataDelete.splice(+index, 1);
+                  setData([...dataDelete]);
+
+                  resolve();
+                }, 1000);
+              }),
           }}
-          title="Reports"
-          data={[
-            {
-              id: '111222',
-              name: 'Kabab I am Suspected',
-              birthYear: 1987,
-              birthCity: 63,
-              imageUrlTransfer:
-                'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
-              imageUrlStatus:
-                'https://lh3.googleusercontent.com/proxy/iYC9_Nk1Qlh3y9zfTj6KpsD8Bi-zTTFvobKjjOH8XQglq1dNuTX12PkqsqIBHNGgkJgslpx5yjwR1lbl2p9cR7CMRcr5NKc',
-            },
-            {
-              id: '45454',
-              name: 'Kabab I am Recovered',
-              birthYear: 2017,
-              birthCity: 34,
-              imageUrlTransfer:
-                'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
-              imageUrlStatus:
-                'https://cdn3.vectorstock.com/i/1000x1000/09/82/check-icon-vector-10850982.jpg',
-            },
-            {
-              id: '45454',
-              name: 'Kabab I am Dead',
-              birthYear: 2017,
-              birthCity: 34,
-              imageUrlTransfer:
-                'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
-              imageUrlStatus:
-                'https://media.istockphoto.com/vectors/red-x-mark-icon-cross-symbol-vector-id692279886?k=6&m=692279886&s=170667a&w=0&h=PkTBYGVXGx0evEEjCMePs5suU12XTqUSlqaQdll87o0=',
-            },
-            {
-              id: '45454',
-              name: 'Kabab I am Tested Positive',
-              birthYear: 2017,
-              birthCity: 34,
-              imageUrlTransfer:
-                'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
-              imageUrlStatus:
-                'https://storage.needpix.com/rsynced_images/blood-2667009_1280.png',
-            },
-            {
-              id: '45454',
-              name: 'Kabab I am Tested Negative',
-              birthYear: 2017,
-              birthCity: 34,
-              imageUrlTransfer:
-                'https://www.kindpng.com/picc/m/745-7450719_right-clipart-colorful-arrow-transparent-arrow-png-yellow.png',
-              imageUrlStatus:
-                'https://storage.needpix.com/rsynced_images/blood-2667006_1280.png',
-            },
-          ]}
         />
       </Paper>
     </div>
