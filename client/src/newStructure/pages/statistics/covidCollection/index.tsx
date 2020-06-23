094eb227ebcb5ea01aae81f1aebb157dd6f65754
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2020-06-22 22:46:48 -0700

    CRDL-66 added page paper

--- client/src/newStructure/pages/statistics/covidCollection/index.tsx
+++ client/src/newStructure/pages/statistics/covidCollection/index.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { ReduxState } from '../../../redux/rootReducer';
-
+import {Divider, Paper} from '@material-ui/core';
 
 const Page: React.FC<any> = (props) => {
-
   return (
-    <div>Covid 19 Data Collection</div>
+    <div
+      style={{
+        maxWidth: 1200,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      }}>
+      <h1>
+        <b>Covid 19 Data Collection</b>
+      </h1>
+      <Divider></Divider>
+      <Paper style={{ padding: '35px 25px', borderRadius: '15px' }}></Paper>
+
+      
+    </div>
   );
-}
+};
 
+const mapStateToProps = ({ patients }: ReduxState) => ({});
 
-
-
-
-const mapStateToProps = ({ patients }: ReduxState) => ({
-
-});
-
-const mapDispatchToProps = (dispatch: any) => ({
-  
-});
+const mapDispatchToProps = (dispatch: any) => ({});
 export const CovidCollection = connect(
   mapStateToProps,
   mapDispatchToProps
