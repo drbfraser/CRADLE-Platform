1c3ad39e03a58bfcefd1524a7b6892441af89cfa
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2020-07-01 13:42:26 -0700

    CRDL-75 seperated component

--- client/src/newStructure/pages/newReading/newReadingCovid/index.tsx
+++ client/src/newStructure/pages/newReading/newReadingCovid/index.tsx
@@ -5,7 +5,7 @@ import {
   FormControl,
   Input,
   InputLabel,
-  Select,
+  // Select,
   FormControlLabel,
   Checkbox,
   Paper,
@@ -19,7 +19,7 @@ import {
 } from '@material-ui/core';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { UrineTestForm } from '../urineTestForm';
-
+import {Demographics } from './demographic'
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -104,100 +104,7 @@ const Page: React.FC<any> = () => {
         ))}
       </Stepper>
       {activeStep === 0 ? (
-        <Paper
-          style={{
-            padding: '35px 25px',
-            marginTop: '2%',
-            borderRadius: '15px',
-          }}>
-          <h1>
-            <b>Collect Basic Demographic</b>
-          </h1>
-
-          <form className={classes.root} noValidate autoComplete="off">
-            <FormControl className={classes.formField}>
-              <InputLabel required htmlFor="component-simple">
-                Patient Initials
-              </InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel required htmlFor="component-outlined">
-                ID
-              </InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel htmlFor="component-outlined">Age</InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel htmlFor="component-outlined">Birthday</InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel required htmlFor="component-outlined">
-                Gender
-              </InputLabel>
-              <Select
-                native
-                labelId="demo-simple-select-label"
-                id="demo-simple-select">
-                <option value={'10'}>Male</option>
-                <option value={'20'}>Female</option>
-                <option value={'30'}>Other</option>
-              </Select>
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <FormControlLabel
-                control={<Checkbox checked={true} name="Pregnant" />}
-                label="Pregnant"
-              />
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel htmlFor="component-outlined">
-                House Hold Number
-              </InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel required htmlFor="component-outlined">
-                Gestational Age
-              </InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel required htmlFor="component-outlined">
-                Gestational Age Unit
-              </InputLabel>
-              <Select
-                native
-                labelId="demo-simple-select-label"
-                id="demo-simple-select">
-                <option value={'10'}>Weeks</option>
-                <option value={'20'}>Months</option>
-              </Select>
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel htmlFor="component-outlined">Zone</InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formField}>
-              <InputLabel htmlFor="component-outlined">Village</InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formFieldDM}>
-              <InputLabel htmlFor="component-outlined">Drug History</InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-            <FormControl className={classes.formFieldDM}>
-              <InputLabel htmlFor="component-outlined">
-                Medical History
-              </InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-          </form>
-        </Paper>
+        <Demographics></Demographics>
       ) : (
         ''
       )}
