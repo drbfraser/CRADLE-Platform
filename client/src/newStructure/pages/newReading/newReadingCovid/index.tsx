f40aedf3afd85714a23bbb30587d4d869351ba60
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2020-07-01 18:05:12 -0700

    CRDL-75 seperated sysmpotoms tab

--- client/src/newStructure/pages/newReading/newReadingCovid/index.tsx
+++ client/src/newStructure/pages/newReading/newReadingCovid/index.tsx
@@ -5,12 +5,7 @@ import {
   FormControl,
   Input,
   InputLabel,
-  // Select,
-  FormControlLabel,
-  Checkbox,
   Paper,
-  FormGroup,
-  FormLabel,
   Stepper,
   Step,
   StepLabel,
@@ -20,6 +15,7 @@ import {
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { UrineTestForm } from '../urineTestForm';
 import { Demographics } from './demographic';
+import { Symptoms } from './symptoms';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -104,97 +100,7 @@ const Page: React.FC<any> = () => {
         ))}
       </Stepper>
       {activeStep === 0 ? <Demographics></Demographics> : ''}
-      {activeStep === 1 ? (
-        <Paper
-          style={{
-            padding: '35px 25px',
-            marginTop: '2%',
-            borderRadius: '15px',
-          }}>
-          <h1>
-            <b>Collect Symptoms</b>
-          </h1>
-
-          <FormControl component="fieldset" className={classes.formControl}>
-            <FormLabel component="legend">Regular</FormLabel>
-            <FormGroup>
-              <FormControlLabel
-                control={<Checkbox checked={true} name="gilad" />}
-                label="None (Patient healthy)"
-              />
-              <FormControlLabel
-                control={<Checkbox checked={false} name="jason" />}
-                label="Headache"
-              />
-              <FormControlLabel
-                control={<Checkbox checked={true} name="antoine" />}
-                label="Blurred Vission"
-              />
-              <FormControlLabel
-                control={<Checkbox checked={true} name="antoine" />}
-                label="Abdominal Pain"
-              />
-            </FormGroup>
-            <FormControlLabel
-              control={<Checkbox checked={true} name="gilad" />}
-              label="Bleeding"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={false} name="jason" />}
-              label="Feverish"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={true} name="antoine" />}
-              label="Unwell"
-            />
-          </FormControl>
-          <FormControl component="fieldset" className={classes.formControl}>
-            <FormLabel component="legend">Covid</FormLabel>
-            <FormGroup>
-              <FormControlLabel
-                control={<Checkbox checked={false} name="gilad" />}
-                label="Cough"
-              />
-              <FormControlLabel
-                control={<Checkbox checked={false} name="jason" />}
-                label="Shortness of Breath"
-              />
-              <FormControlLabel
-                control={<Checkbox checked={true} name="antoine" />}
-                label="Sore Throat"
-              />
-              <FormControlLabel
-                control={<Checkbox checked={true} name="antoine" />}
-                label="Muscle ache"
-              />
-            </FormGroup>
-            <FormControlLabel
-              control={<Checkbox checked={true} name="gilad" />}
-              label="Fatigue"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={false} name="jason" />}
-              label="Loss of sense"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={false} name="jason" />}
-              label="Loss of taste"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={false} name="jason" />}
-              label="Loss of smell"
-            />
-          </FormControl>
-          <FormControl component="fieldset" className={classes.formControl}>
-            <FormControl className={classes.formFieldDM}>
-              <InputLabel htmlFor="component-outlined">Others</InputLabel>
-              <Input id="component-outlined" />
-            </FormControl>
-          </FormControl>
-        </Paper>
-      ) : (
-        ''
-      )}
+      {activeStep === 1 ? <Symptoms></Symptoms> : ''}
       {activeStep === 2 ? (
         <div>
           <Paper
---
+++ client/src/newStructure/pages/newReading/newReadingCovid/symptoms/index.tsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  FormControl,
+  Input,
+  InputLabel,
+  FormControlLabel,
+  Checkbox,
+  Paper,
+  FormGroup,
+  FormLabel,
+} from '@material-ui/core';
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(2),
+      },
+    },
+    formField: {
+      margin: theme.spacing(2),
+      minWidth: '22ch',
+    },
+    formFieldDM: {
+      margin: theme.spacing(2),
+      minWidth: '48ch',
+      minHeight: '15ch',
+    },
+    formControl: {
+      margin: theme.spacing(3),
+    },
+  })
+);
+
+const Page: React.FC<any> = () => {
+  const classes = useStyles();
+  return (
+    <Paper
+      style={{
+        padding: '35px 25px',
+        marginTop: '2%',
+        borderRadius: '15px',
+      }}>
+      <h1>
+        <b>Collect Symptoms</b>
+      </h1>
+
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Regular</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={true} name="gilad" />}
+            label="None (Patient healthy)"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={false} name="jason" />}
+            label="Headache"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={true} name="antoine" />}
+            label="Blurred Vission"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={true} name="antoine" />}
+            label="Abdominal Pain"
+          />
+        </FormGroup>
+        <FormControlLabel
+          control={<Checkbox checked={true} name="gilad" />}
+          label="Bleeding"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={false} name="jason" />}
+          label="Feverish"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={true} name="antoine" />}
+          label="Unwell"
+        />
+      </FormControl>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Covid</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={false} name="gilad" />}
+            label="Cough"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={false} name="jason" />}
+            label="Shortness of Breath"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={true} name="antoine" />}
+            label="Sore Throat"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={true} name="antoine" />}
+            label="Muscle ache"
+          />
+        </FormGroup>
+        <FormControlLabel
+          control={<Checkbox checked={true} name="gilad" />}
+          label="Fatigue"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={false} name="jason" />}
+          label="Loss of sense"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={false} name="jason" />}
+          label="Loss of taste"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={false} name="jason" />}
+          label="Loss of smell"
+        />
+      </FormControl>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormControl className={classes.formFieldDM}>
+          <InputLabel htmlFor="component-outlined">Others</InputLabel>
+          <Input id="component-outlined" />
+        </FormControl>
+      </FormControl>
+    </Paper>
+  );
+};
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = () => ({});
+export const Symptoms = connect(mapStateToProps, mapDispatchToProps)(Page);
