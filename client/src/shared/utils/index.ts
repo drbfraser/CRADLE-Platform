c0ea7cb244f76726bea1f21fc1679cd6d2848812
lza132 <zhouliyang1996@outlook.com>
2020-09-25 02:43:00 -0700

    Only have the date format 'YYYY-MM-DD'

--- client/src/shared/utils/index.ts
+++ client/src/shared/utils/index.ts
@@ -119,17 +119,17 @@ export const getMomentDate = (dateS: OrNull<number>): moment.Moment => {
 export const getPrettyDate = (dateStr: number): string => {
   // * Date comes in from the backend in seconds
   // * Moment date requires milliseconds
-  return getMomentDate(dateStr * 1000).format('MMMM Do YYYY');
+  return getMomentDate(dateStr * 1000).format('YYYY-MM-DD');
 };
 
 export const getPrettyDateTime = (dateStr: number): string => {
   // * Date comes in from the backend in seconds
   // * Moment date requires milliseconds
-  return getMomentDate(dateStr * 1000).format('MMMM Do YYYY, h:mm:ss a');
+  return getMomentDate(dateStr * 1000).format('YYYY-MM-DD');
 };
 
 export const getPrettyDateYYYYmmDD = (dateStr: string): string => {
-  return moment(dateStr).format('MMMM Do YYYY');
+  return moment(dateStr).format('YYYY-MM-DD');
 };
 
 export const getLatestReading = (readings: Array<Reading>): Reading => {
