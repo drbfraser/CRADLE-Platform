a3b723c183927a3b5abb4eccc6479b49b01bedc3
Faraz Fazlalizadeh <FFazlalizadeh@absolute.com>
2020-10-21 20:04:37 -0700

    CRDL-192 applied formatting

--- server/data/crud.py
+++ server/data/crud.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import joinedload
 M = TypeVar("M")
 
 
-
 def create(model: M, refresh=False):
     """
     Inserts a new model into the database.
@@ -44,7 +43,6 @@ def read(m: Type[M], **kwargs) -> Optional[M]:
     return m.query.filter_by(**kwargs).one_or_none()
 
 
-
 def read_all(m: Type[M], **kwargs) -> List[M]:
     """
     Queries the database for all models which match some query parameters defined as
@@ -66,7 +64,6 @@ def read_all(m: Type[M], **kwargs) -> List[M]:
             return m.query.options(joinedload(m.reading)).all()
 
         return m.query.options(joinedload(m.reading)).filter_by(**kwargs).all()
-
 
 
 def update(m: Type[M], changes: dict, **kwargs):
