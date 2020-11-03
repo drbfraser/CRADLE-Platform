510302e191358c5aded492e19c6e7bdc0e510cab
Faraz Fazlalizadeh <FFazlalizadeh@absolute.com>
2020-11-02 20:29:19 -0800

    CRDL-208 updated query Co-Author: Ethan

--- server/service/serialize.py
+++ server/service/serialize.py
@@ -10,8 +10,8 @@ def serialize_patient(p: any):
         "patientId": p.patientId,
         "patientName": p.patientName,
         "villageNumber": p.villageNumber,
-        "trafficLightStatus": p.trafficLightStatus,
-        "dateTimeTaken": p.dateTimeTaken,
+        "trafficLightStatus": p.trafficLightStatus if not "null" else "",
+        "dateTimeTaken": p.dateTimeTaken if not "null" else "",
     }
     return data
 
