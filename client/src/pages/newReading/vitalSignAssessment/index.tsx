352246aedde945ef6a9984d77f1e5cfa732fdc27
LIYANG ZHOU <liyang_zhou_2@sfu.ca>
2020-11-16 19:21:14 -0800

    Apply 1 suggestion(s) to 1 file(s)

--- client/src/pages/newReading/vitalSignAssessment/index.tsx
+++ client/src/pages/newReading/vitalSignAssessment/index.tsx
@@ -175,7 +175,7 @@ const Page: React.FC<IProps> = (props) => {
               type="number"
               helperText={
                 props.vitals.temperatureError
-                  ? 'Must be an integer between 34 - 45.'
+                  ? 'Must be an integer between 30 - 45.'
                   : ''
               }
             />
