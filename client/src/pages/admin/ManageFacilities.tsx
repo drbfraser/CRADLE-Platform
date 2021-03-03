c2c561980bb53dada7238743cd01dc40a5374e39
Ethan <ehinchli@sfu.ca>
2021-03-02 17:32:40 -0800

    Add buttons and error handling to admin facilities table

--- client/src/pages/admin/ManageFacilities.tsx
+++ client/src/pages/admin/ManageFacilities.tsx
@@ -3,56 +3,144 @@ import { apiFetch } from 'src/shared/utils/api';
 import { BASE_URL } from 'src/server/utils';
 import { EndpointEnum } from 'src/server';
 import MUIDataTable from 'mui-datatables';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, IconButton, Tooltip } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import CreateIcon from '@material-ui/icons/Create';
+import { Toast } from 'src/shared/components/toast';
 
 interface IFacility {
-    about: string;
-    facilityType: string;
-    healthFacilityName: string;
-    healthFacilityPhoneNumber: string;
-    location: string;
+  about: string;
+  facilityType: string;
+  healthFacilityName: string;
+  healthFacilityPhoneNumber: string;
+  location: string;
 }
 
-const columns = ["Facility Name", "Phone Number", "Location"];
+const columns = [
+  'Facility Name',
+  'Phone Number',
+  'Location',
+  {
+    name: 'Take Action',
+    options: {
+      sort: false,
+    },
+  },
+];
 
 export const ManageFacilities = () => {
-    const [facilities, setFacilities] = useState<IFacility[]>([]);
-    const [data, setData] = useState<string[][]>([]);
+  const styles = useStyles();
+  const [errorLoading, setErrorLoading] = useState(false);
+  const [facilities, setFacilities] = useState<IFacility[]>([]);
+  const [data, setData] = useState<(string | number)[][]>([]);
 
-    useEffect(() => {
-        const getFacilities = async () => {
-            try {
-                const resp: IFacility[] = await (await apiFetch(BASE_URL + EndpointEnum.HEALTH_FACILITIES)).json();
-                setFacilities(resp);
-            }
-            catch(e) {
-                alert("ERROR");
-            }
-        }
+  useEffect(() => {
+    const getFacilities = async () => {
+      try {
+        const resp: IFacility[] = await (
+          await apiFetch(BASE_URL + EndpointEnum.HEALTH_FACILITIES)
+        ).json();
+        setFacilities(resp);
+      } catch (e) {
+        setErrorLoading(true);
+      }
+    };
 
-        getFacilities();
-    }, []);
+    getFacilities();
+  }, []);
 
-    useEffect(() => {
-        const rows = facilities.map(f => [f.healthFacilityName, f.healthFacilityPhoneNumber, f.location]);
-        setData(rows);
-    }, [facilities]);
+  useEffect(() => {
+    const rows = facilities.map((f, idx) => [
+      f.healthFacilityName,
+      f.healthFacilityPhoneNumber,
+      f.location,
+      idx,
+    ]);
+    setData(rows);
+  }, [facilities]);
+
+  const CreateFacilityButton = () => (
+    <Button
+      className={styles.button}
+      color="primary"
+      variant="contained"
+      size="large"
+      onClick={() => alert('create')}>
+      <AddIcon />
+      New Facility
+    </Button>
+  );
+
+  const Row = ({ row }: { row: (string | number)[] }) => {
+    const cells = row.slice(0, -1);
+    const facility = facilities[row.slice(-1)[0] as number];
 
     return (
-        <>
-            <MUIDataTable
-                title="Health Care Facilities"
-                columns={columns}
-                data={data}
-                options={{
-                    search: false,
-                    download: false,
-                    print: false,
-                    viewColumns: false,
-                    filter: false,
-                    selectToolbarPlacement: "none",
-                    selectableRows: "none",
-                }}
-            />
-        </>
-    )
-}
+      <tr className={styles.row}>
+        {cells.map((item, i) => (
+          <td className={styles.cell} key={i}>
+            {item}
+          </td>
+        ))}
+        <td className={styles.cell}>
+          <Tooltip placement="top" title="Edit Facility">
+            <IconButton onClick={() => alert(facility.healthFacilityName)}>
+              <CreateIcon />
+            </IconButton>
+          </Tooltip>
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className={styles.tableContainer}>
+      {errorLoading && (
+        <Toast
+          status="error"
+          message="Something went wrong loading the health care facilities. Please try again."
+          clearMessage={() => setErrorLoading(false)}
+        />
+      )}
+      <MUIDataTable
+        title="Health Care Facilities"
+        columns={columns}
+        data={data}
+        options={{
+          search: false,
+          download: false,
+          print: false,
+          viewColumns: false,
+          filter: false,
+          selectToolbarPlacement: 'none',
+          selectableRows: 'none',
+          rowHover: false,
+          responsive: 'standard',
+          customToolbar: () => <CreateFacilityButton />,
+          customRowRender: (row, i) => <Row key={i} row={row} />,
+        }}
+      />
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  tableContainer: {
+    '& .MuiTableCell-head': {
+      fontWeight: 'bold',
+    },
+    '& .MuiTableSortLabel-icon': {
+      marginTop: 15,
+    },
+  },
+  row: {
+    borderBottom: '1px solid #ddd',
+  },
+  cell: {
+    padding: '4px 16px',
+  },
+  button: {
+    height: '100%',
+  },
+});
--- client/src/pages/admin/index.tsx
+++ client/src/pages/admin/index.tsx
@@ -4,44 +4,45 @@ import { ManageUsers } from './ManageUsers';
 import { ManageFacilities } from './ManageFacilities';
 
 const pageOptions = [
-    {
-        name: 'Users',
-        component: ManageUsers
-    },
-    {
-        name: 'Health Care Failities',
-        component: ManageFacilities
-    }
+  {
+    name: 'Users',
+    component: ManageUsers,
+  },
+  {
+    name: 'Health Care Failities',
+    component: ManageFacilities,
+  },
 ];
 
 export const AdminPage = () => {
-    const styles = useStyles();
-    const [page, setPage] = useState(0);
-    const PageComponent = pageOptions[page].component;
+  const styles = useStyles();
+  const [page, setPage] = useState(0);
+  const PageComponent = pageOptions[page].component;
 
-    return (
-        <>
-            <h3>
-                Manage &nbsp;
-                <Select
-                    className={styles.manageDropdown}
-                    value={page}
-                    onChange={(e) => setPage(parseInt(e.target.value as string))}
-                    displayEmpty
-                >
-                    {
-                        pageOptions.map((option, i) => <MenuItem value={i}>{option.name}</MenuItem>)
-                    }
-                </Select>
-            </h3>
-            <PageComponent />
-        </>
-    )
-}
+  return (
+    <>
+      <h3>
+        Manage &nbsp;
+        <Select
+          className={styles.manageDropdown}
+          value={page}
+          onChange={(e) => setPage(parseInt(e.target.value as string))}
+          displayEmpty>
+          {pageOptions.map((option, i) => (
+            <MenuItem key={i} value={i}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </h3>
+      <PageComponent />
+    </>
+  );
+};
 
 const useStyles = makeStyles({
-    manageDropdown: {
-        fontWeight: 600,
-        fontSize: 20,
-    }
-})
+  manageDropdown: {
+    fontWeight: 600,
+    fontSize: 20,
+  },
+});
