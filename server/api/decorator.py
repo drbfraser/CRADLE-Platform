11715c9967472fe52762ac25a5d61122abaa9574
Dev <dev_arora@sfu.ca>
2021-03-06 12:19:47 -0800

    Added two comments have to run black again

--- server/api/decorator.py
+++ server/api/decorator.py
@@ -9,25 +9,26 @@ from flask_jwt_extended import (
     get_jwt_claims,
 )
 
+
 def roles_required(listOfRoles):
     def wrapper(fn):
         @wraps(fn)
         def decorator(*args, **kwargs):
 
-            #Verify we have a JWT
+            # Verify we have a JWT
             verify_jwt_in_request()
             identity = get_jwt_identity()
             flag = False
 
-            #Check that at least one the roles required is in the JWT. 
+            # Check that at least one the roles required is in the JWT.
             for item in listOfRoles:
-                if item.name in identity['roles']:
+                if item.name in identity["roles"]:
                     flag = True
             if flag:
                 return fn(*args, **kwargs)
             else:
-                return {'Error' : 'This user does not have the required privilege'}, 401
-                
+                return {"Error": "This user does not have the required privilege"}, 401
+
         return decorator
 
     return wrapper
