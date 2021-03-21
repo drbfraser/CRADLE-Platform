f3dce0bc7d88fa11be2618778443e43a877207b1
Dev <dev_arora@sfu.ca>
2021-03-21 13:35:39 -0700

    Running black again

--- server/models.py
+++ server/models.py
@@ -83,7 +83,6 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
     role = db.Column(db.String(50))
-    
 
     # FOREIGN KEYS
     healthFacilityName = db.Column(
