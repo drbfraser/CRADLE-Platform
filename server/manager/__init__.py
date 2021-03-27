164a1c97cc7213d4f013b75d9743d0fbc6968e86
Dev <dev_arora@sfu.ca>
2021-03-27 16:39:48 -0700

    Reading manager no longer used

--- server/manager/__init__.py
+++ server/manager/__init__.py
@@ -1,9 +1,6 @@
 from manager.Manager import Manager
-
-from database.ReadingRepo import ReadingRepo
 from database.PatientRepo import PatientRepo
 from database.HealthFacilityRepo import HealthFacilityRepo
 
 patientManager = Manager(PatientRepo)
-readingManager = Manager(ReadingRepo)
 healthFacilityManager = Manager(HealthFacilityRepo)
