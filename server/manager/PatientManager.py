a018c3e395efd9bb892ef9f6f00e04eae22cf392
Dev <dev_arora@sfu.ca>
2021-03-28 16:50:08 -0700

    patient manager no longer uses patient or user repo

--- server/manager/PatientManager.py
+++ server/manager/PatientManager.py
@@ -4,7 +4,13 @@ import manager.FilterHelper as filter
 from database.PatientRepo import PatientRepo
 from database.UserRepo import UserRepo
 from manager.Manager import Manager
-from models import Patient
+from models import (
+    Patient, 
+    PatientSchema,
+    User,
+    Reading,
+    Referral
+)
 from data import crud, marshal
 readingRepo = ReadingRepo()
 referralRepo = ReferralRepo()
@@ -16,17 +22,11 @@ class PatientManager(Manager):
 
     def get_global_search_patients(self, current_user, search):
         def __make_gs_patient_dict(p: Patient, is_added: bool) -> dict:
-
-            #patient_dict = PatientRepo().model_to_dict(p)
-            # print(patient_dict)
-
-            patient_dict = marshal.marshal(p)
-            # print(patient_dict)
-
+            patient_dict = marshal.model_to_dict(p, PatientSchema)
             patient_dict["state"] = "Added" if is_added else "Add"
             return patient_dict
 
-        user = UserRepo().select_one(id=current_user["userId"])
+        user = crud.read(User, id=current_user["userId"])
         pairs = filter.annotated_global_patient_list(user, search)
         patients_query = [__make_gs_patient_dict(p, state) for (p, state) in pairs]
         return [self.to_global_search_patient(p) for p in patients_query]
