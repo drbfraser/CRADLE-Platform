cb258d2005ec7abbccc10a025201a17500938e14
Danica Walton <danicaw@sfu.ca>
2021-06-01 22:09:41 -0700

    syle: changes one line of style for client/src/app/index.tsx

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -33,9 +33,8 @@ export const App: React.FC = () => {
   });
   const [activeItem, setActiveItem] = React.useState<OrNull<string>>(null);
   const isBigScreen = useMediaQuery('(min-width:800px)');
-  const [isSidebarOpen, setIsSidebarOpen] = React.useState<boolean>(
-    isBigScreen
-  );
+  const [isSidebarOpen, setIsSidebarOpen] = 
+  React.useState<boolean>(isBigScreen);
 
   const { loggedIn, pathName, user } = useSelector(
     ({ user, router }: ReduxState): SelectorState => ({
