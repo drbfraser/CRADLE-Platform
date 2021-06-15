cda85ae31066443d94b034c92e78f71674b4744f
Nguyen Dang Khoa Pham (Andrey) <ndpham@sfu.ca>
2021-06-14 22:18:54 -0700

    feat: add allergy field on personal info tab

--- client/src/pages/patient/PersonalInfo.tsx
+++ client/src/pages/patient/PersonalInfo.tsx
@@ -46,6 +46,10 @@ export const PersonalInfo = ({ patient }: IProps) => {
               <b>Household number: </b>
               {patient.householdNumber ? patient.householdNumber : `N/A`}
             </p>
+            <p>
+              <b>Allergy: </b>
+              {patient.allergy ? patient.allergy : `N/A`}
+            </p>
             <Button
               color="primary"
               variant="outlined"
