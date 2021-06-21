4f859ee475d790d350b3edde0739391642523a16
zha58 <zha58@sfu.ca>
2021-06-21 14:49:01 -0700

    fix: change variable name

--- server/systemTests/api/test_pregnancies.py
+++ server/systemTests/api/test_pregnancies.py
@@ -4,21 +4,21 @@ import data.crud as crud
 from models import Pregnancy
 
 
-def test_get_pregnancy(pregnancy_factory, preganancy_earlier, api_get):
+def test_get_pregnancy(pregnancy_factory, pregnancy_earlier, api_get):
     # patient_factory.create(patientId=patient_id)
-    pregnancy_factory.create(**preganancy_earlier)
+    pregnancy_factory.create(**pregnancy_earlier)
 
-    pregnancy_id = preganancy_earlier["id"]
+    pregnancy_id = pregnancy_earlier["id"]
     response = api_get(endpoint=f"/api/pregnancies/{pregnancy_id}")
 
     assert response.status_code == 200
-    assert response.json() == preganancy_earlier
+    assert response.json() == pregnancy_earlier
 
 
-def test_put_pregnancy(pregnancy_factory, preganancy_later, api_put):
-    pregnancy_factory.create(**preganancy_later)
+def test_put_pregnancy(pregnancy_factory, pregnancy_later, api_put):
+    pregnancy_factory.create(**pregnancy_later)
 
-    pregnancy_id = preganancy_later["id"]
+    pregnancy_id = pregnancy_later["id"]
     end_date = 1623737526
     outcome = "Mode of delivery: assisted birth"
     response = api_put(
@@ -33,29 +33,29 @@ def test_put_pregnancy(pregnancy_factory, preganancy_later, api_put):
     assert new_pregnancy.outcome == outcome
 
 
-def test_post_pregnancy(patient_id, preganancy_later, api_post):
+def test_post_pregnancy(patient_id, pregnancy_later, api_post):
     try:
         response = api_post(
             endpoint=f"/api/patients/{patient_id}/pregnancies",
-            json=preganancy_later,
+            json=pregnancy_later,
         )
 
-        pregnancy_id = preganancy_later["id"]
+        pregnancy_id = pregnancy_later["id"]
         new_pregnancy = crud.read(Pregnancy, id=pregnancy_id)
 
         assert response.status_code == 201
         assert new_pregnancy.patientId == patient_id
-        assert new_pregnancy.startDate == preganancy_later["startDate"]
+        assert new_pregnancy.startDate == pregnancy_later["startDate"]
 
     finally:
         crud.delete_by(Pregnancy, id=pregnancy_id)
 
 
 def test_get_pregnancy_list(
-    pregnancy_factory, patient_id, preganancy_earlier, preganancy_later, api_get
+    pregnancy_factory, patient_id, pregnancy_earlier, pregnancy_later, api_get
 ):
-    pregnancy_factory.create(**preganancy_earlier)
-    pregnancy_factory.create(**preganancy_later)
+    pregnancy_factory.create(**pregnancy_earlier)
+    pregnancy_factory.create(**pregnancy_later)
 
     response = api_get(
         endpoint=f"/api/patients/{patient_id}/pregnancies",
@@ -66,25 +66,25 @@ def test_get_pregnancy_list(
 
 
 def test_get_pregnancy_status(
-    pregnancy_factory, patient_id, preganancy_earlier, preganancy_later, api_get
+    pregnancy_factory, patient_id, pregnancy_earlier, pregnancy_later, api_get
 ):
-    pregnancy_factory.create(**preganancy_earlier)
-    pregnancy_factory.create(**preganancy_later)
+    pregnancy_factory.create(**pregnancy_earlier)
+    pregnancy_factory.create(**pregnancy_later)
 
     response = api_get(
         endpoint=f"/api/patients/{patient_id}/pregnancies/status",
     )
 
-    preganancy_later["isPregnant"] = True
+    pregnancy_later["isPregnant"] = True
 
     assert response.status_code == 200
-    assert response.json() == preganancy_later
+    assert response.json() == pregnancy_later
 
 
-def test_invalid_pregnancy_not_updated(pregnancy_factory, preganancy_later, api_put):
-    pregnancy_factory.create(**preganancy_later)
+def test_invalid_pregnancy_not_updated(pregnancy_factory, pregnancy_later, api_put):
+    pregnancy_factory.create(**pregnancy_later)
 
-    pregnancy_id = preganancy_later["id"]
+    pregnancy_id = pregnancy_later["id"]
     response = api_put(
         endpoint=f"/api/pregnancies/{pregnancy_id}",
         json={"patientId": "0"},
@@ -93,17 +93,17 @@ def test_invalid_pregnancy_not_updated(pregnancy_factory, preganancy_later, api_
     pregnancy = crud.read(Pregnancy, id=pregnancy_id)
 
     assert response.status_code == 400
-    assert pregnancy.patientId == preganancy_later["patientId"]
+    assert pregnancy.patientId == pregnancy_later["patientId"]
 
 
 def test_invalid_pregnancy_not_created(
-    pregnancy_factory, patient_id, preganancy_later, api_post
+    pregnancy_factory, patient_id, pregnancy_later, api_post
 ):
-    pregnancy_factory.create(**preganancy_later)
+    pregnancy_factory.create(**pregnancy_later)
 
     response = api_post(
         endpoint=f"/api/patients/{patient_id}/pregnancies",
-        json=preganancy_later,
+        json=pregnancy_later,
     )
 
     assert response.status_code == 409
@@ -115,7 +115,7 @@ def patient_id():
 
 
 @pytest.fixture
-def preganancy_earlier(patient_id):
+def pregnancy_earlier(patient_id):
     return {
         "id": 60360714,
         "patientId": patient_id,
@@ -128,7 +128,7 @@ def preganancy_earlier(patient_id):
 
 
 @pytest.fixture
-def preganancy_later(patient_id):
+def pregnancy_later(patient_id):
     return {
         "id": 60360715,
         "patientId": patient_id,
