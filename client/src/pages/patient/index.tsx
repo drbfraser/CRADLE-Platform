26101137cd8bf27751ea799e3645af83aab62bf6
Min Wang <mwa109@sfu.ca>
2022-02-22 01:48:34 -0800

    add cards filters to the patient summary page(Completed)

--- client/src/pages/patient/index.tsx
+++ client/src/pages/patient/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import { Grid, Divider } from '@material-ui/core';
 import { Header } from './Header';
 import { MedicalInfo } from './MedicalInfo';
 import { PersonalInfo } from './PersonalInfo';
+
 import {
   AssessmentCard,
   ReadingCard,
@@ -19,10 +20,56 @@ import { apiFetch, API_URL } from 'src/shared/api';
 import { EndpointEnum, SexEnum } from 'src/shared/enums';
 import APIErrorToast from 'src/shared/components/apiErrorToast/APIErrorToast';
 // import { Card } from 'src/shared/types';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { makeStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+// import { Icon } from '@material-ui/core';
+import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
+// import LineStyleIcon from '@material-ui/icons/LineStyle';
+// import { NotRequiredNullableArraySchema } from 'yup';
+// import FormControl from '@material-ui/core/FormControl';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import Grid from '@material-ui/core/Grid';
+// import Select from '@material-ui/core/Select';
+// import MenuItem from '@material-ui/core/MenuItem';
+
 
 type RouteParams = {
   patientId: string;
 };
+
+type FilterRequestBody = {
+  referrals:number;
+  readings:number;
+  assessments:number;
+};
+
+type Filter = {
+  //parameter is for net request
+  //display_title is for UI display
+  parameter: string;
+  display_title: string;
+};
+
+const Filters: Filter[] = [
+{
+  parameter: 'referrals',
+  display_title: 'Referral',
+},
+{
+  parameter: 'readings',
+  display_title: 'Reading',
+},
+{
+  parameter: "assessments",
+  display_title: "Assessment",
+},
+];
+
+// const FilterCheckBoxTitles: string[] = [
+//  'Referral', 'Reading', 'Assessment',
+// ];
+
 
 // interface IProps {
 //   card:Card;
@@ -33,7 +80,19 @@ export const PatientPage = () => {
   const [patient, setPatient] = useState<Patient>();
   //we will need to send 2 request, the second is specifically for the cards data array
   const [cards, setCards] = useState([]);
+  // var has_more_than_one_cards_originally = false;
+  // const [has_more_than_one_cards_originally] = useState(0);
+  let original_cards_ref = useRef<boolean>(false);
+  // let has_more_than_one_cards_originally = 0;
   const [errorLoading, setErrorLoading] = useState(false);
+  const classes = useStyles();
+  // const selectedFilters = useState<Filters>();
+  const [selectedParameter, setSelectedParameter] =useState<string[]> (['referrals','readings','assessments']);
+  const [filterRequestBody] = useState<FilterRequestBody>({referrals:1, readings:1,assessments:1});
+
+  // const [startDate, setStartDate] = useState<Moment | null>(null);
+
+
 
   useEffect(() => {
     /*eslint no-useless-concat: "error"*/
@@ -53,12 +112,17 @@ export const PatientPage = () => {
     apiFetch(
       API_URL +
         EndpointEnum.PATIENTS +
-        `/${patientId}/get_all_records?readings=1&referrals=1&assessments=1`
+        `/${patientId}/get_all_records?readings=${filterRequestBody.readings}&referrals=${filterRequestBody.referrals}&assessments=${filterRequestBody.assessments}`
     )
       .then((resp) => resp.json())
       .then((cards_data) => {
-        console.log(cards_data);
+        // console.log(cards_data);
+        if(cards_data.length>0){
+          original_cards_ref.current = true;
 
+        }
+        console.log(original_cards_ref);
+        console.log(original_cards_ref.current);
         /////////////////////////////
         handleAssess(cards_data);
 
@@ -67,7 +131,7 @@ export const PatientPage = () => {
       .catch(() => {
         setErrorLoading(true);
       });
-  }, [patientId]);
+  }, [patientId,original_cards_ref]);
 
   const handleAssess = (cards_data: any) => {
     const cards_elements = [] as any;
@@ -128,6 +192,56 @@ export const PatientPage = () => {
     setCards(cards_elements);
   };
 
+
+  // const clearFilter = () => {
+  //   setSelectedParameter(['referrals','readings','assessments']);
+  // };
+
+  const handleChangeFilters = (newFilters:any) => {
+    //setSelectedParameter(['referrals','readings','assessments']);
+    filterRequestBody!.referrals = 0;
+    filterRequestBody!.readings = 0;
+    filterRequestBody!.assessments = 0;
+    var i;
+    for(i=0; i<newFilters.length; i++){
+      if(newFilters[i] === 'referrals'){
+        filterRequestBody!.referrals = 1;
+      }else if(newFilters[i] === 'readings'){
+        filterRequestBody!.readings = 1;
+      }else if(newFilters[i] === 'assessments'){
+        filterRequestBody!.assessments = 1;
+      }else {
+        //illegal type, not handling
+      }
+
+    }
+
+    //request parameters have been ready, now sent request and get new data
+    apiFetch(
+      API_URL +
+        EndpointEnum.PATIENTS +
+        `/${patientId}/get_all_records?readings=${filterRequestBody.readings}&referrals=${filterRequestBody.referrals}&assessments=${filterRequestBody.assessments}`
+    )
+      .then((resp) => resp.json())
+      .then((cards_data) => {
+        //console.log(cards_data);
+
+        /////////////////////////////
+        handleAssess(cards_data);
+
+        ////////////////////
+
+        //after getting the data, render the view
+        setSelectedParameter(newFilters);
+      })
+      .catch(() => {
+        setErrorLoading(true);
+      });
+
+
+  };
+ 
+
   return (
     <>
       <APIErrorToast
@@ -159,9 +273,105 @@ export const PatientPage = () => {
           )}
         </Grid>
       </Grid>
-      <br />
-      <Divider />
-      <br />
+
+          
+      {Boolean(original_cards_ref) && (
+        <>
+        {/* <br />
+        <Divider />
+        <br /> */}
+        <br />
+        <br />
+        <Grid container direction="row" justifyContent="flex-end" alignItems="center">
+        <Grid item container justifyContent="flex-end" spacing={2}>
+        {/* <Grid item md={12} sm={12} xs={12}> */}
+        <Grid item alignItems="center">
+          <>
+          <HourglassEmptyIcon className={classes.filterIcon}/>
+          </>
+        
+        </Grid>
+
+        <Grid item>
+        {/* <Grid item md={6} sm={6} xs={6}> */}
+        {Filters.map((filter_checkbox, index) => (<FormControlLabel
+                control={
+                  <Checkbox
+                    checked={selectedParameter.includes(filter_checkbox.parameter)}
+                    
+                    onChange={(event, checked) => { 
+                      // console.log(checked);
+                      // console.log(selectedParameter);
+                      // console.log(selectedParameter.includes(filter_checkbox.parameter));
+                      // console.log(filter_checkbox.parameter);
+                      
+                      //if the original state is 'selected', now the 'check operation' is to remove this item from the selected array
+                      if (checked) {
+                        // console.log(event.target.value);
+                        const newFilters = [
+                          ...selectedParameter,
+                          event.target.value, 
+                        ];
+                        // const arr = [...selectedParameter,
+                        //   event.target.value];
+                        
+                        handleChangeFilters(newFilters);
+                        // console.log(selectedParameter);
+                        // console.log(arr);
+                      } 
+                      //if the original state is 'not selected', now the 'check operation' is to add this item to the selected array
+                      else {
+                        const newParameters = [...selectedParameter];
+                        const i = newParameters.indexOf(event.target.value);
+                        if (i > -1) {
+                          //https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+                          newParameters.splice(i, 1);
+                        }
+                        handleChangeFilters(newParameters);
+                        //selectedParameter = newParameters;
+                        // console.log(selectedParameter);
+                        // console.log(i);
+                      }
+                      // console.log(selectedParameter);
+                    }}
+                    value={filter_checkbox.parameter}
+                  />
+                }
+                label={
+                  <>
+                    {filter_checkbox.display_title}
+                  </>
+                }
+                key={index}
+              />
+        ))}
+</Grid>
+        </Grid>
+        </Grid>
+        
+        
+
+
+
+
+
+
+
+
+
+
+        </>
+
+
+
+
+
+
+      )}
+      
+
+
+
 
       {/* {patient?.readings
         ? patient?.readings
@@ -173,9 +383,59 @@ export const PatientPage = () => {
               </React.Fragment>
             ))
         : null} */}
-      {console.log(cards)}
+
+      <br />
+      <Divider />
+      <br />
+      {/* {console.log(cards)} */}
 
       {cards ? cards.map((card) => <>{card}</>) : null}
     </>
   );
 };
+
+
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    margin: 0,
+    height: '100%',
+    position: 'relative',
+    resize: 'both',
+  },
+  filterIcon: {
+    height: 30,
+    width: 30,
+    color: '#00b8e6',
+    display: `flex`,
+    flexDirection: `column`,
+    alignItems: `center`,
+  },
+  content: {
+    maxHeight: 600,
+  },
+  formControl: {
+    margin: '4px 8px',
+    minWidth: 180,
+  },
+  inputLabel: {
+    fontSize: '50',
+  },
+  container: {
+    margin: 'auto',
+  },
+  center: {
+    display: `flex`,
+    flexDirection: `column`,
+    alignItems: `center`,
+  },
+  red: {
+    color: '#f44336',
+    padding: '2px',
+  },
+  green: {
+    color: '#4caf50',
+    padding: '2px',
+  },
+}));
