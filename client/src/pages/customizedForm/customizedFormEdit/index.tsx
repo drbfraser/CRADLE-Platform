36be91239084f36902efbabcbd40716710f7b23d
Min Wang <mwa109@sfu.ca>
2022-03-20 01:51:29 -0700

    1.new submit function 2.edit form

---
+++ client/src/pages/customizedForm/customizedFormEdit/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useRouteMatch } from 'react-router-dom';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Typography from '@material-ui/core/Typography';
+import { CustomizedForm } from './CustomizedForm';
+import { goBackWithFallback } from 'src/shared/utils';
+import { SelectHeaderForm } from './customizedFormHeader/SelectHeaderForm';
+import {Question} from 'src/shared/types'
+
+
+import qs from "./customizedFormHeader/form.json";
+
+type RouteParams = {
+  patientId: string;
+};
+
+export const CustomizedFormPage = () => {
+  const classes = useStyles();
+  const { patientId } = useRouteMatch<RouteParams>().params;
+  const [questions, setQuestions]= useState<Question[]>([]);
+  // const questions :Question[] = qs; 
+  //setQuestions(qs);
+  // questions = qs;
+
+ 
+  return (
+    <div className={classes.container}>
+      <div className={classes.title}>
+        <Tooltip title="Go back" placement="top">
+          <IconButton onClick={() => goBackWithFallback('/patients')}>
+            <ChevronLeftIcon color="inherit" fontSize="large" />
+          </IconButton>
+        </Tooltip>
+        <Typography variant="h4">New Form for {patientId}</Typography>
+      </div>
+
+      <br />
+      <SelectHeaderForm patientId={patientId} setQuestions = {setQuestions} />
+      {console.log(qs)}
+
+      {questions!.length > 0 && (<><br /><br /><br /><br /><br /><CustomizedForm patientId={patientId} questions={questions}/></>)}
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  container: {
+    maxWidth: 1250,
+    margin: '0 auto',
+  },
+  title: {
+    display: `flex`,
+    alignItems: `center`,
+  },
+});
