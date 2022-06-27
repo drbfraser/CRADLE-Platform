1de2528766dacef690d294083d6d922866b7da7c
Supreet <ssd19@sfu.ca>
2022-06-27 16:37:19 -0700

    renamed SecondaryButtons to secondaryButtons

---
+++ client/src/shared/components/secondaryButton/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+
+interface SecondaryButtonProps {
+
+    text: string;
+    task: string|any;
+    position: string
+}
+
+export const SecondaryButton = ({text,task: somethingToDo,position}: SecondaryButtonProps) => {
+   if(position === "no right")
+   {
+    return (
+      <Button
+        color="primary"
+        variant="outlined"
+        size = "large"
+        onClick = {somethingToDo}>
+        {text}
+      </Button>
+    );
+}
+else{
+  return (
+    <Button
+      color="primary"
+      variant="outlined"
+      size = "large"
+      style={{ float: 'right' }}
+      onClick = {somethingToDo}>
+      {text}
+    </Button>
+  );
+}
+  };
