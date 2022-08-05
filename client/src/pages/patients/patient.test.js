e0bf8957300e00625e165fc285be3953800998a1
Supreet <ssd19@sfu.ca>
2022-08-05 15:32:28 -0700

    implemented test case for primary btn, text field

--- client/src/pages/patients/patient.test.js
+++ client/src/pages/patients/patient.test.js
@@ -5,20 +5,33 @@ import { PrimaryButton } from "src/shared/components/Button";
 import { TextField } from "formik-material-ui";
 import { Paper } from "@material-ui/core";
 import { PatientsPage } from ".";
+import { useState } from 'react'
+import userEvent from '@testing-library/user-event';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
 
-test("Rendering of the patients page", ()=> {
-    render(<PatientsPage />)
+describe("Testing the rendering of the Page",()=>{
+    test("Rendering of the patients page", ()=> {
+        render(<PatientsPage />)
+    })
 })
 
-test("Rendering of the primary Button - New Patient", ()=>{
-    const handleNewPatientClick = jest.fn()
-    const { getByText } =  render(<PatientsPage />)
-    const newPatientButton = getByText("New Patient")
-    expect(newPatientButton.textContent).toBe("New Patient")
-   
+describe("Testing the primary Button - New Patient",()=>{
+
+    test("Rendering and working of the primary Button - New Patient", ()=>{
+        const historyLength = history.length
+        const { getByText } =  render(<PatientsPage />)
+        const newPatientButton = getByText("New Patient")
+        expect(newPatientButton.textContent).toBe("New Patient")
+        userEvent.click(newPatientButton)
+        expect(history.length).toBe(historyLength+1)
+    })
 })
 
-test("Rendering of the text field - Search", ()=> {
-    const { getByTestId } =  render(<PatientsPage />)
-    const searchTextfield = getByTestId("search-input")
+describe("Testing the text field - Search",()=>{
+    test("Rendering and working of the text field search",()=>{
+        const { getByTestId } =  render(<PatientsPage />)
+        const searchTextfield = getByTestId("search-input")
+        userEvent.type(searchTextfield,"sample search")
+    })
 })
