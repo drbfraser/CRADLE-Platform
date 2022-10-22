96ac97a404c411f4559e4b6dd898bc5f772b136b
Travis Chan <tsc7@sfu.ca>
2022-10-21 20:07:55 -0700

    Fix encryptor to use in user seed generation

--- server/service/encryptor.py
+++ server/service/encryptor.py
@@ -2,14 +2,13 @@ from cryptography.fernet import Fernet
 
 # Encryptor
 # - Fernet uses AES in CBC mode with 128-bit key
-class Encryptor:
-    def generate_key(self):
-        return Fernet.generate_key()
+def generate_key():
+    return Fernet.generate_key()
 
-    def encrypt(self, token, key):
-        f = Fernet(key)
-        return f.encrypt(token)
+def encrypt(token, key):
+    f = Fernet(key)
+    return f.encrypt(token)
 
-    def decrypt(self, token, key):
-        f = Fernet(key)
-        return f.decrypt(token)
+def decrypt(token, key):
+    f = Fernet(key)
+    return f.decrypt(token)
