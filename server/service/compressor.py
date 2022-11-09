ce65c934fc62e4774b7a16e7edd9977e1f739804
Travis Chan <tsc7@sfu.ca>
2022-11-08 19:34:56 -0800

    format compressor.py

--- server/service/compressor.py
+++ server/service/compressor.py
@@ -7,14 +7,17 @@ import gzip
 def compress(data):
     return gzip.compress(data)
 
+
 # Compreses bytes to bytes
 def decompress(data):
     return gzip.decompress(data)
 
+
 # Compresses strings to bytes
 def compress_from_string(string):
-    return gzip.compress(bytes(string, 'utf-8'))
+    return gzip.compress(bytes(string, "utf-8"))
+
 
 # Compreses bytes to strings
 def decompress_to_string(data):
-    return gzip.decompress(data).decode('utf-8')
+    return gzip.decompress(data).decode("utf-8")
