9f1733163758e9a88fe692c82182e0e13feab5a3
Travis Chan <tsc7@sfu.ca>
2022-11-08 17:13:10 -0800

    Add compress from and to string

--- server/service/compressor.py
+++ server/service/compressor.py
@@ -1,10 +1,20 @@
 import gzip
 
-def open(filename, mode):
-    gzip.open(filename, mode)
+# Compressor
+# - Uses gzip to compress/decompress
 
+# Compresses bytes to bytes
 def compress(data):
     return gzip.compress(data)
 
+# Compreses bytes to bytes
 def decompress(data):
     return gzip.decompress(data)
+
+# Compresses strings to bytes
+def compress_from_string(string):
+    return gzip.compress(bytes(string, 'utf-8'))
+
+# Compreses bytes to strings
+def decompress_to_string(data):
+    return gzip.decompress(data).decode('utf-8')
