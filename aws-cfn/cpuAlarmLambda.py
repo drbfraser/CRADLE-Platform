5c33cd08dc17a14c96f0be9b3cb88618018c665a
jaritant <jason_sebastian_aritanto@sfu.ca>
2023-02-27 10:00:09 -0800

    Restart fluentbit after deployment, Add alarm cloud formation and lambda script to send alarms

---
+++ aws-cfn/cpuAlarmLambda.py
@@ -0,0 +1,31 @@
+import json
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+def send_discord_alert(webhook_url):
+    webhook = DiscordWebhook(url=webhook_url)
+
+    # Create a Discord Embed
+
+    embed = DiscordEmbed(
+        title="CPU Usage Alert",
+        description=f"CPU usage is above 90%.",
+        color="ff0000")
+    # Add the Embed to the webhook
+    webhook.add_embed(embed)
+
+    # Execute the webhook
+    response = webhook.execute()
+
+    return response
+
+def lambda_handler(event, context):
+    # Get the webhook URL and message from the event
+    webhook_url = "https://discord.com/api/webhooks/1078436205777125416/4ogwGKRjMSyufI3H9mv0801zXTnaqYKuIF6Ql9g_XwMnTvEztN74AVzwySsXf3PXb77j"
+    # Send the Discord alert
+    response = send_discord_alert(webhook_url)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Discord alert sent successfully')
+    }
