c69c0bb092e6a39cee936f6a00fadfef27964613
Brendan Saw <brendansaw@gmail.com>
2023-03-11 10:27:50 -0800

    Added additional unit test for pregnancy PUT validation

--- server/tests/validation/test_pregnancies.py
+++ server/tests/validation/test_pregnancies.py
@@ -48,3 +48,30 @@ invalid_incorrect_type = {
 def test_validate_post_request(json, patient_id, output):
     message = validate_post_request(json, patient_id)
     assert type(message) == output
+
+
+valid_put_json_no_id = {
+    "pregnancyEndDate": 1620000002,
+    "pregnancyOutcome": "Mode of delivery assisted birth",
+}
+
+put_json_with_id = {"id": 123}
+
+put_json_invalid_id_type = {"id": "123"}
+
+put_json_invalid_key = {"testing": "test"}
+
+
+@pytest.mark.parametrize(
+    "json, pregnancy_id, output",
+    [
+        (valid_put_json_no_id, "0", type(None)),
+        (put_json_with_id, put_json_with_id.get("id"), type(None)),
+        (put_json_with_id, "1249812490480", str),
+        (put_json_invalid_id_type, put_json_invalid_id_type.get("id"), str),
+        (put_json_invalid_key, "0", str),
+    ],
+)
+def test_validate_put_request(json, pregnancy_id, output):
+    message = validate_put_request(json, pregnancy_id)
+    assert type(message) == output
