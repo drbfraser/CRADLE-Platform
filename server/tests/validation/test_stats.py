61bc9b8661e1557f3238ed822e332724c928d1c1
Brendan Saw <brendansaw@gmail.com>
2023-03-19 12:35:46 -0700

    Added tests for validate_time_frame_readings

--- server/tests/validation/test_stats.py
+++ server/tests/validation/test_stats.py
@@ -15,3 +15,21 @@ invalid_type = {"from": "1546702448", "to": 1547212259}
 def test_validate_timestamp(json, output):
     message = validate_timestamp(json)
     assert type(message) == output
+
+
+valid_timeframe = {"timeframe": valid_json}
+timeframe_with_invalid_json = {"timeframe": missing_required_field}
+missing_timeframe_field = {}
+
+
+@pytest.mark.parametrize(
+    "json, output",
+    [
+        (valid_timeframe, type(None)),
+        (timeframe_with_invalid_json, str),
+        (missing_timeframe_field, str),
+    ],
+)
+def test_validate_validate_time_frame_readings(json, output):
+    message = validate_time_frame_readings(json)
+    assert type(message) == output
