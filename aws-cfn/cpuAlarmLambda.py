041ca3acb750c092a6331ee2ee821c488109f868
Brendan Saw <brendansaw@gmail.com>
2023-04-18 18:35:52 -0700

    Ran Black for Python formatting

--- aws-cfn/cpuAlarmLambda.py
+++ aws-cfn/cpuAlarmLambda.py
@@ -2,15 +2,15 @@ import json
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import os
 
+
 def send_discord_alert(webhook_url):
     webhook = DiscordWebhook(url=webhook_url)
 
     # Create a Discord Embed
 
     embed = DiscordEmbed(
-        title="CPU Usage Alert",
-        description=f"CPU usage is above 90%.",
-        color="ff0000")
+        title="CPU Usage Alert", description=f"CPU usage is above 90%.", color="ff0000"
+    )
     # Add the Embed to the webhook
     webhook.add_embed(embed)
 
@@ -19,9 +19,10 @@ def send_discord_alert(webhook_url):
 
     return response
 
+
 def lambda_handler(event, context):
     # Get the webhook URL and message from the event
-    webhook_url = os.environ.get('webhook_url')
+    webhook_url = os.environ.get("webhook_url")
     # Send the Discord alert
     response = send_discord_alert(webhook_url)
 
