985fd73d2ece3c9145401b45834ff6da4ec4a2a7
Lizuoqiu <bla127@sfu.ca>
2023-07-17 17:10:06 +0800

    updated: abstract the function regex check into a module for calling

--- server/api/resources/sms_relay.py
+++ server/api/resources/sms_relay.py
@@ -97,17 +97,7 @@ def sms_relay_procedure():
 
     phoneNumber = json_request["phoneNumber"]
 
-    # Add regex check for phone number, the format of phone number is xxx-xxx-xxxxx
-    regex_phone_number_format_with_area_code = (
-        r"^([0-9+-]\+?\d{1}?[-]?\(?\d{3}[)-]?\d{3}[-]?\d{4,5})$"
-    )
-    regex_phone_number_format_normal = r"^(\d{3}[-]?\d{3}[-]?\d{4,5})$"
-    checked_number_with_area_code = re.match(
-        regex_phone_number_format_with_area_code, phoneNumber
-    )
-    checked_number = re.match(regex_phone_number_format_normal, phoneNumber)
-
-    if not checked_number and not checked_number_with_area_code:
+    if not phoneNumber_regex_check(phoneNumber):
         abort(401, message=invalid_phone_number.format(phoneNumber=phoneNumber))
 
     user = crud.read(User, phoneNumber=phoneNumber)
@@ -171,6 +161,22 @@ def sms_relay_procedure():
     response_body = json.dumps(response.json())
     return create_flask_response(response_code, response_body, user)
 
+# Abstract regex check into a module to avoid duplicate
+def phoneNumber_regex_check(phone_number):
+    # Add regex check for phone number, the format of phone number is xxx-xxx-xxxxx
+    regex_phone_number_format_with_area_code = (
+        r"^([0-9+-]\+?\d{1}?[-]?\(?\d{3}[)-]?\d{3}[-]?\d{4,5})$"
+    )
+    regex_phone_number_format_normal = r"^(\d{3}[-]?\d{3}[-]?\d{4,5})$"
+    checked_number_with_area_code = re.match(
+        regex_phone_number_format_with_area_code, phone_number
+    )
+    checked_number = re.match(regex_phone_number_format_normal, phone_number)
+
+    if not checked_number and not checked_number_with_area_code:
+        return False
+    else:
+        return True
 
 # /api/sms_relay
 class Root(Resource):
