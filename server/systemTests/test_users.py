4176add9d67b0455d3f383ce4298b879c519584a
Lizuoqiu <bla127@sfu.ca>
2023-07-25 15:59:31 +0800

    Added: add a unit test for sms key GET and PUT methods

--- server/systemTests/test_users.py
+++ server/systemTests/test_users.py
@@ -2,6 +2,9 @@ import pytest
 import requests
 import json
 
+from data import crud
+from models import SmsSecretKey
+
 
 @pytest.fixture(scope="module")
 def jwt_token():
@@ -31,6 +34,39 @@ def test_get_current_user(jwt_token):
 
     print(json.dumps(resp_body, indent=4))
     assert response.status_code == 200
+
+
+def test_sms_secret_key_for_sms_relay(jwt_token, admin_user_id):
+    url_sms_secret_key_for_user = (
+        f"http://localhost:5000/api/user/{admin_user_id}/smskey"
+    )
+    headers = {"Authorization": "Bearer " + jwt_token}
+    get_response = requests.get(url_sms_secret_key_for_user, headers=headers)
+    resp_body = get_response.json()
+    user = crud.read(SmsSecretKey, userId=admin_user_id)
+
+    print(json.dumps(resp_body, indent=4))
+    assert get_response.status_code == 200
+    assert resp_body["message"] == "A sms key has been found"
+    assert resp_body["sms_key"] is not None and resp_body["sms_key"] == user.secret_Key
+    assert user.secret_Key is not None and user.secret_Key == resp_body["sms_key"]
+
+    put_response = requests.put(url_sms_secret_key_for_user, headers=headers)
+    put_resp_body = put_response.json()
+    assert put_response.status_code == 200
+    assert (
+        put_resp_body["message"]
+        == "New key has been updated, detail is showing below: "
+    )
+    assert (
+        put_resp_body["sms_key"] is not None
+        and put_resp_body["sms_key"] != resp_body["sms_key"]
+    )
+
+
+@pytest.fixture
+def admin_user_id():
+    return 1
 
 
 @pytest.fixture
