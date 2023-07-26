72495a077465a15e9fc2853a3d7510c7901ebdb1
Lizuoqiu <bla127@sfu.ca>
2023-07-26 09:58:13 +0800

    fixed failed pipeline

--- server/manage.py
+++ server/manage.py
@@ -123,7 +123,6 @@ def seed_test_data(ctx):
     )
 
     create_sms_key(
-        "3b79718f-1197-4a62-933f-de486db776d1",
         "1",
         "46820654985a5ef1c680b50b7ee2f27691ee1cea1bfdd37c7ba150f7e4a155f1",
         "2023-09-04 01:01:16",
@@ -131,7 +130,6 @@ def seed_test_data(ctx):
     )
 
     create_sms_key(
-        "d9293827-bf9b-41e5-9e20-f205273d1262",
         "2",
         "f7b0a06ab61b5fba1a381e5a2786fc45b1079caee5bb9d90f530654c85e22194",
         "2023-09-03 01:22:21",
@@ -139,7 +137,6 @@ def seed_test_data(ctx):
     )
 
     create_sms_key(
-        "fd01d519-1bae-4f9e-9433-7ac07cc62fcc",
         "3",
         "6b0719c6fe35bb21174297e7a23cf54c8ab11c5c7e3cbe4a040268520fa97e75",
         "2024-01-24 06:29:08",
@@ -594,14 +591,12 @@ def create_form(patient_id):
 
 def create_sms_key(
     # : check the database and format to make sure it works
-    id,
     userId,
     secret_Key,
     expiry_date,
     stale_date,
 ):
     sms_key = {
-        "id": id,
         "userId": userId,
         "secret_Key": str(secret_Key),
         "expiry_date": str(expiry_date),
