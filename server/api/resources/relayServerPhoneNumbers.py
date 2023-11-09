46aad24dac3122da7b14bfee3a3fee9feefb93d2
Sara Hwang <sha173@sfu.ca>
2023-11-09 00:02:05 -0800

    Add database table for sms relay server phone numbers and endpoints

---
+++ server/api/resources/relayServerPhoneNumbers.py
@@ -0,0 +1,37 @@
+import data.crud as crud
+import data.marshal as marshal
+from api.decorator import roles_required
+from flasgger import swag_from
+from flask import request
+from flask_jwt_extended import jwt_required
+from flask_restful import Resource, abort, reqparse
+from models import RelayServerPhoneNumber
+from enums import RoleEnum
+
+
+class RelayServerPhoneNumbers(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "phone", type=str, required=True, help="Phone number is required."
+    )
+
+    @jwt_required()
+    @swag_from(
+        "../../specifications/relay-server-phone-number-get.yml", methods=["GET"]
+    )
+    def get(self):
+        phone_numbers = crud.read_all(RelayServerPhoneNumber)
+        return [marshal.marshal(f, shallow=True) for f in phone_numbers]
+
+    @staticmethod
+    @roles_required([RoleEnum.ADMIN])
+    @swag_from(
+        "../../specifications/relay-server-phone-number-post.yml",
+        methods=["POST"],
+    )
+    def post():
+        req = request.get_json(force=True)
+        if len(req) == 0:
+            abort(400, message="Request body is empty")
+        crud.create(RelayServerPhoneNumber(phone=req["phone"]), refresh=True)
+        return {"message": "Relay server phone number added successfully"}, 200
