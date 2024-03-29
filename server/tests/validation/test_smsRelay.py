c661ed87071acc74278b43385ae30cc3d41c5763
chhokara <chhokara@gmail.com>
2024-03-29 00:45:48 -0700

    570: create test cases

---
+++ server/tests/validation/test_smsRelay.py
@@ -0,0 +1,30 @@
+import pytest
+from validation.sms_relay import validate_request, validate_decrypted_body
+
+request_test_cases = [
+    {
+        "json": {"phoneNumber": "604-715-2845", "encryptedData": "thisdataisencrypted"},
+        "output": type(None),
+    },
+    {"json": {"encryptedData": "thisdataisencrypted"}, "output": str},
+    {"json": {"phoneNumber": "604-715-2845",}, "output": str},
+    {
+        "json": {
+            "phoneNumber": 604 - 715 - 2845,
+            "encryptedData": "thisdataisencrypted",
+        },
+        "output": str,
+    },
+    {
+        "json": {"phoneNumber": "604-715-2845", "encryptedData": 1234567890},
+        "output": str,
+    },
+]
+
+
+@pytest.mark.parametrize("validate_request_test_cases", request_test_cases)
+def test_validate_request(test_case):
+    json_data = test_case["json"]
+    output = test_case["output"]
+    message = validate_request(json_data)
+    assert type(message) == output
