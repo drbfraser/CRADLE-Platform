76945c0bd78f42a7fc4abc770e48ccd86f87395e
chhokara <chhokara@gmail.com>
2024-03-31 23:59:33 -0700

    570: add test cases for validate_decrypted_body function

--- server/tests/validation/test_smsRelay.py
+++ server/tests/validation/test_smsRelay.py
@@ -21,10 +21,49 @@ request_test_cases = [
     },
 ]
 
+decrypted_body_test_cases = [
+    {
+        "json": {"requestNumber": "12345", "method": "GET", "endpoint": "my/endpoint"},
+        "output": type(None),
+    },
+    {"json": {"requestNumber": "12345", "endpoint": "my/endpoint"}, "output": str},
+    {"json": {"requestNumber": "12345", "method": "GET",}, "output": str},
+    {"json": {"method": "GET", "endpoint": "my/endpoint"}, "output": str},
+    {
+        "json": {
+            "requestNumber": "12345",
+            "method": "GET",
+            "endpoint": "my/endpoint",
+            "invalid": "invalidkey",
+        },
+        "output": str,
+    },
+    {
+        "json": {"requestNumber": 12345, "method": "GET", "endpoint": "my/endpoint"},
+        "output": str,
+    },
+    {
+        "json": {"requestNumber": "12345", "method": 12345, "endpoint": "my/endpoint"},
+        "output": str,
+    },
+    {
+        "json": {"requestNumber": "12345", "method": "GET", "endpoint": 12345},
+        "output": str,
+    },
+]
+
 
 @pytest.mark.parametrize("test_case", request_test_cases)
 def test_validate_request(test_case):
     json_data = test_case["json"]
     output = test_case["output"]
     message = validate_request(json_data)
+    assert type(message) == output
+
+
+@pytest.mark.parametrize("test_case", decrypted_body_test_cases)
+def test_validate_decrypted_body(test_case):
+    json_data = test_case["json"]
+    output = test_case["output"]
+    message = validate_decrypted_body(json_data)
     assert type(message) == output
