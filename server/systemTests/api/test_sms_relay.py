5e8749edfb752b3e839899e04afed8cc23e22cf4
chhokara <chhokara@gmail.com>
2024-04-22 00:01:07 -0700

    572: add a log

--- server/systemTests/api/test_sms_relay.py
+++ server/systemTests/api/test_sms_relay.py
@@ -28,8 +28,11 @@ def test_create_patient_with_sms_relay(database, api_post):
     json_body = make_sms_relay_json(1, method, endpoint, body=patient_json)
     response = api_post(endpoint=sms_relay_endpoint, json=json_body)
     database.session.commit()
+    response_dict = json.loads(response.text)
+    print("THIS IS THE RESPONSE: ", response.text)
     try:
         assert response.status_code == 200
+        assert response_dict["code"] == 201
         assert crud.read(Patient, patientId=patient_id) is not None
 
         for r in reading_ids:
@@ -57,9 +60,11 @@ def test_create_referral_with_sms_relay(
 
     response = api_post(endpoint=sms_relay_endpoint, json=json_body)
     database.session.commit()
+    response_dict = json.loads(response.text)
 
     try:
         assert response.status_code == 200
+        assert response_dict["code"] == 201
         assert crud.read(Referral, id=referral_id) is not None
 
     finally:
@@ -81,9 +86,11 @@ def test_create_readings_with_sms_relay(
 
     response = api_post(endpoint=sms_relay_endpoint, json=json_body)
     database.session.commit()
+    response_dict = json.loads(response.text)
 
     try:
         assert response.status_code == 200
+        assert response_dict["code"] == 201
         assert crud.read(Reading, readingId=reading_id) is not None
 
     finally:
@@ -104,8 +111,10 @@ def test_update_patient_name_with_sms_relay(database, patient_factory, api_post)
 
     response = api_post(endpoint=sms_relay_endpoint, json=json_body)
     database.session.commit()
+    response_dict = json.loads(response.text)
 
     assert response.status_code == 200
+    assert response_dict["code"] == 200
     assert crud.read(Patient, patientId=patient_id).patientName == new_patient_name
 
 
@@ -125,8 +134,10 @@ def test_create_assessments_with_sms_relay(
     database.session.commit()
 
     followupInstructions = assessment_json["followupInstructions"]
+    response_dict = json.loads(response.text)
 
     assert response.status_code == 200
+    assert response_dict["code"] == 201
     assert (
         crud.read(FollowUp, patientId=patient_id).followupInstructions
         == followupInstructions
@@ -152,7 +163,9 @@ def test_update_assessments_with_sms_relay(
     json_body = make_sms_relay_json(6, method, endpoint, body=assessment_json)
     response = api_post(endpoint=sms_relay_endpoint, json=json_body)
     database.session.commit()
+    response_dict = json.loads(response.text)
     assert response.status_code == 200
+    assert response_dict["code"] == 200
     assert crud.read(FollowUp, id=assessment_id).followupInstructions == newInstructions
 
 
