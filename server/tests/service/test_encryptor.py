a9716dfb181b0635153ec72323d7765c4a289427
chhokara <chhokara@gmail.com>
2024-04-23 13:18:19 -0700

    573: add back parametrization

--- server/tests/service/test_encryptor.py
+++ server/tests/service/test_encryptor.py
@@ -3,50 +3,63 @@ import service.encryptor as encryptor
 import service.compressor as compressor
 
 
-def test_encryptor():
+@pytest.mark.parametrize("message", ["test1", "test2", "test3"])
+def test_encryptor(message):
     key = "1a9b4f7c3e8d2f5a6b4f7c3e8d2f5a1a"
-    plain_text = b"Hello, world!"
     iv = "0791C97495596B09202D443D81054C77"
 
+    plain_text = message.encode("utf-8")
+
     encrypted_data = encryptor.encrypt(plain_text, iv, key)
-    assert b"test" != encrypted_data
+    assert plain_text != encrypted_data
 
     decrypted_data = encryptor.decrypt(encrypted_data, key)
     assert decrypted_data == plain_text
 
 
-def test_encryptor_wrong_key():
+@pytest.mark.parametrize(
+    "invalid_key",
+    [
+        "1a9b4f7c3e8d2f5a6b4f7c3e8d2f5a1b",
+        "1a9b4f7c3e8d2f5a6b4f7c3e8d2f5a10",
+        "1a9b4f7c3e8d2f5a6b4f7c3e8d2f5a11",
+    ],
+)
+def test_encryptor_wrong_key(invalid_key):
     key = "1a9b4f7c3e8d2f5a6b4f7c3e8d2f5a1a"
     iv = "0791C97495596B09202D443D81054C77"
-    message_bytes = b"test"
-    encrypted_data = encryptor.encrypt(message_bytes, iv, key)
-    invalid_key = "1a9b4f7c3e8d2f5a6b4f7c3e8d2f5a1b"
+
+    message = "test"
+    plain_text = message.encode("utf-8")
+    encrypted_data = encryptor.encrypt(plain_text, iv, key)
 
     decrypted_data = encryptor.decrypt(encrypted_data, key)
     decrypted_data2 = encryptor.decrypt(encrypted_data, invalid_key)
 
     assert decrypted_data != decrypted_data2
-    assert decrypted_data == message_bytes
+    assert decrypted_data == plain_text
 
 
-def test_encryptor_wrong_parameter_type():
+@pytest.mark.parametrize("invalid_key_type", [1, 3.14, True])
+def test_encryptor_wrong_parameter_type(invalid_key_type):
     key = "1a9b4f7c3e8d2f5a6b4f7c3e8d2f5a1a"
     iv = "0791C97495596B09202D443D81054C77"
-    message_bytes = b"test"
-    encrypted_data = encryptor.encrypt(message_bytes, iv, key)
-    invalid_key_type = 5
+
+    message = "test"
+    plain_text = message.encode("utf-8")
+    encrypted_data = encryptor.encrypt(plain_text, iv, key)
 
     with pytest.raises(TypeError):
         assert encryptor.decrypt(encrypted_data, invalid_key_type)
 
     decrypted_data2 = encryptor.decrypt(encrypted_data, key)
-    assert decrypted_data2 == message_bytes
+    assert decrypted_data2 == plain_text
 
 
-def test_encryptor_compressor():
+@pytest.mark.parametrize("message", ["test1", "test2", "test3"])
+def test_encryptor_compressor(message):
     key = "1a9b4f7c3e8d2f5a6b4f7c3e8d2f5a1a"
     iv = "0791C97495596B09202D443D81054C77"
-    message = "test"
 
     compressed_data = compressor.compress_from_string(message)
     encrypted_data = encryptor.encrypt(compressed_data, iv, key)
