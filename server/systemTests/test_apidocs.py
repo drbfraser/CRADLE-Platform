36018d319d5ea10c2f353dd329660bd8e27ae089
rrajesh <rrajesh@sfu.ca>
2024-05-17 16:28:30 -0700

    553: Add test to check apidocs remain accessible

---
+++ server/systemTests/test_apidocs.py
@@ -0,0 +1,12 @@
+import requests
+
+def test_apidocs_are_accessible():
+    """
+    Test if the swagger api documentation(accessible at http://localhost:5000/apidocs) is working.
+
+    This check ensures we don't forget to update `Cradle-Platform/server/specifications` when making changes
+    to api routes
+    """
+    response = requests.get("http://localhost:5000/apispec_1.json")
+
+    assert response.status_code == 200
