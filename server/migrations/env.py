5c45c4149eb9c8ac56bc9dd1092ef7a8b4b699d9
mla400 <mla400@sfu.ca>
2024-07-15 01:00:39 -0400

    fix checks

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -1,14 +1,18 @@
 from __future__ import with_statement
 
 import logging
+import os
+import sys
 from logging.config import fileConfig
-import os, sys
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+
+from sqlalchemy import engine_from_config, pool
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from app import app
 from alembic import context
 from alembic.script import ScriptDirectory
+
+from app import app
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
