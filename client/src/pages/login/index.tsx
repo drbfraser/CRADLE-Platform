28feaa478486fb8dc9a716049cd725d556a30a7d
Ryan Milligan <rcm9@sfu.ca>
2024-09-14 14:38:45 -0700

    fix: fix login error by moving image into separate component

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -7,27 +7,13 @@ import { Box, useMediaQuery } from '@mui/material';
 import { TOP_BAR_HEIGHT } from 'src/shared/constants';
 
 export const LoginPage: React.FC = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   // if the user has reached the login page, they likely came directly here
   // therefore Redux will be empty and we must check local storage for a token
   if (localStorage.getItem('token') !== null) {
     return <Redirect to="/referrals" />;
   }
-
-  /**
-   * When the aspect ratio of the window is too low, the splash image on the
-   * login page causes issues where the login form becomes inaccessible. To
-   * prevent this from happening, we can conditionally render the image only
-   * when the window is above a certain aspect ratio. The exact value for the
-   * aspect ratio threshold may need further tweaking in the future if it is
-   * found to not be sufficient for all cases.
-   */
-  const aspectRatioThreshold = useMediaQuery('(min-aspect-ratio:1.3/1)');
-
-  // Below this height, the image starts to get messed up.
-  const minHeight = useMediaQuery('(min-height:500px)');
-  const showImage = aspectRatioThreshold && minHeight;
 
   return (
     <Stack
@@ -36,19 +22,34 @@ export const LoginPage: React.FC = () => {
         height: '100%',
         maxHeight: '100%',
       }}>
-      {showImage && (
-        <Box
-          id="loginSplashImageContainer"
-          sx={{
-            height: '100%',
-            width: 'auto',
-            maxHeight: `calc(100vh - ${TOP_BAR_HEIGHT})`,
-          }}>
-          <img alt="logo" src={image} className={classes.imgStyle} />
-        </Box>
-      )}
-
+      <LoginSplashImage />
       <LoginForm />
     </Stack>
   );
+};
+
+const LoginSplashImage = () => {
+  const classes = useStyles();
+  /**
+   * When the aspect ratio of the window is too low, the splash image on the
+   * login page causes issues where the login form becomes inaccessible. To
+   * prevent this from happening, we can conditionally render the image only
+   * when the window is above a certain aspect ratio. The exact value for the
+   * aspect ratio threshold may need further tweaking in the future if it is
+   * found to not be sufficient for all cases.
+   */
+  const showImage = useMediaQuery(
+    '(min-aspect-ratio:1.3/1) and (min-height:500px)'
+  );
+  return showImage ? (
+    <Box
+      id="loginSplashImageContainer"
+      sx={{
+        height: '100%',
+        width: 'auto',
+        maxHeight: `calc(100vh - ${TOP_BAR_HEIGHT})`,
+      }}>
+      <img alt="logo" src={image} className={classes.imgStyle} />
+    </Box>
+  ) : null;
 };
