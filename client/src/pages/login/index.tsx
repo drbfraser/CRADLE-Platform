55ac0bb6a78ac09decfe97b4a31fac16ec4d0a0e
Ryan Milligan <rcm9@sfu.ca>
2024-09-13 23:42:25 -0700

    fix: hide image based on aspect ratio rather than absolute width

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,21 +1,23 @@
 import { LoginForm } from './form';
 import { Redirect } from 'react-router-dom';
 import image from './img/splash_screen_4.png';
-import { useDimensionsContext } from 'src/app/context/hooks';
+// import { useDimensionsContext } from 'src/app/context/hooks';
 import { useStyles } from './styles';
 import Stack from '@mui/material/Stack';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import { TOP_BAR_HEIGHT } from 'src/shared/constants';
 
 export const LoginPage: React.FC = () => {
   const classes = useStyles();
-  const { isBigScreen } = useDimensionsContext();
+  // const { isBigScreen } = useDimensionsContext();
 
   // if the user has reached the login page, they likely came directly here
   // therefore Redux will be empty and we must check local storage for a token
   if (localStorage.getItem('token') !== null) {
     return <Redirect to="/referrals" />;
   }
+
+  const aspectRatioMediaQuery = useMediaQuery('(min-aspect-ratio:1.3/1)');
 
   return (
     <Stack
@@ -24,7 +26,8 @@ export const LoginPage: React.FC = () => {
         height: '100%',
         maxHeight: '100%',
       }}>
-      {isBigScreen && (
+      {/* {isBigScreen && aspectRatioMediaQuery && ( */}
+      {aspectRatioMediaQuery && (
         <Box
           id="loginSplashImageContainer"
           sx={{
