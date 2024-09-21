57be12ebb6acf701f01781a0aae73bfc39a12116
Ryan Milligan <rcm9@sfu.ca>
2024-09-20 17:28:37 -0700

    refactor(MUI): migrate styles for ResourceTabPage

--- client/src/pages/resources/ResourceTabPage/index.tsx
+++ client/src/pages/resources/ResourceTabPage/index.tsx
@@ -5,12 +5,10 @@ import { PrimaryButton, SecondaryButton } from 'src/shared/components/Button';
 import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
 import PropTypes from 'prop-types';
-import { Theme, useTheme } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import { useState } from 'react';
-import { useMediaQuery } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 
 interface IProps {
   posterImgSrc: any[];
@@ -18,36 +16,7 @@ interface IProps {
   videoUrl: string;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    center: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    root: {
-      maxWidth: '100%',
-      flexGrow: 1,
-    },
-    header: {
-      textAlign: 'center',
-      paddingLeft: theme.spacing(4),
-      backgroundColor: '#15152B',
-      color: '#F2F2F2',
-    },
-    img: {
-      height: 480,
-      maxWidth: 640,
-      overflow: 'hidden',
-      display: 'block',
-      width: '100%',
-      objectFit: 'contain',
-      backgroundColor: 'aliceblue',
-    },
-  })
-);
 function ResourceTabPage(props: IProps): JSX.Element {
-  const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
   const maxSteps = props.posterImgSrc.length;
   const theme = useTheme();
@@ -63,20 +32,39 @@ function ResourceTabPage(props: IProps): JSX.Element {
 
   return (
     <>
-      <div className={classes.center}>
-        <div
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Box
           className="margin"
           style={{ width: isBigScreen ? '640px' : '340px' }}>
           <Paper
             square
             elevation={0}
-            className={classes.header}
+            sx={(theme) => ({
+              textAlign: 'center',
+              paddingLeft: theme.spacing(4),
+              backgroundColor: '#15152B',
+              color: '#F2F2F2',
+            })}
             style={{ width: isBigScreen ? '640px' : '340px' }}>
             <Typography>{props.posterImgSrc[activeStep].label}</Typography>
           </Paper>
-          <img
+          <Box
+            component={'img'}
             alt="Resources"
-            className={classes.img}
+            sx={{
+              height: '480px',
+              maxWidth: '640px',
+              overflow: 'hidden',
+              display: 'block',
+              width: '100%',
+              objectFit: 'contain',
+              backgroundColor: 'aliceblue',
+            }}
             src={props.posterImgSrc[activeStep].imgPath}
           />
 
@@ -86,7 +74,10 @@ function ResourceTabPage(props: IProps): JSX.Element {
             steps={maxSteps}
             position="static"
             activeStep={activeStep}
-            className={classes.root}
+            sx={{
+              maxWidth: '100%',
+              flexGrow: 1,
+            }}
             nextButton={
               <PrimaryButton
                 disabled={activeStep === maxSteps - 1}
@@ -109,9 +100,15 @@ function ResourceTabPage(props: IProps): JSX.Element {
               Click to view/download PDF
             </a>
           </p>
-        </div>
-      </div>
-      <div className="centered-flexbox margin">
+        </Box>
+      </Box>
+      <Box
+        className="centered-flexbox margin"
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <iframe
           width={isBigScreen ? '640' : '340'}
           height="480"
@@ -120,7 +117,7 @@ function ResourceTabPage(props: IProps): JSX.Element {
           frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen></iframe>
-      </div>
+      </Box>
     </>
   );
 }
