2cdadd428e66581ec8cd152cb553d9a891705ad9
Ryan Milligan <rcm9@sfu.ca>
2024-10-11 11:06:48 -0700

    refactor: wrap app in AppProvider

--- client/src/app/providers/TopLevelContextProviders.tsx
+++ client/src/app/providers/TopLevelContextProviders.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { MaterialUIContextProvider } from 'src/app/providers/materialUI';
+import { AppProvider } from '@toolpad/core/AppProvider';
+
+import { theme } from 'src/app/providers/materialUI/theme';
 
 /** Combined Context Providers to wrap the top level App component. */
 
@@ -14,9 +17,11 @@ export const TopLevelContextProviders = ({ children }: PropsWithChildren) => {
     <ReduxProvider store={reduxStore}>
       <BrowserRouter>
         <MaterialUIContextProvider>
-          <LocalizationProvider dateAdapter={AdapterMoment}>
-            {children}
-          </LocalizationProvider>
+          <AppProvider theme={theme}>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+              {children}
+            </LocalizationProvider>
+          </AppProvider>
         </MaterialUIContextProvider>
       </BrowserRouter>
     </ReduxProvider>
