4213a70386c42925ce4e614ff6a7cef9657e43e2
Ryan Milligan <rcm9@sfu.ca>
2024-10-16 11:55:48 -0700

    refactor: pass history object to router provider

--- client/src/app/providers/TopLevelContextProviders.tsx
+++ client/src/app/providers/TopLevelContextProviders.tsx
@@ -2,20 +2,21 @@ import { PropsWithChildren } from 'react';
 
 import { Provider as ReduxProvider } from 'react-redux';
 import { reduxStore } from 'src/redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { MaterialUIContextProvider } from 'src/app/providers/materialUI';
 import { AppProvider } from '@toolpad/core/AppProvider';
 
 import { theme } from 'src/app/providers/materialUI/theme';
+import { history } from '../../redux/reducers/history';
 
 /** Combined Context Providers to wrap the top level App component. */
 
 export const TopLevelContextProviders = ({ children }: PropsWithChildren) => {
   return (
     <ReduxProvider store={reduxStore}>
-      <BrowserRouter>
+      <Router history={history}>
         <MaterialUIContextProvider>
           <AppProvider theme={theme}>
             <LocalizationProvider dateAdapter={AdapterMoment}>
@@ -23,7 +24,7 @@ export const TopLevelContextProviders = ({ children }: PropsWithChildren) => {
             </LocalizationProvider>
           </AppProvider>
         </MaterialUIContextProvider>
-      </BrowserRouter>
+      </Router>
     </ReduxProvider>
   );
 };
