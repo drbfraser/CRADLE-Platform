f9967008fc32c00eaf597b8f75ca9c90f670fc98
Ryan Milligan <rcm9@sfu.ca>
2024-10-16 12:59:58 -0700

    refactor: add onRedirectCallback

--- client/src/app/providers/auth/AuthProvider.tsx
+++ client/src/app/providers/auth/AuthProvider.tsx
@@ -1,17 +1,27 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import { PropsWithChildren } from 'react';
 import { getAuthConfig } from './authConfig';
+import { useHistory } from 'react-router-dom';
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const history = useHistory();
   const authConfig = getAuthConfig();
   const providerConfig = {
     domain: authConfig.domain,
     clientId: authConfig.clientId,
     authorizationParams: {
-      redirect_uri: window.location.origin,
+      redirect_uri: authConfig.callbackURL,
       ...(authConfig.audience ? { audience: authConfig.audience } : null),
     },
   };
 
-  return <Auth0Provider {...providerConfig}>{children}</Auth0Provider>;
+  return (
+    <Auth0Provider
+      {...providerConfig}
+      onRedirectCallback={(appState) => {
+        history.push(appState?.returnTo || window.location.pathname);
+      }}>
+      {children}
+    </Auth0Provider>
+  );
 };
