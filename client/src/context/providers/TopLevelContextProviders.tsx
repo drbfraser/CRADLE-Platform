f26852c525cc1e26836644567f400b22d58e12b6
Ryan Milligan <rcm9@sfu.ca>
2024-10-22 15:06:13 -0700

    fix: import

--- client/src/context/providers/TopLevelContextProviders.tsx
+++ client/src/context/providers/TopLevelContextProviders.tsx
@@ -5,10 +5,10 @@ import { reduxStore } from 'src/redux/store';
 import { Router } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { MaterialUIContextProvider } from 'src/app/providers/materialUI';
+import { MaterialUIContextProvider } from 'src/context/providers/materialUI';
 import { AppProvider } from '@toolpad/core/AppProvider'; // MUI Toolpad.
 
-import { theme } from 'src/app/providers/materialUI/theme';
+import { theme } from 'src/context/providers/materialUI/theme';
 import { history } from 'src/shared/history';
 import { AuthProvider } from './auth/AuthProvider';
 
