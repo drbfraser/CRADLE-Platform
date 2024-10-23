63f9231f08232e5fb2b7e13a158e1f54b0a227da
Ryan Milligan <rcm9@sfu.ca>
2024-10-22 18:21:43 -0700

    refactor: replace Router with BrowserRouter

--- client/src/context/providers/TopLevelContextProviders.tsx
+++ client/src/context/providers/TopLevelContextProviders.tsx
@@ -2,20 +2,19 @@ import { PropsWithChildren } from 'react';
 
 import { Provider as ReduxProvider } from 'react-redux';
 import { reduxStore } from 'src/redux/store';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { MaterialUIContextProvider } from 'src/context/providers/materialUI';
 import { AppProvider } from '@toolpad/core/AppProvider'; // MUI Toolpad.
 
 import { theme } from 'src/context/providers/materialUI/theme';
-import { history } from 'src/shared/history';
 
 /** Combined Context Providers to wrap the top level App component. */
 
 export const TopLevelContextProviders = ({ children }: PropsWithChildren) => {
   return (
-    <Router history={history}>
+    <BrowserRouter>
       <ReduxProvider store={reduxStore}>
         <MaterialUIContextProvider>
           <AppProvider theme={theme}>
@@ -25,6 +24,6 @@ export const TopLevelContextProviders = ({ children }: PropsWithChildren) => {
           </AppProvider>
         </MaterialUIContextProvider>
       </ReduxProvider>
-    </Router>
+    </BrowserRouter>
   );
 };
