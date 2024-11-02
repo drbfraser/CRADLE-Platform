e7ddcba5d04c825f99b3d43ed1cf912420bf022c
Ryan Milligan <rcm9@sfu.ca>
2024-11-02 09:31:54 -0700

    refactor: align height of search buttons

--- client/src/pages/referrals/index.tsx
+++ client/src/pages/referrals/index.tsx
@@ -36,7 +36,6 @@ export const ReferralsPage = () => {
   // ensure that we wait until the user has stopped typing
   const debounceSetSearch = debounce(setSearch, 500);
   const theme = useTheme();
-  const isBigScreen = useMediaQuery(theme.breakpoints.up('lg'));
   const isTransformed = useMediaQuery(`(min-width:${BREAKPOINT}px)`);
 
   const userId = useSelector(({ user }: ReduxState): number | undefined => {
@@ -81,6 +80,9 @@ export const ReferralsPage = () => {
       <DashboardPaper>
         <Box
           sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '0.5rem',
             padding: {
               xs: '15px',
               lg: '30px',
@@ -88,87 +90,122 @@ export const ReferralsPage = () => {
           }}>
           <Box
             sx={{
-              display: 'inline-block',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              gap: '1rem',
+              flexWrap: 'wrap',
             }}>
             <Typography
               variant="h2"
               sx={{
-                display: 'inline-block',
-                fontSize: '1.7rem',
+                textAlign: 'center',
+                verticalAlign: 'center',
+                fontSize: '2rem',
                 fontWeight: 'bold',
               }}>
               Referrals
             </Typography>
+
             <Box
               sx={{
-                marginTop: '16px',
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '0.5rem',
+                height: '50px',
+                alignItems: 'center',
+                '& .MuiInputBase-root': {
+                  height: '50px',
+                  width: '180px',
+                },
               }}>
-              <AutoRefresher
-                setRefresh={setRefresh}
-                refreshTimer={refreshTimer}
-                setIsRefreshDialogOpen={setIsRefreshDialogOpen}
-              />
-            </Box>
-          </Box>
-
-          {!isBigScreen && <br />}
-          <RefreshDialog
-            onClose={() => {
-              setIsRefreshDialogOpen(false);
-            }}
-            open={isRefreshDialogOpen}
-            isTransformed={isTransformed}
-            setRefreshTimer={setRefreshTimer}
-            refreshTimer={refreshTimer}
-          />
-          <FilterDialog
-            onClose={() => {
-              setIsFilterDialogOpen(false);
-            }}
-            open={isFilterDialogOpen}
-            filter={filter!}
-            setFilter={setFilter}
-            isTransformed={isTransformed}
-            setIsPromptShown={setIsPromptShown}
-          />
-          <Box sx={isBigScreen ? SEARCH_SX : SEARCH_THIN_SX}>
-            <Box>
               <TextField
                 label="Search"
                 placeholder="Patient ID, Name or Village"
                 variant="outlined"
                 onChange={(e) => debounceSetSearch(e.target.value)}
               />
-              {isPromptShown && (
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    marginTop: '0.25rem',
-                    gap: '0.25rem',
-                  }}>
-                  <Typography color="textSecondary" variant="caption">
-                    Currently filtered to your health facility.
-                  </Typography>
-                  <Typography color="textSecondary" variant="caption">
-                    Click Clear Filter to see all.
-                  </Typography>
-                </Box>
-              )}
+
+              <PrimaryButton
+                sx={{
+                  height: '100%',
+                  fontSize: 'large',
+                  '@media (max-width: 720px)': {
+                    fontSize: 'medium',
+                  },
+                }}
+                onClick={() => setIsFilterDialogOpen(true)}>
+                Filter Search
+              </PrimaryButton>
+
+              <CancelButton
+                sx={{
+                  height: '100%',
+                  fontSize: 'large',
+                  '@media (max-width: 720px)': {
+                    fontSize: 'medium',
+                  },
+                }}
+                onClick={() => {
+                  setFilter(undefined);
+                  setIsPromptShown(false);
+                }}
+                className="mx-auto">
+                Clear Filter
+              </CancelButton>
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <RefreshDialog
+              onClose={() => {
+                setIsRefreshDialogOpen(false);
+              }}
+              open={isRefreshDialogOpen}
+              isTransformed={isTransformed}
+              setRefreshTimer={setRefreshTimer}
+              refreshTimer={refreshTimer}
+            />
+            <FilterDialog
+              onClose={() => {
+                setIsFilterDialogOpen(false);
+              }}
+              open={isFilterDialogOpen}
+              filter={filter!}
+              setFilter={setFilter}
+              isTransformed={isTransformed}
+              setIsPromptShown={setIsPromptShown}
+            />
+            <Box>
+              <AutoRefresher
+                setRefresh={setRefresh}
+                refreshTimer={refreshTimer}
+                setIsRefreshDialogOpen={setIsRefreshDialogOpen}
+              />
             </Box>
 
-            <PrimaryButton onClick={() => setIsFilterDialogOpen(true)}>
-              Filter Search
-            </PrimaryButton>
-
-            <CancelButton
-              onClick={() => {
-                setFilter(undefined);
-                setIsPromptShown(false);
-              }}
-              className="mx-auto">
-              Clear Filter
-            </CancelButton>
+            {isPromptShown && (
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '0.25rem',
+                }}>
+                <Typography color="textSecondary" variant="caption">
+                  Currently filtered to your health facility.
+                </Typography>
+                <Typography color="textSecondary" variant="caption">
+                  Click Clear Filter to see all.
+                </Typography>
+              </Box>
+            )}
           </Box>
         </Box>
 
