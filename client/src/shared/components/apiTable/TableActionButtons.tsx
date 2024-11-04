233c4bca3f3f488b7b69fad79cae36e8dd9cf3ea
Ryan Milligan <rcm9@sfu.ca>
2024-11-04 11:46:57 -0800

    refactor: improve icon button responsiveness

--- client/src/shared/components/apiTable/TableActionButtons.tsx
+++ client/src/shared/components/apiTable/TableActionButtons.tsx
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-import { Box, IconButton, SvgIconTypeMap, Tooltip } from '@mui/material';
+import {
+  Box,
+  IconButton,
+  SvgIconTypeMap,
+  Tooltip,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import { OverridableComponent } from '@mui/types';
 
 export type TableAction = {
@@ -17,12 +24,16 @@ export const TableActionButtons = ({ actions }: TableActionButtonsProps) => {
   return (
     <Box
       sx={{
-        marginX: 'auto',
+        margin: 'auto',
+        height: '100%',
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        gap: '4px',
+        gap: {
+          md: '4px',
+          xs: '1px',
+        },
       }}>
       {actions.map((action) => (
         <TableActionButton key={action.tooltip} {...action} />
@@ -31,16 +42,39 @@ export const TableActionButtons = ({ actions }: TableActionButtonsProps) => {
   );
 };
 
+const SMALL_ICON = '16px';
+const MEDIUM_ICON = '20px';
+const LARGE_ICON = '24px';
+
 type TableActionButtonProps = TableAction;
 export const TableActionButton = ({
   tooltip,
   Icon,
   onClick,
 }: TableActionButtonProps) => {
+  const theme = useTheme();
+  const isXSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+  let buttonSize: 'small' | 'medium' | 'large' = 'large';
+  let iconSize: typeof SMALL_ICON | typeof MEDIUM_ICON | typeof LARGE_ICON =
+    LARGE_ICON;
+  if (isXSmallScreen) {
+    buttonSize = 'small';
+    iconSize = SMALL_ICON;
+  } else if (isSmallScreen) {
+    buttonSize = 'medium';
+    iconSize = MEDIUM_ICON;
+  }
+
   return (
     <Tooltip placement="top" title={tooltip}>
-      <IconButton onClick={onClick} size="large">
-        <Icon />
+      <IconButton onClick={onClick} size={buttonSize}>
+        <Icon
+          sx={{
+            fontSize: iconSize,
+          }}
+        />
       </IconButton>
     </Tooltip>
   );
