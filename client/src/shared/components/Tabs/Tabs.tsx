4ee30e71158808d7d0a1c714aab03a647e1c1dd8
Ryan Milligan <rcm9@sfu.ca>
2024-11-04 10:43:31 -0800

    fix: make tabs fit on small screens

--- client/src/shared/components/Tabs/Tabs.tsx
+++ client/src/shared/components/Tabs/Tabs.tsx
@@ -1,5 +1,11 @@
 import { PropsWithChildren, useState } from 'react';
-import { Box, Tab as MuiTab, Tabs as MuiTabs } from '@mui/material';
+import {
+  Box,
+  Tab as MuiTab,
+  Tabs as MuiTabs,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 
 type TabPanelData = {
   label: string;
@@ -11,10 +17,35 @@ type TabsProps = {
 };
 export const Tabs = ({ panels }: TabsProps) => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
-
+  const theme = useTheme();
+  const isXSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
   return (
     <Box>
       <MuiTabs
+        variant={isXSmallScreen ? 'fullWidth' : 'scrollable'}
+        scrollButtons={'auto'}
+        allowScrollButtonsMobile
+        sx={{
+          overflow: 'hidden',
+          width: '100%',
+          '& .MuiTab-root': {
+            fontSize: {
+              lg: 'medium',
+              md: 'small',
+              sm: 'x-small',
+              xs: 'xx-small',
+            },
+            padding: {
+              md: '16px',
+              sm: '8px',
+              xs: '4px',
+            },
+            minWidth: {
+              md: '90px',
+              xs: '50px',
+            },
+          },
+        }}
         value={activeTabIndex}
         onChange={(_event, index) => setActiveTabIndex(index)}>
         {panels.map((panel) => (
