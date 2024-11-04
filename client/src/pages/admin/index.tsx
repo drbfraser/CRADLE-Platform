47bd06e115a50709201a5b6818c7ce85b2e293b5
Ryan Milligan <rcm9@sfu.ca>
2024-11-04 13:01:34 -0800

    refactor: use custom tabs

--- client/src/pages/admin/index.tsx
+++ client/src/pages/admin/index.tsx
@@ -5,7 +5,6 @@ import { ManageUsers } from './manageUsers/ManageUsers';
 import { ManagePatients } from './managePatients/ManagePatients';
 import { Route, Routes } from 'react-router-dom';
 import { CustomFormTemplate } from './manageFormTemplates/editFormTemplate/CustomFormTemplate';
-import { Tabs, Tab } from '@mui/material';
 import {
   useState,
   PropsWithChildren,
@@ -14,32 +13,28 @@ import {
   useCallback,
 } from 'react';
 import { DashboardPaper } from 'src/shared/components/dashboard/DashboardPaper';
+import { Tabs } from 'src/shared/components/Tabs/Tabs';
 
-const pages = [
+const panels = [
   {
-    name: 'Users',
+    label: 'Users',
     Component: ManageUsers,
-    path: 'users',
   },
   {
-    name: 'Health Care Facilities',
+    label: 'Health Care Facilities',
     Component: ManageFacilities,
-    path: 'facilities',
   },
   {
-    name: 'Form Templates',
+    label: 'Form Templates',
     Component: ManageFormTemplates,
-    path: 'form-templates',
   },
   {
-    name: 'Relay App',
+    label: 'Relay App',
     Component: ManageRelayApp,
-    path: 'app',
   },
   {
-    name: 'Patients',
+    label: 'Patients',
     Component: ManagePatients,
-    path: 'patients',
   },
 ];
 
@@ -47,83 +42,15 @@ export const AdminPage = () => {
   return (
     <Routes>
       <Route path={`form-templates/new`} element={<CustomFormTemplate />} />
-      {pages.map((page) => (
-        <Route key={page.path} path={page.path} element={<page.Component />} />
-      ))}
       <Route path="" element={<AdminDashboard />} />
     </Routes>
-  );
-};
-
-const adminTabContext = createContext<AdminTabContext>(null!);
-const AdminTabsContextProvider = ({ children }: PropsWithChildren) => {
-  const [activeTabIndex, setActiveTabIndex] = useState(0);
-
-  const context = {
-    activeTabIndex: activeTabIndex,
-    setActiveTabIndex: setActiveTabIndex,
-  };
-
-  return (
-    <adminTabContext.Provider value={context}>
-      {children}
-    </adminTabContext.Provider>
   );
 };
 
 const AdminDashboard = () => {
   return (
     <DashboardPaper>
-      <AdminTabsContextProvider>
-        <AdminDashboardTabs />
-      </AdminTabsContextProvider>
+      <Tabs panels={panels} />
     </DashboardPaper>
   );
-};
-const AdminDashboardTabs = () => {
-  const { activeTabIndex, setActiveTabIndex } =
-    useContext<AdminTabContext>(adminTabContext);
-  const handleChange = useCallback(
-    (event: React.SyntheticEvent, newValue: number) => {
-      setActiveTabIndex(newValue);
-    },
-    [setActiveTabIndex]
-  );
-
-  return (
-    <>
-      <Tabs value={activeTabIndex} onChange={handleChange}>
-        {pages.map((page) => (
-          <Tab key={page.path} label={page.name} />
-        ))}
-      </Tabs>
-
-      {pages.map((page, index) => (
-        <AdminTabPanel
-          key={page.path}
-          index={index}
-          activeTabIndex={activeTabIndex}>
-          <page.Component />
-        </AdminTabPanel>
-      ))}
-    </>
-  );
-};
-
-type AdminTabPanelProps = PropsWithChildren & {
-  index: number;
-  activeTabIndex: number;
-};
-const AdminTabPanel = ({
-  children,
-  index,
-  activeTabIndex,
-}: AdminTabPanelProps) => {
-  const show = index === activeTabIndex;
-  return show ? <>{children}</> : null;
-};
-
-type AdminTabContext = {
-  activeTabIndex: number;
-  setActiveTabIndex: React.Dispatch<React.SetStateAction<number>>;
 };
