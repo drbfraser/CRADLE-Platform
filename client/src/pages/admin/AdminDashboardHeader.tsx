04e798827975ea2cb71240845996a335fff3785c
Ryan Milligan <rcm9@sfu.ca>
2024-11-04 21:14:33 -0800

    feat: add AdminDashboardHeader

---
+++ client/src/pages/admin/AdminDashboardHeader.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Typography } from '@mui/material';
+import { PropsWithChildren } from 'react';
+
+type AdminDashboardHeaderProps = PropsWithChildren & {
+  title: string;
+};
+export const AdminDashboardHeader = ({
+  children,
+  title,
+}: AdminDashboardHeaderProps) => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        minHeight: {
+          md: '72px',
+          sm: '64px',
+          xs: '56px',
+        },
+        paddingY: {
+          xs: '8px',
+        },
+        paddingX: {
+          xs: '8px',
+          md: '12px',
+          lg: '16px',
+        },
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+      }}>
+      <Typography variant={'h4'} component={'h4'} sx={{ marginRight: '12px' }}>
+        {title}
+      </Typography>
+      {children}
+    </Box>
+  );
+};
