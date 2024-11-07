edb0accae600f7ea9f76b904d2f3a1f468f2408f
Ryan Milligan <rcm9@sfu.ca>
2024-11-06 17:47:31 -0800

    feat: add validator for user put request

--- server/validation/users.py
+++ server/validation/users.py
@@ -1,9 +1,11 @@
 import re
 from typing import List
 
-from pydantic import BaseModel, ValidationError, field_validator
+from pydantic import BaseModel, ValidationError, field_validator, model_validator
+from typing_extensions import Self
 
 from enums import RoleEnum
+from server.shared.user_utils import UserUtils
 from shared.phone_number_utils import PhoneNumberUtils
 from validation.validation_exception import ValidationExceptionError
 
@@ -23,18 +25,17 @@ class UserValidator(BaseModel):
         if value not in supported_roles:
             error = {"message": "Not a supported role"}
             raise ValueError(error)
-
         return value
 
     @field_validator("email", mode="before")
     @classmethod
-    def validate_email(cls, email: str):
+    def validate_email_format(cls, email: str):
         # Validate email format.
         email_regex_pattern = r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
         email = email.lower()
         if re.match(email_regex_pattern, email) is None:
             raise ValueError(f"Email ({email}) is invalid.")
-        return email
+        return email.lower()
 
     @field_validator("phone_numbers", mode="before")
     @classmethod
@@ -79,6 +80,17 @@ class UserValidator(BaseModel):
 class UserRegisterValidator(UserValidator):
     password: str
 
+    # Override base class after field validator for email.
+    @field_validator("email", mode="after")
+    @classmethod
+    def validate_email_uniqueness(cls, email: str):
+        # Email should already be converted to lowercase.
+        print(f"Validate email uniqueness: ({email})")
+        # Validate that email isn't already taken.
+        if UserUtils.does_email_exist(email):
+            raise ValueError(f"Email ({email}) is already in use.")
+        return email
+
     @staticmethod
     def validate(request_body: dict):
         try:
@@ -86,3 +98,18 @@ class UserRegisterValidator(UserValidator):
         except ValidationError as e:
             error_message = str(e.errors()[0]["msg"])
             raise ValidationExceptionError(error_message)
+
+class UserPutRequestValidator(UserValidator):
+    """
+    Pydantic validation model for the `/api/user/<int:userId> [PUT]`
+    api endpoint.
+    """
+
+    id: int
+
+    @model_validator(mode="after")
+    def validate_email_uniqueness(self) -> Self:
+        # Validate that email doesn't belong to another user.
+        if not UserUtils.is_email_unique_to_user(self.email, self.id):
+            raise ValueError(f"Email ({self.email}) is already in use.")
+        return self
