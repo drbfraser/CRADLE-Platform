6789fe16d6f79291d86235d8612874d634eb5e82
Ryan Milligan <rcm9@sfu.ca>
2024-11-07 17:48:53 -0800

    refactor: use title case for facility names

--- server/shared/health_facility_utils.py
+++ server/shared/health_facility_utils.py
@@ -5,21 +5,25 @@ from shared.phone_number_utils import PhoneNumberUtils
 
 valid_facility_types = [valid_type.value for valid_type in FacilityTypeEnum]
 
+
 class HealthFacilityUtils:
     @staticmethod
     def does_facility_exist(facility_name: str):
         facility_name = facility_name.lower()
         facility_model = crud.read(HealthFacilityOrm, name=facility_name)
         return facility_model is not None
+
     # End of function.
 
     @staticmethod
-    def create_health_facility(facility_name: str,
-                               phone_number: str,
-                               facility_type: str,
-                               location: str,
-                               about: str):
-        facility_name = facility_name.lower()
+    def create_health_facility(
+        facility_name: str,
+        phone_number: str,
+        facility_type: str,
+        location: str,
+        about: str,
+    ):
+        facility_name = facility_name.title()
 
         if facility_type not in valid_facility_types:
             raise RuntimeError(f"Type ({facility_type}) is not a valid facility type.")
@@ -34,16 +38,21 @@ class HealthFacilityUtils:
         formatted_phone_number = PhoneNumberUtils.format_to_E164(phone_number)
 
         # Check if phone number is unique.
-        if PhoneNumberUtils.does_E164_phone_number_exist(formatted_phone_number):
+        if PhoneNumberUtils.does_phone_number_exist(formatted_phone_number):
             raise RuntimeError(f"Phone number ({phone_number}) is already assigned.")
 
         # Check if facility already exists.
         if HealthFacilityUtils.does_facility_exist(facility_name):
             raise RuntimeError(f"Facility ({facility_name}) already exists.")
 
-        crud.create(HealthFacilityOrm(name=facility_name,
-                                        phone_number=formatted_phone_number,
-                                        type=facility_type,
-                                        location=location,
-                                        about=about))
+        crud.create(
+            HealthFacilityOrm(
+                name=facility_name,
+                phone_number=formatted_phone_number,
+                type=facility_type,
+                location=location,
+                about=about,
+            )
+        )
+
     # End of function.
