842208784d69ee0dcf9f2a21c7249e251bfe430d
Ryan Milligan <rcm9@sfu.ca>
2024-11-09 18:35:45 -0800

    refactor: remove redundant seed logic

--- server/manage.py
+++ server/manage.py
@@ -1,6 +1,5 @@
 import datetime
 import json
-import os
 import random
 import string
 import time
@@ -13,12 +12,16 @@ import numpy as np
 from flask.cli import FlaskGroup
 
 import models
-from api.util import create_secret_key_for_user
 from data import crud, marshal
-from enums import RoleEnum
 from models import db
-from seed_users import clear_user_pool as empty_user_pool
-from seed_users import populate_test_users, seed_facilities
+from seed_users import (
+    clear_user_pool as empty_user_pool,
+)
+from seed_users import (
+    facilities_list,
+    seed_minimal_users,
+    seed_test_users,
+)
 
 # cli = FlaskGroup(app)
 cli = FlaskGroup()
@@ -38,21 +41,18 @@ def drop_all_tables():
     db.drop_all()
     db.session.commit()
 
+
 # USAGE: python manage.py seed_users
 @cli.command("seed_users")
 def seed_users():
-    seed_facilities()
-    populate_test_users()
+    seed_test_users()
 
-# USAGE: python manage.py seed_facilities
-@cli.command("seed_facilities")
-def seed_health_facilities():
-    seed_facilities()
 
 # USAGE: python manage.py clear_user_pool
 @cli.command("clear_user_pool")
 def clear_user_pool():
     empty_user_pool()
+
 
 # Extracts a username from the email address of a user - Only used in manage.py to generate seed test data
 def get_username_from_email(email):
@@ -67,11 +67,7 @@ def get_username_from_email(email):
 
 # USAGE: python manage.py seed_minimal
 @cli.command("seed_minimal")
-def seed_minimal(
-    email="admin@admin.com",
-    password="Admin_123",
-
-):
+def seed_minimal():
     """
     Seeds the database with the minimum amount of data required for it to be functional.
 
@@ -85,21 +81,7 @@ def seed_minimal(
     Defaults can be overridden, such as:
        python ./manage.py seed_minimal --email="abc@test.com" --password="TeyHo5@e!0B" --facility_name="Sunny Creek"
     """
-    print("Seeding health facility...")
-    facility_name = "H0000"
-    create_health_facility(facility_name=facility_name, phone_number="555-555-0000")
-
-    print("Creating admin user...")
-    create_user(
-        email,
-        "Admin",
-        password,
-        facility_name,
-        RoleEnum.ADMIN.value,
-        ["+1-888-456-7890", "+1-098-765-4321", os.environ.get("EMULATOR_PHONE_NUMBER")],
-        1,
-    )
-
+    seed_minimal_users()
     print("Finished seeding minimal data set")
 
 
@@ -112,58 +94,8 @@ def seed_test_data(ctx):
 
     The data inserted here should be deterministically generated to ease testing.
     """
-    # Start with a minimal setup.
-    ctx.invoke(seed_minimal)
-
-    # Add the rest of the users.
-    print("Creating test health facilities and users...")
-    create_health_facility("H1000", phone_number="555-555-1000")
-    create_health_facility("H2000", phone_number="555-555-2000")
-    create_user(
-        "brian@admin.com",
-        "Brian",
-        "brian123",
-        "H0000",
-        RoleEnum.ADMIN.value,
-        ["+1-604-123-4567", "+1-604-123-4568"],
-        2,
-    )
-    create_user(
-        "vht@vht.com",
-        "TestVHT",
-        "vht123",
-        "H0000",
-        RoleEnum.VHT.value,
-        ["555-555-5555", "666-666-6666", "777-777-7777"],
-        3,
-    )
-    create_user(
-        "vht2@vht.com",
-        "TestVHT2",
-        "vht123",
-        "H1000",
-        RoleEnum.VHT.value,
-        ["+256-415-123456", "+256-415-123457", "+256-415-123458", "+256-415-123459"],
-        4,
-    )
-    create_user(
-        "hcw@hcw.com",
-        "TestHCW",
-        "hcw123",
-        "H0000",
-        RoleEnum.HCW.value,
-        ["+256-416-123456"],
-        5,
-    )
-    create_user(
-        "cho@cho.com",
-        "TestCHO",
-        "cho123",
-        "H0000",
-        RoleEnum.CHO.value,
-        ["+256-417-123456"],
-        6,
-    )
+    # Seed users and health facilities.
+    seed_test_users()
 
     print("Creating test patients, readings, referrals, and records...")
     create_patient_reading_referral_pregnancy(
@@ -278,6 +210,7 @@ def seed_test_patient():
 @click.pass_context
 def seed(ctx):
     start = time.time()
+    ctx.invoke(seed_test_data)
 
     # SEED villages
     print("Seeding Villages...")
@@ -285,23 +218,6 @@ def seed(ctx):
     for village in village_list:
         v_schema = {"village_number": village}
         db.session.add(village_schema.load(v_schema))
-
-    # SEED health facilities
-    print("Seeding health facilities...")
-
-    health_facility_schema = models.HealthFacilitySchema()
-    for index, hf in enumerate(facility_locations):
-        hf_schema = {
-            "name": get_facility_name(index),
-            "phone_number": get_facility_phone_number(hf["areaCode"]),
-            "type": get_facility_type(),
-            "about": get_facility_about(),
-            "location": hf["city"],
-            "new_referrals": str(round(time.time() * 1000)),
-        }
-        db.session.add(health_facility_schema.load(hf_schema))
-
-    ctx.invoke(seed_test_data)
 
     print("Seeding Patients with readings and referrals...")
     # seed patients with readings and referrals
@@ -328,7 +244,6 @@ def seed(ctx):
             pregnant = False
         else:
             pregnant = bool(random.getrandbits(1))
-
 
         gestational_timestamp = None
 
@@ -415,75 +330,6 @@ def seed(ctx):
     print(f"The seed script took: {round(end - start, 3)} seconds")
 
 
-# Creates a user and adds it to the database
-def create_user(email, name, password, health_facility_name, role, phone_numbers, user_id):
-    # Check if the email already exists
-    existing_user = models.UserOrm.query.filter_by(username=name).first()
-    if existing_user:
-        print(f"User with username '{name}' already exists.")
-        return None
-
-    # Create a new User instance
-    new_user = models.UserOrm(
-        id=user_id,
-        name=name,
-        email=email,
-        username=get_username_from_email(email),
-        health_facility_name=health_facility_name,
-        role=role,
-    )
-
-    new_phone_numbers = []
-    for phone_number in phone_numbers:
-        # Check if the phone number already exists
-        existing_phone = models.UserPhoneNumberOrm.query.filter_by(
-            number=phone_number,
-        ).first()
-        if existing_phone:
-            print(
-                f"Phone number '{phone_number}' is already associated with another user.",
-            )
-            return None
-
-        # Create a new UserPhoneNumber instance and associate it with the user
-        new_phone_numbers.append(
-            models.UserPhoneNumberOrm(number=phone_number, user=new_user),
-        )
-
-    try:
-        # Add the new user and phone numbers to the database
-        db.session.add(new_user)
-        db.session.add_all(new_phone_numbers)
-        db.session.commit()
-        create_secret_key_for_user(user_id)
-        print(f"User '{name}' created successfully.")
-        return new_user
-    except Exception as e:
-        db.session.rollback()
-        print(f"Failed to create user: {e}")
-        return None
-
-
-def create_health_facility(
-    facility_name,
-    phone_number,
-    facility_type="HOSPITAL",
-    location="Sample Location",
-    about="Sample health centre",
-):
-    facility = {
-        "name": facility_name,
-        "type": facility_type,
-        "phone_number": phone_number,
-        "location": location,
-        "about": about,
-        "new_referrals": str(round(time.time() * 1000)),
-    }
-    schema = models.HealthFacilitySchema()
-    db.session.add(schema.load(facility))
-    db.session.commit()
-
-
 def create_patient_reading_referral_pregnancy(
     patient_id,
     reading_id,
@@ -555,8 +401,8 @@ def create_patient_reading_referral_pregnancy(
     db.session.add(patient_schema.load(patient))
     db.session.commit()
 
-    readingModel = marshal.unmarshal(models.ReadingOrm, reading)
-    crud.create(readingModel, refresh=True)
+    reading_orm = marshal.unmarshal(models.ReadingOrm, reading)
+    crud.create(reading_orm, refresh=True)
 
     referral_schema = models.ReferralSchema()
     db.session.add(referral_schema.load(referral))
@@ -598,7 +444,7 @@ def create_medical_record(patient_id, info, is_drug_record, date_created=1622541
 
 
 def create_patient_association(patient_id, user_id):
-    association = {"patientId": patient_id, "user_id": user_id}
+    association = {"patient_id": patient_id, "user_id": user_id}
     schema = models.PatientAssociationsSchema()
     db.session.add(schema.load(association))
     db.session.commit()
@@ -930,25 +776,6 @@ def generate_phone_numbers():
     return numbers
 
 
-def get_facility_phone_number(area_code):
-    return facility_phone_numbers[area_code]
-
-
-def generate_health_facilities():
-    n = len(facility_locations)
-
-    # Sets are unique element lists, prevents from having duplicates
-    facilities = set()
-    while len(facilities) < n:
-        facilities.add(
-            "H" + "".join([f"{randint(0, 9)}" for num in range(4)]),
-        )
-
-    facilities = list(facilities)
-
-    return sorted(facilities)
-
-
 def generate_villages():
     n = len(facility_locations)
     villages = set()
@@ -997,7 +824,7 @@ if __name__ == "__main__":
 
     users_list = [1, 2, 3, 4]
     village_list = generate_villages()
-    health_facility_list = generate_health_facilities()
+    health_facility_list = facilities_list
 
     facility_type = ["HCF_2", "HCF_3", "HCF_4", "HOSPITAL"]
     facility_about = [
@@ -1006,8 +833,6 @@ if __name__ == "__main__":
         "Has specialized equipment",
         "Urgent requests only",
     ]
-
-    facility_phone_numbers = generate_phone_numbers()
 
     symptoms_list = ["HEADACHE", "BLURRED VISION", "ABDO PAIN", "BLEEDING", "FEVERISH"]
     sex_list = ["FEMALE", "MALE"]
