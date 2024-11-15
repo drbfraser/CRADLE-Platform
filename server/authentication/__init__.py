2ffa82e0ca8b6cce6d42cafe4a57e26fda54a2b7
Ryan Milligan <rcm9@sfu.ca>
2024-11-14 18:37:34 -0800

    refactor: add COGNITO prefix

--- server/authentication/__init__.py
+++ server/authentication/__init__.py
@@ -20,15 +20,15 @@ COGNITO_USER_POOL_ID = os.environ["COGNITO_USER_POOL_ID"]
 COGNITO_APP_CLIENT_ID = os.environ["COGNITO_APP_CLIENT_ID"]
 COGNITO_CLIENT_SECRET = os.environ["COGNITO_CLIENT_SECRET"]
 
-AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+COGNITO_AWS_ACCESS_KEY_ID = os.environ["COGNITO_AWS_ACCESS_KEY_ID"]
+COGNITO_AWS_SECRET_ACCESS_KEY = os.environ["COGNITO_AWS_SECRET_ACCESS_KEY"]
 
 cognito = CognitoClientWrapper(
     cognito_idp_client=boto3.client(
         service_name="cognito-idp",
         region_name=AWS_REGION,
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        aws_access_key_id=COGNITO_AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=COGNITO_AWS_SECRET_ACCESS_KEY,
     ),
     user_pool_id=COGNITO_USER_POOL_ID,
     client_id=COGNITO_APP_CLIENT_ID,
