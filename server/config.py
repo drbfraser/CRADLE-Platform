0a05bbfaba8efc8e0812b02977ee57b14ee01f9f
Ryan Milligan <rcm9@sfu.ca>
2024-11-14 17:40:25 -0800

    style: run ruff format

--- server/config.py
+++ server/config.py
@@ -125,12 +125,17 @@ app.config.from_object(Config)
 jwt = JWTManager(app)
 app.json_encoder = JSONEncoder
 
-db = SQLAlchemy(app, metadata=MetaData(naming_convention={
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
-}))
+db = SQLAlchemy(
+    app,
+    metadata=MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    ),
+)
 migrate = Migrate(app, db, compare_type=True)
 ma = Marshmallow(app)
--- server/validation/validation_exception.py
+++ server/validation/validation_exception.py
@@ -1,4 +1,2 @@
-
-
 class ValidationExceptionError(Exception):
     pass
