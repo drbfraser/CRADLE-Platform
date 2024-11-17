7d1dbcd2865894a0ea7267c7ee6752bd8810d417
Ryan Milligan <rcm9@sfu.ca>
2024-11-17 15:18:01 -0800

    feat: add initial migration

---
+++ server/migrations/versions/5641d7c32ebd_initial_migration.py
@@ -0,0 +1,24 @@
+"""
+initial migration
+
+Revision ID: 5641d7c32ebd
+Revises:
+Create Date: 2024-11-17 23:12:45.609884
+
+"""
+
+
+
+# revision identifiers, used by Alembic.
+revision = "5641d7c32ebd"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
