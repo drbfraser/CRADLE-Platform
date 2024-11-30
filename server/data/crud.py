56380a9bcf1d019ef8f406cbb088ebb3246f306c
Ryan Milligan <rcm9@sfu.ca>
2024-11-29 22:21:12 -0800

    fix: fix patient_id

--- server/data/crud.py
+++ server/data/crud.py
@@ -1265,7 +1265,7 @@ def __filter_by_patient_association(
     is_cho,
 ) -> Query:
     if user_id is not None:
-        if model.patient_id is not None:
+        if hasattr(model, model.patient_id):
             join_column = model.patient_id
         else:
             join_column = model.id
