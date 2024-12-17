6e5fdcb18c4d5fdff1ebfd94930236973546d28f
HUIYAN LI <hla110@sfu.ca>
2024-12-17 12:07:37 -0800

    fix: corrected misspelled field name

--- server/tests/validation/test_facilities.py
+++ server/tests/validation/test_facilities.py
@@ -6,28 +6,28 @@ from validation.validation_exception import ValidationExceptionError
 FACILITY_NAME = "H12"
 PHONE_NUMBER = "+1-604-715-2845"
 LOCATION = "some location"
-DESCIPTION = "Biggest hospital"
+DESCRIPTION = "Biggest hospital"
 FACILITY_TYPE = "HOSPITAL"
 
 facility_with_valid_fields_should_return_none = {
     "name": FACILITY_NAME,
     "phone_number": PHONE_NUMBER,
     "location": LOCATION,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": FACILITY_TYPE,
 }
 
 facility_missing_optional_field_phone_number_should_return_none = {
     "name": FACILITY_NAME,
     "location": LOCATION,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": FACILITY_TYPE,
 }
 
 facility_missing_optional_field_location_should_return_none = {
     "name": FACILITY_NAME,
     "phone_number": PHONE_NUMBER,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": FACILITY_TYPE,
 }
 
@@ -42,13 +42,13 @@ facility_missing_optional_field_type_should_return_none = {
     "name": FACILITY_NAME,
     "phone_number": PHONE_NUMBER,
     "location": LOCATION,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
 }
 
 facility_missing_required_field_name_should_throw_exception = {
     "phone_number": PHONE_NUMBER,
     "location": LOCATION,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": FACILITY_TYPE,
 }
 
@@ -56,7 +56,7 @@ facility_field_name_has_wrong_type_should_throw_exception = {
     "name": 1,
     "phone_number": PHONE_NUMBER,
     "location": LOCATION,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": FACILITY_TYPE,
 }
 
@@ -64,7 +64,7 @@ facility_field_phone_number_has_wrong_type_should_throw_exception = {
     "name": FACILITY_NAME,
     "phone_number": 1,
     "location": 1,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": FACILITY_TYPE,
 }
 
@@ -72,7 +72,7 @@ facility_field_location_has_wrong_type_should_throw_exception = {
     "name": FACILITY_NAME,
     "phone_number": PHONE_NUMBER,
     "location": 1,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": FACILITY_TYPE,
 }
 
@@ -88,7 +88,7 @@ facility_field_type_has_wrong_type_should_throw_exception = {
     "name": FACILITY_NAME,
     "phone_number": PHONE_NUMBER,
     "location": LOCATION,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": 1,
 }
 
@@ -96,7 +96,7 @@ facility_field_type_is_not_FacilityTypeEnum_should_throw_exception = {
     "name": FACILITY_NAME,
     "phone_number": PHONE_NUMBER,
     "location": LOCATION,
-    "about": DESCIPTION,
+    "about": DESCRIPTION,
     "type": "wrong FacilityTypeEnum",
 }
 
