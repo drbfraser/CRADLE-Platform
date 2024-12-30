3e874ff84b267b9d63f8fe18083380f62fd847cc
Ryan Milligan <rcm9@sfu.ca>
2024-12-30 11:35:34 -0800

    feat: add model_config to CradleBaseModel

--- server/validation/__init__.py
+++ server/validation/__init__.py
@@ -1,15 +1,19 @@
 from typing import Any
 
 from humps import decamelize
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, ConfigDict, model_validator
 
 
 class CradleBaseModel(BaseModel):
     """
-    Pydantic Base Model that converts input data to be in snake case.
+    Pydantic Base Model to define shared logic and configuration.
     All of our Pydantic Models should inherit from this instead of `BaseModel`.
     """
 
+    # Common configs shared by all of our Pydantic models.
+    model_config = ConfigDict(validate_assignment=True, use_enum_values=True)
+
+    # Convert input data to be in snake case.
     @model_validator(mode="before")
     @classmethod
     def convert_to_snake_case(cls, data: Any):
