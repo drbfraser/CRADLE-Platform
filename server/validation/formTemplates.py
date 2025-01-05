54c06c45e1c780501eb93ba7adee287e95fe6b57
Ryan Milligan <rcm9@sfu.ca>
2025-01-05 13:04:14 -0800

    fix: fix validate_questions()

--- server/validation/formTemplates.py
+++ server/validation/formTemplates.py
@@ -1,11 +1,10 @@
 from typing import List, Optional
 
-from pydantic import ValidationError
+from pydantic import field_validator
 
 from service import questionTree
 from validation import CradleBaseModel
 from validation.questions import TemplateQuestionValidator
-from validation.validation_exception import ValidationExceptionError
 
 
 class ClassificationValidator(CradleBaseModel):
@@ -19,67 +18,48 @@ class FormTemplateValidator(CradleBaseModel, extra="forbid"):
     questions: List[TemplateQuestionValidator]
     id: Optional[str] = None
 
-    @staticmethod
-    def validate(request_body: dict):
-        """
-        Raises an error if the template part in /api/forms/templates POST or PUT
-        request is not valid.
-
-        :param request_body: The request body as a dict object
-        """
-        try:
-            return FormTemplateValidator(**request_body)
-        except ValidationError as e:
-            print(e)
-            raise ValidationExceptionError(str(e.errors()[0]["msg"]))
-
-    @staticmethod
-    def validate_questions(questions: list):
+    @field_validator("questions", mode="after")
+    @classmethod
+    def validate_questions(cls, questions: List[TemplateQuestionValidator]):
         """
         Raises an error if the questions part in /api/forms/templates POST or PUT
         request is not valid (json format, lang versions consistency, question_index constraint).
 
         :param questions: The request body as a dict object
         """
-        lang_version_list, question_index = None, None
+        lang_version_list = None
+        question_index = 0
         for index, question in enumerate(questions):
-            # validate each question
-            try:
-                TemplateQuestionValidator(**question)
-            except ValidationError as e:
-                print(e)
-                raise ValidationExceptionError(str(e.errors()[0]["msg"]))
-
             # further validate for extra requirements:
             # lang versions consistency: all questions should have same kinds of versions
             # question_index constraint: question index in ascending order
             if index == 0:
-                lang_version_list = [
-                    v.get("lang") for v in question.get("question_lang_versions")
-                ]
+                lang_version_list = [v.lang for v in question.question_lang_versions]
                 lang_version_list.sort()
 
-                question_index = question.get("question_index")
+                question_index = question.question_index
             else:
                 tmp_lang_version_list = [
-                    v.get("lang") for v in question.get("question_lang_versions")
+                    v.lang for v in question.question_lang_versions
                 ]
                 tmp_lang_version_list.sort()
                 if tmp_lang_version_list != lang_version_list:
-                    raise ValidationExceptionError(
+                    raise ValueError(
                         "lang versions provided between questions are not consistent",
                     )
 
-                cur_question_index = question.get("question_index")
+                cur_question_index = question.question_index
                 if question_index < cur_question_index:
                     question_index = cur_question_index
                 else:
-                    raise ValidationExceptionError(
-                        "questions should be in index-ascending order",
+                    raise ValueError(
+                        "Questions should be in index-ascending order.",
                     )
 
         # validate question question_index tree dfs order
-        error = questionTree.is_dfs_order(questions)
-        if error:
+        question_list = [question.model_dump() for question in questions]
+        error = questionTree.is_dfs_order(question_list)
+        if error is not None:
             print(error)
-            raise ValidationExceptionError(str(error))
+            raise ValueError(error)
+        return questions
