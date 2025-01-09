3892437fcaf0e27b62e5f4498b7a61cb78439e46
Ryan Milligan <rcm9@sfu.ca>
2025-01-09 15:13:11 -0800

    fix: remove ValidationException

--- server/validation/users.py
+++ server/validation/users.py
@@ -1,16 +1,11 @@
 import re
-from typing import List
 
-from pydantic import (
-    ValidationError,
-    field_validator,
-)
+from pydantic import field_validator
 
 from common.commonUtil import format_phone_number, is_valid_email_format
 from common.constants import USERNAME_REGEX_PATTERN
 from enums import RoleEnum
 from validation import CradleBaseModel
-from validation.validation_exception import ValidationExceptionError
 
 supported_roles = [role.value for role in RoleEnum]
 
@@ -22,6 +17,14 @@ class UserValidator(CradleBaseModel):
     modify certain fields, this base class only defines the fields which are
     allowed to be changed. This way, the model for validating PUT requests does
     not need to include fields which it should not be able to change.
+
+    {
+        "name": "Jane",
+        "email": "jane@mail.com",
+        "health_facility_name": "facility7",
+        "role": "admin",
+        "phone_numbers": [ "+604-555-1234" ]
+    }
     """
 
     email: str
@@ -57,36 +60,11 @@ class UserValidator(CradleBaseModel):
 
     @field_validator("phone_numbers")
     @classmethod
-    def format_phone_numbers(cls, phone_numbers: List[str]) -> List[str]:
+    def format_phone_numbers(cls, phone_numbers: list[str]) -> list[str]:
         formatted_phone_numbers: set[str] = set()
         for phone_number in phone_numbers:
             formatted_phone_numbers.add(format_phone_number(phone_number))
         return list(formatted_phone_numbers)
-
-    @staticmethod
-    def validate(request_body: dict):
-        """
-        Returns an error message if the /api/user post or put request
-        is not valid. Else, returns None.
-
-        :param request_body: The request body as a dict object
-                            {
-                                "name": "Jane",
-                                "email": "jane@mail.com",
-                                "health_facility_name": "facility7",
-                                "role": "admin",
-                                "phone_numbers": [ "+604-555-1234" ]
-                            }
-        :throw: An error if the request body is invalid. None otherwise
-        :return pydantic model representation of the request body param
-        """
-        try:
-            # Pydantic will validate field presence and type
-            return UserValidator(**request_body)
-        except ValidationError as e:
-            # Extracts the first error message from the validation errors list
-            error_message = str(e.errors()[0]["msg"])
-            raise ValidationExceptionError(error_message)
 
 
 class UserRegisterValidator(UserValidator):
@@ -111,15 +89,6 @@ class UserRegisterValidator(UserValidator):
                 f"Username ({username}) is invalid. Username must start with a letter and must contain only alphanumeric or underscore characters."
             )
         return username
-
-    @staticmethod
-    def validate(request_body: dict):
-        try:
-            return UserRegisterValidator(**request_body)
-        except ValidationError as e:
-            error_message = str(e.errors()[0]["msg"])
-            print(e)
-            raise ValidationExceptionError(error_message)
 
 
 class UserAuthValidator(CradleBaseModel, extra="forbid"):
