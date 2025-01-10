a989ac75b10a1892458a9156a4a459818e01a6f9
Ryan Milligan <rcm9@sfu.ca>
2025-01-09 18:26:37 -0800

    feat: add SmsKeyExamples

--- server/validation/sms_key.py
+++ server/validation/sms_key.py
@@ -1,6 +1,20 @@
 from validation import CradleBaseModel
 
 
+class SmsKeyExamples:
+    _key = "717c82dcc3814541b9ec502f4f753198261c6191457c0cb2626c33effaf2d520"
+    _message = "NORMAL"
+    _expiry_date = "2025-02-18 23:20:17"
+    _stale_date = "2025-02-08 23:20:17"
+
+    valid_example = {
+        "key": _key,
+        "message": _message,
+        "expiry_date": _expiry_date,
+        "stale_date": _stale_date,
+    }
+
+
 class SmsKeyModel(CradleBaseModel):
     key: str
     message: str
