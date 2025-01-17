dde0e6aef69060f986962137df48050d50a9bd40
Harry Nguyen <hln5@sfu.ca>
2025-01-17 14:11:43 -0800

    refactor(Patient summary): include patient name in header and replace deprecated version of MUI Grid with new version

--- client/src/pages/patient/Header.tsx
+++ client/src/pages/patient/Header.tsx
@@ -1,16 +1,17 @@
-import { Grid, IconButton, Tooltip, Typography } from '@mui/material';
-
+import { useNavigate } from 'react-router-dom';
+import { IconButton, Tooltip, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import { Add } from '@mui/icons-material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import Skeleton from '@mui/material/Skeleton';
+
 import { Patient } from 'src/shared/types';
 import { PrimaryButton } from 'src/shared/components/Button';
-import Skeleton from '@mui/material/Skeleton';
-import { useNavigate } from 'react-router-dom';
 
 interface IProps {
   patient?: Patient;
   isThereAPendingReferral: boolean;
-  setConfirmDialogPerformAssessmentOpen: any;
+  setConfirmDialogPerformAssessmentOpen: (isOpen: boolean) => void;
 }
 
 export const Header = ({
@@ -19,6 +20,7 @@ export const Header = ({
   setConfirmDialogPerformAssessmentOpen,
 }: IProps) => {
   const navigate = useNavigate();
+
   const handleSubmitNewFormClick = () => {
     if (patient) {
       navigate(`/forms/new/${patient.id}`);
@@ -47,42 +49,39 @@ export const Header = ({
 
   return (
     <Grid container justifyContent="space-between" mb={2}>
-      <Grid item>
-        <Grid container alignItems="center">
-          <Tooltip title="Go back" placement="top">
-            <IconButton onClick={() => navigate('/patients')} size="large">
-              <ChevronLeftIcon color="inherit" fontSize="large" />
-            </IconButton>
-          </Tooltip>
-          <Typography variant="h4">
-            {patient ? (
-              `Patient Summary for ${patient.id}`
-            ) : (
-              <Skeleton width={500} />
-            )}
-          </Typography>
-        </Grid>
+      <Grid container alignItems="center">
+        <Tooltip title="Go back" placement="top">
+          <IconButton onClick={() => navigate('/patients')} size="large">
+            <ChevronLeftIcon color="inherit" fontSize="large" />
+          </IconButton>
+        </Tooltip>
+        <Typography variant="h4">
+          {patient ? (
+            `Patient Summary for: ${patient.name} (${patient.id})`
+          ) : (
+            <Skeleton width={500} />
+          )}
+        </Typography>
       </Grid>
-      <Grid item>
-        <Grid
-          container
-          alignItems="center"
-          gap={1}
-          alignContent="center"
-          height={'100%'}>
-          <PrimaryButton onClick={handleSubmitNewFormClick}>
-            <Add /> Submit New Form
-          </PrimaryButton>
-          <PrimaryButton onClick={handleAddReadingClick}>
-            <Add /> Add New Reading
-          </PrimaryButton>
-          <PrimaryButton onClick={handleCreateReferralClick}>
-            <Add /> Create Referral
-          </PrimaryButton>
-          <PrimaryButton onClick={handlePerformAssessmentClick}>
-            <Add /> Perform Assessment
-          </PrimaryButton>
-        </Grid>
+
+      <Grid
+        container
+        alignItems="center"
+        gap={1}
+        alignContent="center"
+        height={'100%'}>
+        <PrimaryButton onClick={handleSubmitNewFormClick}>
+          <Add /> Submit New Form
+        </PrimaryButton>
+        <PrimaryButton onClick={handleAddReadingClick}>
+          <Add /> Add New Reading
+        </PrimaryButton>
+        <PrimaryButton onClick={handleCreateReferralClick}>
+          <Add /> Create Referral
+        </PrimaryButton>
+        <PrimaryButton onClick={handlePerformAssessmentClick}>
+          <Add /> Perform Assessment
+        </PrimaryButton>
       </Grid>
     </Grid>
   );
