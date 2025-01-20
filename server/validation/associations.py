25ee00eed3cc2978da0f736bf85e309e46404e25
Ryan Milligan <rcm9@sfu.ca>
2025-01-20 15:22:52 -0800

    docs: add example for PatientAssociationModel

--- server/validation/associations.py
+++ server/validation/associations.py
@@ -4,14 +4,16 @@ from validation import CradleBaseModel
 
 
 class PatientAssociationModel(CradleBaseModel):
-    """
-    {
-        "patient_id": 47, - required
-        "health_facility_name": "H0000",
-        "user_id": 1,
-    }
-    """
-
     patient_id: int
     health_facility_name: Optional[str] = None
     user_id: Optional[int] = None
+
+    model_config = dict(
+        openapi_extras={
+            "example": {
+                "patient_id": 47,
+                "health_facility_name": "H0000",
+                "user_id": 1,
+            }
+        }
+    )
