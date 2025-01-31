3558a98668a2b5b9efc436fb159b23a366e27192
Ryan Milligan <rcm9@sfu.ca>
2025-01-31 14:20:14 -0800

    refactor: delete unused api util functions

--- server/common/api_utils.py
+++ server/common/api_utils.py
@@ -3,7 +3,6 @@ import re
 from typing import Optional
 
 from flask import request
-from humps import decamelize
 from pydantic import AliasChoices, Field, field_validator
 from pydantic.alias_generators import to_snake
 
@@ -12,65 +11,8 @@ from config import app
 from validation import CradleBaseModel
 
 
-def get_query_params() -> dict:
-    """
-    Extracts URL search params contained in the request
-    and converts the keys to snake case.
-
-    :param request: Flask request object
-    :return: URL search params converted to snake case,
-      stored in a dictionary
-    """
-    request_args: dict = decamelize(request.args)
-
-    params = {
-        "search_text": request_args.get("search"),
-        "order_by": request_args.get("sort_by"),
-        "direction": request_args.get("sort_dir"),
-        "limit": request_args.get("limit"),
-        "page": request_args.get("page"),
-        "date_range": request_args.get("date_range"),
-        "readings": request_args.get("readings"),
-        "referrals": request_args.get("referrals"),
-        "assessments": request_args.get("assessments"),
-        "forms": request_args.get("forms"),
-        "lang": request_args.get("lang"),
-        "is_assessed": request_args.get("is_assessed"),
-        "is_pregnant": request_args.get("is_pregnant"),
-        "include_archived": request_args.get("include_archived"),
-        "vital_signs": list(
-            filter(None, decamelize(request.args.getlist("vital_signs")))
-        ),
-        "referrers": list(filter(None, decamelize(request.args.getlist("referrer")))),
-        "health_facilities": list(
-            filter(None, decamelize(request.args.getlist("health_facility")))
-        ),
-    }
-
-    # If not None, order_by will be assigned a field name, so it too must
-    # me converted to snake case.
-    order_by = params.get("order_by")
-    if order_by is not None:
-        params["order_by"] = decamelize(order_by)
-
-    return {k: v for k, v in params.items() if v and k is not None}
-
-
-def get_query_param_bool(name: str) -> bool:
-    """
-    Returns true if the request URL contains a boolean query parameter with a given
-    ``name`` who's value is "true".
-
-    :param request: A request
-    :param name: The name of the parameter to check for
-    :return: True if the value for the parameter is "true", otherwise False.
-    """
-    request_args: dict = decamelize(request.args)
-    return request_args.get(name, "false") == "true"
-
-
 @app.after_request
-def log_request_details(response):
+def _log_request_details(response):
     """
     middleware function for logging changes made by users
     """
