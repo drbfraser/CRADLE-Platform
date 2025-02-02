7a78909838b8f9fab8fc7f1524b512880de8e53f
Ryan Milligan <rcm9@sfu.ca>
2025-02-02 11:30:19 -0800

    docs: modify comments

--- client/src/shared/api/validation/user.ts
+++ client/src/shared/api/validation/user.ts
@@ -2,17 +2,6 @@ import { z } from 'zod';
 import { UserRoleEnum } from 'src/shared/enums';
 import { smsKeySchema } from './smsKey';
 
-/**
- * Oct 24, 2024 - It seems that for the last 2 years, there has been a pretty
- * major bug with this User model definition. The "phoneNumber" attribute was
- * not aligned with the "phoneNumbers" attribute of the objects returned from
- * the server. Apparently the front-end was completely unaware of this
- * one-to-many relationship where users can have multiple phone numbers.
- * Consequently there are various things in the client-side code which expect
- * a "phoneNumber" attribute, so I have add the "phoneNumbers" array while
- * keeping the "phoneNumber" attribute.
- */
-
 export const userSchema = z.object({
   id: z.number().int(),
   username: z.string(),
@@ -28,7 +17,7 @@ export const userSchema = z.object({
 export type User = z.infer<typeof userSchema>;
 
 /** Schema of object to send to endpoint for creating a new user.
- *  These properties are generated automatically on the server.
+ *  These omitted properties are generated automatically on the server.
  */
 export const newUserSchema = userSchema.omit({
   id: true,
@@ -37,7 +26,7 @@ export const newUserSchema = userSchema.omit({
 export type NewUser = z.infer<typeof newUserSchema>;
 
 /** Schema of object to send to endpoint for editing/updating an existing user.
- *  Some fields like `username` cannot be changed and so are excluded here.
+ *  Some fields like `username` cannot be changed and so are omitted here.
  */
 export const editUserSchema = userSchema.omit({
   username: true,
