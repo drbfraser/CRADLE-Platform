6d8c786693663fc807dc83e6fff2943427ace274
Harry Nguyen <hln5@sfu.ca>
2025-02-04 11:12:04 -0800

    refactor(patient page): use tanstack query for fetching data

--- client/src/context/providers/TopLevelContextProviders.tsx
+++ client/src/context/providers/TopLevelContextProviders.tsx
@@ -1,26 +1,31 @@
 import { PropsWithChildren } from 'react';
-
 import { Provider as ReduxProvider } from 'react-redux';
-import { reduxStore } from 'src/redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { reduxStore } from 'src/redux/store';
 import { MaterialUIContextProvider } from 'src/context/providers/materialUI';
 import { MuiToolpadProvider } from './materialUI/MuiToolpadProvider';
+
+const queryClient = new QueryClient();
 
 /** Combined Context Providers to wrap the top level App component. */
 export const TopLevelContextProviders = ({ children }: PropsWithChildren) => {
   return (
-    <BrowserRouter>
-      <ReduxProvider store={reduxStore}>
-        <MaterialUIContextProvider>
-          <MuiToolpadProvider>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-              {children}
-            </LocalizationProvider>
-          </MuiToolpadProvider>
-        </MaterialUIContextProvider>
-      </ReduxProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ReduxProvider store={reduxStore}>
+          <MaterialUIContextProvider>
+            <MuiToolpadProvider>
+              <LocalizationProvider dateAdapter={AdapterMoment}>
+                {children}
+              </LocalizationProvider>
+            </MuiToolpadProvider>
+          </MaterialUIContextProvider>
+        </ReduxProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
