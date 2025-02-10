db66a094f03ba792fbd1b87b253ead137090107f
Harry Nguyen <hln5@sfu.ca>
2025-02-10 13:07:14 -0800

    refactor(edit user form): use react query mutations

--- client/src/pages/admin/manageUsers/UserForms/EditUserDialog.tsx
+++ client/src/pages/admin/manageUsers/UserForms/EditUserDialog.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { Form, Formik } from 'formik';
 import {
   Checkbox,
   Dialog,
@@ -8,16 +12,12 @@ import {
   MenuItem,
   TextField,
 } from '@mui/material';
-import { CancelButton, PrimaryButton } from 'src/shared/components/Button';
-import { Form, Formik, FormikHelpers } from 'formik';
-import { UserField, fieldLabels, makeEditUserValidationSchema } from './state';
 
+import { CancelButton, PrimaryButton } from 'src/shared/components/Button';
 import APIErrorToast from 'src/shared/components/apiErrorToast/APIErrorToast';
 import { UserRoleEnum } from 'src/shared/enums';
 import { editUserAsync } from 'src/shared/api/api';
-import { useState } from 'react';
 import { EditUser, User } from 'src/shared/api/validation/user';
-import { AxiosError } from 'axios';
 import { UserPhoneNumbersFieldArray } from 'src/pages/admin/manageUsers/UserForms/UserPhoneNumbersFieldArray';
 import {
   UserEmailField,
@@ -26,72 +26,73 @@ import {
   UserRoleField,
 } from './UserFormFields';
 import { getOtherUsersEmailsAndPhoneNumbers } from './userFormHelpers';
+import { UserField, fieldLabels, makeEditUserValidationSchema } from './state';
 
 interface IProps {
   open: boolean;
   onClose: () => void;
   users: User[];
-  editUser: EditUser;
+  userToEdit: EditUser;
 }
 
-export const EditUserDialog = ({ open, onClose, users, editUser }: IProps) => {
-  const [submitError, setSubmitError] = useState(false);
+export const EditUserDialog = ({
+  open,
+  onClose,
+  users,
+  userToEdit,
+}: IProps) => {
   const [errorMessage, setErrorMessage] = useState('');
 
   const { otherUsersEmails, otherUsersPhoneNumbers } =
-    getOtherUsersEmailsAndPhoneNumbers(users, editUser.id);
+    getOtherUsersEmailsAndPhoneNumbers(users, userToEdit.id);
   const validationSchema = makeEditUserValidationSchema(
     otherUsersEmails,
     otherUsersPhoneNumbers
   );
 
-  const handleSubmit = async (
-    user: EditUser,
-    { setSubmitting }: FormikHelpers<EditUser>
-  ) => {
+  const editUser = useMutation({
+    mutationFn: (editedUser: EditUser) =>
+      editUserAsync(editedUser, userToEdit.id),
+  });
+
+  const handleSubmit = async (user: EditUser) => {
     const editedUser = { ...user };
     /* Remove any blank phone numbers. */
     editedUser.phoneNumbers = editedUser.phoneNumbers.filter((phoneNumber) => {
       return phoneNumber.trim().length > 0;
     });
 
-    try {
-      await editUserAsync(editedUser, editUser.id);
-      onClose();
-    } catch (e) {
-      let message = 'Something went wrong';
-      if (e instanceof AxiosError) {
-        const responseBody = e.response?.data;
-        message = responseBody.message;
-        if ('message' in responseBody) {
+    editUser.mutate(editedUser, {
+      onSuccess: () => onClose(),
+      onError: (e) => {
+        let message = 'Something went wrong';
+        if (e instanceof AxiosError) {
+          const responseBody = e.response?.data;
           message = responseBody.message;
+          if ('message' in responseBody) {
+            message = responseBody.message;
+          }
+        } else if (typeof e == 'string') {
+          console.log('e is a string');
+          message = e;
         }
-      } else if (typeof e == 'string') {
-        console.log('e is a string');
-        message = e;
-      }
-      setSubmitting(false);
-      setErrorMessage(`Error: ${message}`);
-      setSubmitError(true);
-    }
+        setErrorMessage(`Error: ${message}`);
+      },
+    });
   };
 
   return (
     <>
-      <APIErrorToast
-        open={submitError}
-        onClose={() => {
-          setSubmitError(false);
-          setErrorMessage('');
-        }}
-        errorMessage={errorMessage}
-      />
+      {editUser.isError && !editUser.isPending && (
+        <APIErrorToast errorMessage={errorMessage} />
+      )}
+
       <Dialog open={open} maxWidth="sm" fullWidth>
-        <DialogTitle>{'Edit'} User</DialogTitle>
+        <DialogTitle>Edit User</DialogTitle>
         <DialogContent>
           <Formik
             initialValues={{
-              ...editUser,
+              ...userToEdit,
             }}
             validationSchema={validationSchema}
             onSubmit={handleSubmit}>
@@ -99,7 +100,6 @@ export const EditUserDialog = ({ open, onClose, users, editUser }: IProps) => {
               values,
               touched,
               errors,
-              isSubmitting,
               isValid,
               handleChange,
               handleBlur,
@@ -118,51 +118,49 @@ export const EditUserDialog = ({ open, onClose, users, editUser }: IProps) => {
                   <UserHealthFacilityField />
                   <UserRoleField />
                   {values.role === UserRoleEnum.CHO && (
-                    <>
-                      <TextField
-                        label={fieldLabels[UserField.supervises]}
-                        name={UserField.supervises}
-                        value={values.supervises}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        error={touched.supervises && !!errors.supervises}
-                        helperText={touched.supervises && errors.supervises}
-                        variant="outlined"
-                        fullWidth
-                        select
-                        slotProps={{
-                          select: {
-                            multiple: true,
-                            renderValue: (ids: unknown) =>
-                              (ids as number[])
-                                .map(
-                                  (id) =>
-                                    users.find((u) => u.id === id)?.name ??
-                                    'Unknown'
-                                )
-                                .join(', '),
-                          },
-                        }}>
-                        {users
-                          .filter(
-                            (user) =>
-                              editUser.supervises.includes(user.id) ||
-                              (user.role === UserRoleEnum.VHT &&
-                                user.id !== editUser.id)
-                          )
-                          .map((user) => (
-                            <MenuItem key={user.id} value={user.id}>
-                              <Checkbox
-                                checked={
-                                  values.supervises &&
-                                  values.supervises?.indexOf(user.id) >= 0
-                                }
-                              />
-                              {user.name} ({user.email})
-                            </MenuItem>
-                          ))}
-                      </TextField>
-                    </>
+                    <TextField
+                      label={fieldLabels[UserField.supervises]}
+                      name={UserField.supervises}
+                      value={values.supervises}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      error={touched.supervises && !!errors.supervises}
+                      helperText={touched.supervises && errors.supervises}
+                      variant="outlined"
+                      fullWidth
+                      select
+                      slotProps={{
+                        select: {
+                          multiple: true,
+                          renderValue: (ids: unknown) =>
+                            (ids as number[])
+                              .map(
+                                (id) =>
+                                  users.find((u) => u.id === id)?.name ??
+                                  'Unknown'
+                              )
+                              .join(', '),
+                        },
+                      }}>
+                      {users
+                        .filter(
+                          (user) =>
+                            userToEdit.supervises.includes(user.id) ||
+                            (user.role === UserRoleEnum.VHT &&
+                              user.id !== userToEdit.id)
+                        )
+                        .map((user) => (
+                          <MenuItem key={user.id} value={user.id}>
+                            <Checkbox
+                              checked={
+                                values.supervises &&
+                                values.supervises.indexOf(user.id) >= 0
+                              }
+                            />
+                            {user.name} ({user.email})
+                          </MenuItem>
+                        ))}
+                    </TextField>
                   )}
                   <DialogActions>
                     <CancelButton type="button" onClick={onClose}>
@@ -170,8 +168,8 @@ export const EditUserDialog = ({ open, onClose, users, editUser }: IProps) => {
                     </CancelButton>
                     <PrimaryButton
                       type="submit"
-                      disabled={isSubmitting || !isValid}>
-                      {'Save'}
+                      disabled={editUser.isPending || !isValid}>
+                      Save
                     </PrimaryButton>
                   </DialogActions>
                 </FormGroup>
