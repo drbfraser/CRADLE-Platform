70ca1269e842c0f9d2a24d956ade593e749a2bc0
Harry Nguyen <hln5@sfu.ca>
2025-02-10 16:24:05 -0800

    refactor(single reason form): use react query mutations

--- client/src/pages/singleReasonForm/index.tsx
+++ client/src/pages/singleReasonForm/index.tsx
@@ -1,37 +1,40 @@
-import { useEffect, useState } from 'react';
-
-import { SingleReasonForm } from './SingleReasonForm';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Box, IconButton, Tooltip, Typography } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
+import { Box, IconButton, Tooltip, Typography } from '@mui/material';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { FormContainer } from 'src/shared/components/layout/FormContainer';
+import { SingleReasonForm } from './SingleReasonForm';
 
 export const SingleReasonFormPage = () => {
   const { referralId, cancellationType } = useParams();
-  const [title, setTitle] = useState('');
   const navigate = useNavigate();
 
-  useEffect(() => {
-    switch (cancellationType) {
-      case 'cancel_referral':
-        setTitle('Reason for Cancelling');
-        break;
-      case 'not_attend_referral':
-        setTitle('Reason for Not Attend');
-        break;
-      case 'undo_cancel_referral':
-        setTitle('Reason for Undo Cancelling');
-        break;
-      default:
-        setTitle('');
-        break;
-    }
-  }, [cancellationType]);
+  if (!referralId || !cancellationType) {
+    console.error('ERROR: invalid path.');
+    navigate('/referrals', { replace: true });
+    return null;
+  }
+
+  let title = undefined;
+  switch (cancellationType) {
+    case 'cancel_referral':
+      title = 'Reason for Cancelling';
+      break;
+    case 'not_attend_referral':
+      title = 'Reason for Not Attend';
+      break;
+    case 'undo_cancel_referral':
+      title = 'Reason for Undo Cancelling';
+      break;
+    default:
+      console.warn(`unknown cancellation type: ${cancellationType}`);
+      break;
+  }
 
   return (
     <FormContainer>
       <Box
         sx={{
+          marginBottom: '1rem',
           display: `flex`,
           alignItems: `center`,
         }}>
@@ -42,7 +45,7 @@ export const SingleReasonFormPage = () => {
         </Tooltip>
         <Typography variant="h4">{title}</Typography>
       </Box>
-      <br />
+
       <SingleReasonForm
         referralId={referralId}
         cancellationType={cancellationType}
