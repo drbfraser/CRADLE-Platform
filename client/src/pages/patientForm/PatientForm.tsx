049ac388a4c8641eac04a45c4dfc6c2e085cb6e3
Harry Nguyen <hln5@sfu.ca>
2025-02-11 13:02:49 -0800

    refactor(patient forms): split giant patient form into multiple forms for easier maintainability + use react query

--- client/src/pages/patientForm/PatientForm.tsx
+++ client/src/pages/patientForm/PatientForm.tsx
@@ -1,268 +1,189 @@
-import {
-  CancelButton,
-  PrimaryButton,
-  SecondaryButton,
-} from 'src/shared/components/Button';
+import { PrimaryButton, SecondaryButton } from 'src/shared/components/Button';
 import { Form, Formik, FormikHelpers, FormikProps } from 'formik';
-import {
-  drugHistoryValidationSchema,
-  medicalHistoryValidationSchema,
-} from './medicalInfo/validation';
-import {
-  handleDeleteRecord,
-  handleMedicalRecordInfo,
-  handlePregnancyInfo,
-  handleSubmit,
-} from './handlers';
 
 import APIErrorToast from 'src/shared/components/apiErrorToast/APIErrorToast';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { ConfirmDialog } from 'src/shared/components/confirmDialog/index';
-import IconButton from '@mui/material/IconButton';
 import { MedicalInfoForm } from './medicalInfo';
-import { PatientState } from './state';
+import { PatientField, PatientState } from './state';
 import { PersonalInfoForm } from './personalInfo';
-import { PregnancyInfoForm } from './pregnancyInfo';
 import Step from '@mui/material/Step/Step';
 import StepLabel from '@mui/material/StepLabel/StepLabel';
 import Stepper from '@mui/material/Stepper/Stepper';
-import Tooltip from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
 import { personalInfoValidationSchema } from './personalInfo/validation';
-import { pregnancyInfoValidationSchema } from './pregnancyInfo/validation';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { PregnancyInfoForm } from './pregnancyInfo';
+import { pregnancyInfoValidationSchema } from './pregnancyInfo/validation';
+import {
+  getPatientData,
+  PatientData,
+} from './personalInfo/EditPersonalInfoForm';
+import { useMutation } from '@tanstack/react-query';
+import { API_URL, axiosFetch } from 'src/shared/api/api';
+import { EndpointEnum, GestationalAgeUnitEnum } from 'src/shared/enums';
+import { getDOBForEstimatedAge } from 'src/shared/utils';
+import { gestationalAgeUnitTimestamp } from 'src/shared/constants';
+import PatientFormHeader from './PatientFormHeader';
+
+const STEPS = [
+  {
+    editId: 'personalInfo',
+    name: 'Patient Personal Information',
+    component: PersonalInfoForm,
+    validationSchema: personalInfoValidationSchema(true),
+    title: 'New Patient',
+  },
+  {
+    editId: 'pregnancyInfo',
+    name: 'Pregnancy Information',
+    component: PregnancyInfoForm,
+    validationSchema: pregnancyInfoValidationSchema,
+    title: 'New Patient',
+  },
+  {
+    editId: 'medicalHistory',
+    name: 'Medical Information',
+    component: MedicalInfoForm,
+    validationSchema: undefined,
+    isDrugRecord: undefined,
+    title: 'Add Medical History',
+  },
+];
+
+type PregnancyData = {
+  patientId: string;
+  startDate: number;
+};
 
 interface PatientFormProps {
-  editId: string;
-  patientId?: string;
-  pregnancyId?: string;
-  universalRecordId?: string;
   initialState: PatientState;
-  creatingNew: boolean;
-  creatingNewPregnancy: boolean;
 }
 
-export const PatientForm = ({
-  editId,
-  patientId,
-  pregnancyId,
-  universalRecordId,
-  initialState,
-  creatingNew,
-  creatingNewPregnancy,
-}: PatientFormProps) => {
-  const navigate = useNavigate();
-
+export const PatientForm = ({ initialState }: PatientFormProps) => {
   const theme = useTheme();
   const isBigScreen = useMediaQuery(theme.breakpoints.up('lg'));
+  const navigate = useNavigate();
 
-  const [submitError, setSubmitError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState<string>('');
-  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
-
-  const pages = [
-    {
-      editId: 'personalInfo',
-      name: 'Patient Personal Information',
-      component: PersonalInfoForm,
-      validationSchema: personalInfoValidationSchema(
-        creatingNew ? true : false
-      ),
-      title: creatingNew ? 'New Patient' : 'Edit Personal Information',
+  const addPatientInfo = useMutation({
+    mutationFn: (patientData: PatientData) => {
+      const endpoint = API_URL + EndpointEnum.PATIENTS;
+      return axiosFetch(endpoint, { method: 'POST', data: patientData });
     },
-    {
-      editId: 'pregnancyInfo',
-      name: 'Pregnancy Information',
-      component: PregnancyInfoForm,
-      validationSchema: pregnancyInfoValidationSchema,
-      title: creatingNewPregnancy
-        ? 'Add New Pregnancy'
-        : editId
-        ? 'Edit/Close Pregnancy'
-        : 'New Patient',
-    },
-    {
-      editId: editId === 'drugHistory' ? 'drugHistory' : 'medicalHistory',
-      name: 'Medical Information',
-      component: MedicalInfoForm,
-      validationSchema: editId
-        ? editId === 'drugHistory'
-          ? drugHistoryValidationSchema
-          : medicalHistoryValidationSchema
-        : undefined,
-      isDrugRecord: editId
-        ? editId === 'drugHistory'
-          ? true
-          : false
-        : undefined,
-      title:
-        editId === 'drugHistory'
-          ? initialState.drugHistory
-            ? 'Update Drug History'
-            : 'Add Drug History'
-          : initialState.medicalHistory
-          ? 'Update Medical History'
-          : 'Add Medical History',
-    },
-  ];
+  });
 
-  const initPageNum = editId
-    ? pages.findIndex((page) => {
-        return page.editId === editId;
-      })
-    : creatingNewPregnancy
-    ? 1
-    : 0;
-  const [pageNum, setPageNum] = useState(initPageNum);
-  const PageComponent = pages[pageNum].component;
-  const isFinalPage = pageNum === pages.length - 1;
+  const addPregnancyInfo = useMutation({
+    mutationFn: (values: PregnancyData) => {
+      const endpoint =
+        API_URL +
+        EndpointEnum.PATIENTS +
+        `/${values.patientId}` +
+        EndpointEnum.PREGNANCIES;
+      return axiosFetch(endpoint, { method: 'POST', data: values });
+    },
+  });
 
-  const handleNext = async (
-    values: PatientState,
-    helpers: FormikHelpers<PatientState>
-  ) => {
-    if (editId || creatingNewPregnancy) {
-      if (pages[pageNum].editId === 'pregnancyInfo') {
-        handlePregnancyInfo(
-          patientId,
-          pregnancyId,
-          creatingNewPregnancy,
-          values,
-          navigate,
-          setSubmitError,
-          helpers.setSubmitting,
-          setErrorMessage
-        );
-      } else if (
-        pages[pageNum].editId === 'drugHistory' ||
-        pages[pageNum].editId === 'medicalHistory'
-      ) {
-        handleMedicalRecordInfo(
-          patientId,
-          values,
-          pages[pageNum].isDrugRecord,
-          navigate,
-          setSubmitError,
-          helpers.setSubmitting
-        );
-      } else {
-        handleSubmit(
-          values,
-          false,
-          navigate,
-          setSubmitError,
-          helpers.setSubmitting,
-          setErrorMessage
+  const handleSubmit = async (values: PatientState) => {
+    try {
+      const patientData = getPatientData(values);
+      if (!patientData.isExactDateOfBirth) {
+        patientData.dateOfBirth = getDOBForEstimatedAge(
+          parseInt(values[PatientField.estimatedAge])
         );
       }
-    } else if (isFinalPage) {
-      handleSubmit(
-        values,
-        true,
-        navigate,
-        setSubmitError,
-        helpers.setSubmitting,
-        setErrorMessage
-      );
-    } else {
-      helpers.setTouched({});
-      helpers.setSubmitting(false);
-      setPageNum(pageNum + 1);
+      const res = await addPatientInfo.mutateAsync(patientData);
+      const patientId = res.data.id;
+
+      const isPregnant = Boolean(values[PatientField.isPregnant]);
+      if (isPregnant) {
+        let pregnancyStartDate =
+          values.gestationalAgeUnit === GestationalAgeUnitEnum.WEEKS
+            ? gestationalAgeUnitTimestamp[GestationalAgeUnitEnum.WEEKS](
+                values.gestationalAgeWeeks,
+                values.gestationalAgeDays
+              )
+            : gestationalAgeUnitTimestamp[GestationalAgeUnitEnum.MONTHS](
+                values.gestationalAgeMonths
+              );
+        pregnancyStartDate = Math.round(pregnancyStartDate);
+        await addPregnancyInfo.mutateAsync({
+          startDate: pregnancyStartDate,
+          patientId,
+        });
+      }
+
+      navigate(`/patients/${patientId}`);
+    } catch (e) {
+      console.error(e);
     }
   };
 
+  const [stepNum, setStepNum] = useState(0);
+  const PageComponent = STEPS[stepNum].component;
+  const isFinalPage = stepNum === STEPS.length - 1;
+
+  const handleNextStep = (
+    values: PatientState,
+    helpers: FormikHelpers<PatientState>
+  ) => {
+    if (isFinalPage) {
+      handleSubmit(values);
+    } else {
+      helpers.setTouched({});
+      setStepNum(stepNum + 1);
+    }
+  };
+
+  const isSubmitting = addPatientInfo.isPending || addPregnancyInfo.isPaused;
   return (
     <>
-      <APIErrorToast
-        open={submitError}
-        onClose={() => setSubmitError(false)}
-        errorMessage={errorMessage}
-      />
-      <Box
-        sx={{
-          display: `flex`,
-          alignItems: `center`,
-        }}>
-        <Tooltip title="Go back" placement="top">
-          <IconButton
-            onClick={() => navigate(`/patients/${patientId ?? ''}`)}
-            size="large">
-            <ChevronLeftIcon color="inherit" fontSize="large" />
-          </IconButton>
-        </Tooltip>
-        <Typography variant="h4">{pages[pageNum].title}</Typography>
-      </Box>
-      {creatingNew && !creatingNewPregnancy && (
-        <Stepper
-          activeStep={pageNum}
-          orientation={isBigScreen ? 'horizontal' : 'vertical'}>
-          {pages.map((page, idx) => (
-            <Step key={idx}>
-              <StepLabel>{page.name}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
+      {addPatientInfo.isError && !addPatientInfo.isPending && (
+        <APIErrorToast errorMessage={addPatientInfo.error.message} />
       )}
-      <br />
+      {addPregnancyInfo.isError && !addPregnancyInfo.isPending && (
+        <APIErrorToast errorMessage={addPregnancyInfo.error.message} />
+      )}
+
+      <PatientFormHeader title={STEPS[stepNum].title} />
+
+      <Stepper
+        sx={{ marginBottom: '1rem' }}
+        activeStep={stepNum}
+        orientation={isBigScreen ? 'horizontal' : 'vertical'}>
+        {STEPS.map((step, index) => (
+          <Step key={index}>
+            <StepLabel>{step.name}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+
       <Formik
         initialValues={initialState}
-        onSubmit={handleNext}
-        validationSchema={pages[pageNum].validationSchema}>
+        onSubmit={handleNextStep}
+        validationSchema={STEPS[stepNum].validationSchema}>
         {(formikProps: FormikProps<PatientState>) => (
           <Form>
             <PageComponent
               formikProps={formikProps}
-              creatingNew={creatingNew}
-              isDrugRecord={pages[pageNum].isDrugRecord}
-              creatingNewPregnancy={creatingNewPregnancy}
+              creatingNew={true}
+              isDrugRecord={STEPS[stepNum].isDrugRecord}
             />
-            <br />
-            {creatingNew && (
+
+            <Box sx={{ marginTop: '1rem' }}>
               <SecondaryButton
-                onClick={() => setPageNum(pageNum - 1)}
-                disabled={pageNum === 0 || formikProps.isSubmitting}>
+                onClick={() => setStepNum(stepNum - 1)}
+                disabled={stepNum === 0 || isSubmitting}>
                 Back
               </SecondaryButton>
-            )}
-            {editId && universalRecordId && (
-              <>
-                <CancelButton onClick={() => setIsDialogOpen(true)}>
-                  Delete
-                </CancelButton>
-                <ConfirmDialog
-                  title="Delete Record?"
-                  content="Are you sure you want to delete this record?"
-                  open={isDialogOpen}
-                  onClose={() => {
-                    setIsDialogOpen(false);
-                  }}
-                  onConfirm={() =>
-                    handleDeleteRecord(
-                      editId,
-                      universalRecordId,
-                      navigate,
-                      setSubmitError,
-                      formikProps.setSubmitting
-                    ).then(() => setIsDialogOpen(false))
-                  }
-                />
-              </>
-            )}
-            <PrimaryButton
-              sx={{
-                float: 'right',
-                margin: 5,
-              }}
-              type="submit"
-              disabled={formikProps.isSubmitting}>
-              {editId || creatingNewPregnancy
-                ? 'Save'
-                : isFinalPage
-                ? 'Create'
-                : 'Next'}
-            </PrimaryButton>
+
+              <PrimaryButton
+                sx={{
+                  float: 'right',
+                }}
+                type="submit"
+                disabled={isSubmitting}>
+                {isFinalPage ? 'Create' : 'Next'}
+              </PrimaryButton>
+            </Box>
           </Form>
         )}
       </Formik>
--- client/src/pages/patientForm/pregnancyInfo/index.tsx
+++ client/src/pages/patientForm/pregnancyInfo/index.tsx
@@ -29,157 +29,152 @@ export const PregnancyInfoForm = ({
   const isFemale = formikProps.values.sex === SexEnum.FEMALE;
   const isPregnant = formikProps.values.isPregnant;
   return (
-    <Paper>
-      <Box p={2}>
-        <Grid container spacing={2}>
-          {creatingNew && (
-            <Grid item md={2} sm={12}>
-              <Field
-                component={CheckboxWithLabel}
-                type="checkbox"
-                name={PatientField.isPregnant}
-                onChange={handleChangeCustom(
-                  formikProps.handleChange,
-                  formikProps.setFieldValue
-                )}
-                Label={{ label: 'Pregnant' }}
-                disabled={!isFemale}
-              />
+    <Paper sx={{ padding: 4 }}>
+      <Grid container spacing={2}>
+        {creatingNew && (
+          <Grid item md={2} sm={12}>
+            <Field
+              component={CheckboxWithLabel}
+              type="checkbox"
+              name={PatientField.isPregnant}
+              onChange={handleChangeCustom(
+                formikProps.handleChange,
+                formikProps.setFieldValue
+              )}
+              Label={{ label: 'Pregnant' }}
+              disabled={!isFemale}
+            />
+          </Grid>
+        )}
+        <Grid item md={4} sm={12}>
+          <FormControl fullWidth variant="outlined">
+            <Field
+              component={Select}
+              fullWidth
+              label="Gestational Age Unit"
+              name={PatientField.gestationalAgeUnit}
+              required={(creatingNew && isPregnant) || creatingNewPregnancy}
+              disabled={creatingNew && !isPregnant}>
+              {gestationalAgeUnitOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Field>
+          </FormControl>
+        </Grid>
+        <Grid item md={4} sm={12}>
+          {formikProps.values.gestationalAgeUnit ===
+          GestationalAgeUnitEnum.MONTHS ? (
+            <Field
+              component={TextField}
+              fullWidth
+              variant="outlined"
+              type="number"
+              name={PatientField.gestationalAgeMonths}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">months</InputAdornment>
+                ),
+              }}
+              required={(creatingNew && isPregnant) || creatingNewPregnancy}
+              disabled={creatingNew && !isPregnant}
+            />
+          ) : (
+            <Grid container>
+              <Grid item md={5} sm={12}>
+                <Field
+                  component={TextField}
+                  fullWidth
+                  variant="outlined"
+                  type="number"
+                  name={PatientField.gestationalAgeWeeks}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">weeks</InputAdornment>
+                    ),
+                  }}
+                  required={(creatingNew && isPregnant) || creatingNewPregnancy}
+                  disabled={creatingNew && !isPregnant}
+                />
+              </Grid>
+              <Grid item md={2} sm={12}>
+                <Box
+                  sx={{
+                    textAlign: 'center',
+                    fontSize: '35px',
+                  }}>
+                  +
+                </Box>
+              </Grid>
+              <Grid item md={5} sm={12}>
+                <Field
+                  component={TextField}
+                  fullWidth
+                  variant="outlined"
+                  type="number"
+                  name={PatientField.gestationalAgeDays}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">days</InputAdornment>
+                    ),
+                  }}
+                  required={(creatingNew && isPregnant) || creatingNewPregnancy}
+                  disabled={creatingNew && !isPregnant}
+                />
+              </Grid>
             </Grid>
           )}
-          <Grid item md={4} sm={12}>
-            <FormControl fullWidth variant="outlined">
-              <Field
-                component={Select}
-                fullWidth
-                label="Gestational Age Unit"
-                name={PatientField.gestationalAgeUnit}
-                required={(creatingNew && isPregnant) || creatingNewPregnancy}
-                disabled={creatingNew && !isPregnant}>
-                {gestationalAgeUnitOptions.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.name}
-                  </MenuItem>
-                ))}
-              </Field>
-            </FormControl>
-          </Grid>
-          <Grid item md={4} sm={12}>
-            {formikProps.values.gestationalAgeUnit ===
-            GestationalAgeUnitEnum.MONTHS ? (
+        </Grid>
+        {!creatingNew && (
+          <>
+            <Grid item md={4} sm={12}>
               <Field
                 component={TextField}
                 fullWidth
                 variant="outlined"
-                type="number"
-                name={PatientField.gestationalAgeMonths}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">months</InputAdornment>
-                  ),
+                type="date"
+                label="End Date"
+                name={PatientField.pregnancyEndDate}
+                InputLabelProps={{
+                  shrink: true,
                 }}
-                required={(creatingNew && isPregnant) || creatingNewPregnancy}
-                disabled={creatingNew && !isPregnant}
+                required={Boolean(formikProps.values.pregnancyOutcome)}
               />
-            ) : (
-              <Grid container>
-                <Grid item md={5} sm={12}>
-                  <Field
-                    component={TextField}
-                    fullWidth
-                    variant="outlined"
-                    type="number"
-                    name={PatientField.gestationalAgeWeeks}
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">weeks</InputAdornment>
-                      ),
-                    }}
-                    required={
-                      (creatingNew && isPregnant) || creatingNewPregnancy
-                    }
-                    disabled={creatingNew && !isPregnant}
-                  />
-                </Grid>
-                <Grid item md={2} sm={12}>
-                  <Box
-                    sx={{
-                      textAlign: 'center',
-                      fontSize: '35px',
-                    }}>
-                    +
-                  </Box>
-                </Grid>
-                <Grid item md={5} sm={12}>
-                  <Field
-                    component={TextField}
-                    fullWidth
-                    variant="outlined"
-                    type="number"
-                    name={PatientField.gestationalAgeDays}
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">days</InputAdornment>
-                      ),
-                    }}
-                    required={
-                      (creatingNew && isPregnant) || creatingNewPregnancy
-                    }
-                    disabled={creatingNew && !isPregnant}
-                  />
-                </Grid>
-              </Grid>
-            )}
-          </Grid>
-          {!creatingNew && (
-            <>
-              <Grid item md={4} sm={12}>
-                <Field
-                  component={TextField}
-                  fullWidth
-                  variant="outlined"
-                  type="date"
-                  label="End Date"
-                  name={PatientField.pregnancyEndDate}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  required={Boolean(formikProps.values.pregnancyOutcome)}
-                />
-              </Grid>
-              <Grid item md={6} sm={12}>
-                <Field
-                  component={TextField}
-                  fullWidth
-                  multiline
-                  rows={4}
-                  variant="outlined"
-                  label="Outcome (e.g. mode of delivery, birth weight)"
-                  placeholder="Mode of delivery, birth weight"
-                  name={PatientField.pregnancyOutcome}
-                  required={Boolean(formikProps.values.pregnancyEndDate)}
-                />
-              </Grid>
-            </>
-          )}
-        </Grid>
-        {creatingNewPregnancy && (
-          <Typography color="textSecondary" variant="caption">
-            Only fill in end date and outcome if you want to add a completed
-            pregnancy
-          </Typography>
+            </Grid>
+            <Grid item md={6} sm={12}>
+              <Field
+                component={TextField}
+                fullWidth
+                multiline
+                rows={4}
+                variant="outlined"
+                label="Outcome (e.g. mode of delivery, birth weight)"
+                placeholder="Mode of delivery, birth weight"
+                name={PatientField.pregnancyOutcome}
+                required={Boolean(formikProps.values.pregnancyEndDate)}
+              />
+            </Grid>
+          </>
         )}
-        {!creatingNew && !creatingNewPregnancy && (
-          <Typography color="textSecondary" variant="caption">
-            Filling in end date and outcome will close this pregnancy
-          </Typography>
-        )}
-        {formikProps.values.sex === SexEnum.MALE && (
-          <Typography color="textSecondary" variant="caption">
-            Cannot fill because the patient is Male, click Next
-          </Typography>
-        )}
-      </Box>
+      </Grid>
+
+      {creatingNewPregnancy && (
+        <Typography color="textSecondary" variant="caption">
+          Only fill in end date and outcome if you want to add a completed
+          pregnancy
+        </Typography>
+      )}
+      {!creatingNew && !creatingNewPregnancy && (
+        <Typography color="textSecondary" variant="caption">
+          Filling in end date and outcome will close this pregnancy
+        </Typography>
+      )}
+      {formikProps.values.sex === SexEnum.MALE && (
+        <Typography color="textSecondary" variant="caption">
+          Cannot fill because the patient is Male, click Next
+        </Typography>
+      )}
     </Paper>
   );
 };
