7b4c4f73a2ab010cc74b161fe0739812c28d50a4
Harry Nguyen <hln5@sfu.ca>
2025-02-13 14:10:35 -0800

    refactor(archive patient dialog): use react query + ensure patient is defined

--- client/src/pages/admin/managePatients/ArchivePatientDialog.tsx
+++ client/src/pages/admin/managePatients/ArchivePatientDialog.tsx
@@ -1,46 +1,39 @@
+import { useMutation } from '@tanstack/react-query';
 import APIErrorToast from 'src/shared/components/apiErrorToast/APIErrorToast';
 import { ConfirmDialog } from 'src/shared/components/confirmDialog/index';
 import { Patient } from 'src/shared/types';
 import { Toast } from 'src/shared/components/toast';
 import { archivePatientAsync } from 'src/shared/api/api';
-import { useState } from 'react';
 
 interface IProps {
   open: boolean;
   onClose: () => void;
-  patient: Patient | undefined;
+  patient: Patient;
 }
 
-const ArchivePatient = ({ open, onClose, patient }: IProps) => {
-  const [submitError, setSubmitError] = useState(false);
-  const [submitSuccess, setSubmitSuccess] = useState(false);
-  const name = patient?.name;
+const ArchivePatientDialog = ({ open, onClose, patient }: IProps) => {
+  const archivePatient = useMutation({
+    mutationFn: archivePatientAsync,
+  });
 
   const handleDelete = async () => {
-    if (!patient) {
-      return;
-    }
-
-    try {
-      await archivePatientAsync(patient.id);
-
-      setSubmitError(false);
-      setSubmitSuccess(true);
-      onClose();
-    } catch (e) {
-      setSubmitError(true);
-    }
+    archivePatient.mutate(patient.id, {
+      onSuccess: () => onClose(),
+    });
   };
 
+  const name = patient.name;
   return (
     <>
       <Toast
         severity="success"
         message="Patient successfully archived!"
-        open={submitSuccess}
-        onClose={() => setSubmitSuccess(false)}
+        open={archivePatient.isSuccess}
       />
-      <APIErrorToast open={submitError} onClose={() => setSubmitError(false)} />
+      {archivePatient.isError && (
+        <APIErrorToast onClose={() => archivePatient.reset()} />
+      )}
+
       <ConfirmDialog
         title={`Archive Patient: ${name}`}
         content={`Are you sure you want to archive ${name}'s account? `}
@@ -52,4 +45,4 @@ const ArchivePatient = ({ open, onClose, patient }: IProps) => {
   );
 };
 
-export default ArchivePatient;
+export default ArchivePatientDialog;
