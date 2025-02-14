df3fae7d5b29c02b20d395d2b3c6fc7f4cd509ce
Harry Nguyen <hln5@sfu.ca>
2025-02-14 14:01:07 -0800

    refactor(statistics page): use async/await instead of promise chaining

--- client/src/pages/statistics/utils/queries.ts
+++ client/src/pages/statistics/utils/queries.ts
@@ -3,17 +3,19 @@ import { statsDataSchema } from 'src/shared/api/validation/statistics';
 import { ZodError } from 'zod';
 import { getUserStatisticsAsync } from 'src/shared/api/api';
 
-export const statsQueryFn = (fetchData: Promise<unknown>) =>
-  fetchData
-    .then((res) => statsDataSchema.parseAsync(res))
-    .catch((e) => {
-      if (e instanceof ZodError) {
-        console.error(`Error validating statisitcs data: ${e}`);
-      } else {
-        console.error(`Error querying for statistics: ${e}`);
-      }
-      throw e;
-    });
+export const statsQueryFn = async (fetchData: Promise<unknown>) => {
+  try {
+    const statsData = await fetchData;
+    return await statsDataSchema.parseAsync(statsData);
+  } catch (e) {
+    if (e instanceof ZodError) {
+      console.error(`Error validating statisitcs data: ${e}`);
+    } else {
+      console.error(`Error querying for statistics: ${e}`);
+    }
+    throw e;
+  }
+};
 
 export const useUserStatsQuery = (user: string, from: number, to: number) => {
   return useQuery({
