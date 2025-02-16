83f20d6561a42ba3abd6c2fc99ee257342c78f9d
Ryan Milligan <rcm9@sfu.ca>
2025-02-15 20:25:11 -0800

    test: add login test

---
+++ client/playwright/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'http://localhost:3000';
+
+export const USERNAME = 'admin';
+export const PASSWORD = 'cradle-admin';
---
+++ client/playwright/login.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import { BASE_URL, USERNAME, PASSWORD } from './constants';
+
+test('Login', async ({ page }) => {
+  await page.goto(BASE_URL);
+
+  await expect(page.getByRole('heading', { name: /Log in/i })).toBeVisible();
+
+  // Fill in credentials.
+  await page.getByRole('textbox', { name: /username/i }).fill(USERNAME);
+  await page.getByRole('textbox', { name: /password/i }).fill(PASSWORD);
+
+  // Click Log in button.
+  await page.getByRole('button', { name: /Log in/i }).click();
+
+  // Check that we have been redirected to the referrals page.
+  await expect(page).toHaveURL(`${BASE_URL}/referrals`);
+});
