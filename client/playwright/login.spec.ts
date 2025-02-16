31fc06ca51bdc130fd5b58a2c2fd02085b361a6d
Ryan Milligan <rcm9@sfu.ca>
2025-02-16 14:27:22 -0800

    test: describe login tests

--- client/playwright/login.spec.ts
+++ client/playwright/login.spec.ts
@@ -1,36 +1,38 @@
 import { test, expect } from '@playwright/test';
 import { BASE_URL, USERNAME, PASSWORD } from './constants';
 
-test('Login', async ({ page }) => {
-  await page.goto(BASE_URL);
+test.describe('Login Tests', () => {
+  test('Login', async ({ page }) => {
+    await page.goto(BASE_URL);
 
-  await expect(page.getByRole('heading', { name: /Login/ })).toBeVisible();
+    await expect(page.getByRole('heading', { name: /Login/ })).toBeVisible();
 
-  // Fill in credentials.
-  await page.getByRole('textbox', { name: 'username' }).fill(USERNAME);
-  await page.getByRole('textbox', { name: 'password' }).fill(PASSWORD);
+    // Fill in credentials.
+    await page.getByRole('textbox', { name: 'username' }).fill(USERNAME);
+    await page.getByRole('textbox', { name: 'password' }).fill(PASSWORD);
 
-  // Click Log in button.
-  await page.getByRole('button', { name: 'login' }).click();
+    // Click Log in button.
+    await page.getByRole('button', { name: 'login' }).click();
 
-  // Check that we have been redirected to the referrals page.
-  await expect(page).toHaveURL(/.*\/referrals/);
-});
+    // Check that we have been redirected to the referrals page.
+    await expect(page).toHaveURL(/.*\/referrals/);
+  });
 
-test('Login attempt without username', async ({ page }) => {
-  await page.goto(BASE_URL);
+  test('Login attempt without username', async ({ page }) => {
+    await page.goto(BASE_URL);
 
-  await page.getByRole('textbox', { name: 'username' }).fill(USERNAME);
+    await page.getByRole('textbox', { name: 'username' }).fill(USERNAME);
 
-  // Login button should be disabled.
-  await expect(page.getByRole('button', { name: 'login' })).toBeDisabled();
-});
+    // Login button should be disabled.
+    await expect(page.getByRole('button', { name: 'login' })).toBeDisabled();
+  });
 
-test('Login attempt without password', async ({ page }) => {
-  await page.goto(BASE_URL);
+  test('Login attempt without password', async ({ page }) => {
+    await page.goto(BASE_URL);
 
-  await page.getByRole('textbox', { name: 'password' }).fill(PASSWORD);
+    await page.getByRole('textbox', { name: 'password' }).fill(PASSWORD);
 
-  // Login button should be disabled.
-  await expect(page.getByRole('button', { name: 'login' })).toBeDisabled();
+    // Login button should be disabled.
+    await expect(page.getByRole('button', { name: 'login' })).toBeDisabled();
+  });
 });
