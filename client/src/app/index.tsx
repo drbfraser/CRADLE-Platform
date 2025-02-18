2be675516f29b56979bd640c545029e7f602be53
Harry Nguyen <hln5@sfu.ca>
2025-02-18 11:45:33 -0800

    tooling: install react-query devtools

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Box, CssBaseline } from '@mui/material';
 
-import { AppRoutes } from './routes';
-import { CssBaseline } from '@mui/material';
-import { TopBar } from './topBar';
-import { Box } from '@mui/material';
 import { TopLevelContextProviders } from 'src/context/providers/TopLevelContextProviders';
+import { AppRoutes } from './routes';
+import { TopBar } from './topBar';
 import { SidebarWrapper } from './sidebar/SidebarWrapper';
 
 export const App: React.FC = () => {
   return (
     <TopLevelContextProviders>
+      <ReactQueryDevtools />
       <CssBaseline />
       <Box
         id={'rootContainer'}
