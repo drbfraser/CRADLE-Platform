be928bece25c493dabf6e8702ba1533b4294e6ed
Ryan Milligan <rcm9@sfu.ca>
2025-02-18 10:26:19 -0800

    refactor: remove unused import

--- client/playwright/fixtures.ts
+++ client/playwright/fixtures.ts
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { test as base } from '@playwright/test';
-import { ADMIN_CREDENTIALS } from './constants';
 
 export const test = base.extend({
   page: async ({ page }, use) => {
