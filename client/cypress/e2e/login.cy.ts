e157ddcaa971914d85616729611294a722326dc4
Ryan Milligan <rcm9@sfu.ca>
2025-02-18 12:08:35 -0800

    test: fix cypress login tests

--- client/cypress/e2e/login.cy.ts
+++ client/cypress/e2e/login.cy.ts
@@ -9,30 +9,23 @@ describe('login spec', () => {
 
     cy.get('button').should('contain', 'Login').click();
 
-    // Check that we have been redirected to Referrals page.
     cy.url().should('contain', '/referrals');
   });
   it('No Password Login', () => {
-    cy.visit(BASE_URL);
+    cy.visit('/');
     cy.get('input[name=username]').type(ADMIN_CREDENTIALS.username);
 
-    cy.get('button').should('contain', 'Login').click();
-    // Check that URL hasn't changed.
-    cy.url().should('eq', BASE_URL);
+    cy.get('button').contains('Login').should('be.disabled');
   });
   it('No Email Login', () => {
-    cy.visit(BASE_URL);
+    cy.visit('/');
     cy.get('input[name=password]').type(ADMIN_CREDENTIALS.password);
 
-    cy.get('button').should('contain', 'Login').click();
-    // Check that URL hasn't changed.
-    cy.url().should('eq', BASE_URL);
+    cy.get('button').contains('Login').should('be.disabled');
   });
   it('No Credentials Login', () => {
-    cy.visit(BASE_URL);
+    cy.visit('/');
 
-    cy.get('button').should('contain', 'Login').click();
-    // Check that URL hasn't changed.
-    cy.url().should('eq', BASE_URL);
+    cy.get('button').contains('Login').should('be.disabled');
   });
 });
