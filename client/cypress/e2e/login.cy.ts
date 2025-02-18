55b10df9712fc961c0069458a470d2f1b6c301d1
Ryan Milligan <rcm9@sfu.ca>
2025-02-17 17:47:03 -0800

    test: enable tests

--- client/cypress/e2e/login.cy.ts
+++ client/cypress/e2e/login.cy.ts
@@ -12,27 +12,27 @@ describe('login spec', () => {
     // Check that we have been redirected to Referrals page.
     cy.url().should('contain', '/referrals');
   });
-  // it('No Password Login', () => {
-  //   cy.visit(BASE_URL);
-  //   cy.get('input[name=username]').type(ADMIN_CREDENTIALS.username);
+  it('No Password Login', () => {
+    cy.visit(BASE_URL);
+    cy.get('input[name=username]').type(ADMIN_CREDENTIALS.username);
 
-  //   cy.get('button').should('contain', 'Login').click();
-  //   // Check that URL hasn't changed.
-  //   cy.url().should('eq', BASE_URL);
-  // });
-  // it('No Email Login', () => {
-  //   cy.visit(BASE_URL);
-  //   cy.get('input[name=password]').type(ADMIN_CREDENTIALS.password);
+    cy.get('button').should('contain', 'Login').click();
+    // Check that URL hasn't changed.
+    cy.url().should('eq', BASE_URL);
+  });
+  it('No Email Login', () => {
+    cy.visit(BASE_URL);
+    cy.get('input[name=password]').type(ADMIN_CREDENTIALS.password);
 
-  //   cy.get('button').should('contain', 'Login').click();
-  //   // Check that URL hasn't changed.
-  //   cy.url().should('eq', BASE_URL);
-  // });
-  // it('No Credentials Login', () => {
-  //   cy.visit(BASE_URL);
+    cy.get('button').should('contain', 'Login').click();
+    // Check that URL hasn't changed.
+    cy.url().should('eq', BASE_URL);
+  });
+  it('No Credentials Login', () => {
+    cy.visit(BASE_URL);
 
-  //   cy.get('button').should('contain', 'Login').click();
-  //   // Check that URL hasn't changed.
-  //   cy.url().should('eq', BASE_URL);
-  // });
+    cy.get('button').should('contain', 'Login').click();
+    // Check that URL hasn't changed.
+    cy.url().should('eq', BASE_URL);
+  });
 });
