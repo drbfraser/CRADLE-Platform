80d76355e517e2d1988e222643deb83c638d3104
Harry Nguyen <hln5@sfu.ca>
2025-02-18 16:23:25 -0800

    refactor(patient forms): get pre-existing data to show on edit personal info and edit pregnancy forms

--- client/src/pages/patient/PersonalInfo.tsx
+++ client/src/pages/patient/PersonalInfo.tsx
@@ -17,66 +17,64 @@ export const PersonalInfo = ({ patient }: IProps) => {
     navigate(`/patients/${patient?.id}/edit/personalInfo`);
 
   return (
-    <Paper>
-      <Box p={3}>
-        <Typography component="h5" variant="h5">
-          <RecentActorsIcon fontSize="large" />
-          Personal Information
-        </Typography>
-        <Divider />
-        {patient ? (
-          <>
-            <Box m={2}>
-              <Grid container spacing={2}>
-                <Grid size={6}>
-                  <b>ID: </b>
-                  {patient.id}
-                </Grid>
-                <Grid size={6}>
-                  <b>Sex: </b>
-                  {patient.sex}
-                </Grid>
-
-                <Grid size={12}>
-                  <b>Age: </b>
-                  {patient.dateOfBirth === undefined ||
-                  patient.dateOfBirth === null
-                    ? `N/A`
-                    : getAgeToDisplay(
-                        patient.dateOfBirth,
-                        patient.isExactDateOfBirth
-                      )}
-                </Grid>
-
-                <Grid size={6}>
-                  <b> Zone: </b>
-                  {patient.zone ? patient.zone : `N/A`}
-                </Grid>
-                <Grid size={6}>
-                  <b>Village Number: </b>
-                  {patient.villageNumber ? patient.villageNumber : `N/A`}
-                </Grid>
-
-                <Grid size={12}>
-                  <b>Household number: </b>
-                  {patient.householdNumber ? patient.householdNumber : `N/A`}
-                </Grid>
-
-                <Grid size={12}>
-                  <b>Allergies: </b>
-                  {patient.allergy ? patient.allergy : `N/A`}
-                </Grid>
+    <Paper sx={{ padding: 3 }}>
+      <Typography component="h5" variant="h5">
+        <RecentActorsIcon fontSize="large" />
+        Personal Information
+      </Typography>
+      <Divider />
+      {patient ? (
+        <>
+          <Box m={2}>
+            <Grid container spacing={2}>
+              <Grid size={6}>
+                <b>ID: </b>
+                {patient.id}
               </Grid>
-            </Box>
+              <Grid size={6}>
+                <b>Sex: </b>
+                {patient.sex}
+              </Grid>
 
-            <SecondaryButton onClick={handleEditClick} size="small">
-              Edit Patient
-            </SecondaryButton>
-          </>
-        ) : (
-          <Skeleton variant="rectangular" height={200} />
-        )}
-      </Box>
+              <Grid size={12}>
+                <b>Age: </b>
+                {patient.dateOfBirth === undefined ||
+                patient.dateOfBirth === null
+                  ? `N/A`
+                  : getAgeToDisplay(
+                      patient.dateOfBirth,
+                      patient.isExactDateOfBirth
+                    )}
+              </Grid>
+
+              <Grid size={6}>
+                <b> Zone: </b>
+                {patient.zone ? patient.zone : `N/A`}
+              </Grid>
+              <Grid size={6}>
+                <b>Village Number: </b>
+                {patient.villageNumber ? patient.villageNumber : `N/A`}
+              </Grid>
+
+              <Grid size={12}>
+                <b>Household number: </b>
+                {patient.householdNumber ? patient.householdNumber : `N/A`}
+              </Grid>
+
+              <Grid size={12}>
+                <b>Allergies: </b>
+                {patient.allergy ? patient.allergy : `N/A`}
+              </Grid>
+            </Grid>
+          </Box>
+
+          <SecondaryButton onClick={handleEditClick} size="small">
+            Edit Patient
+          </SecondaryButton>
+        </>
+      ) : (
+        <Skeleton variant="rectangular" height={200} />
+      )}
     </Paper>
   );
 };
