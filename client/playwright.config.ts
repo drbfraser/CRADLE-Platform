d4f8706371f74f7dce5ee83fe18aa2195107378a
Ryan Milligan <rcm9@sfu.ca>
2025-02-19 15:15:15 -0800

    cicd

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -90,6 +90,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     url: 'http://127.0.0.1:3000',
+    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 });
