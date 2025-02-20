9d7e220d9ce9253ceeefaa5cc86ea2b9136f26fc
Harry Nguyen <hln5@sfu.ca>
2025-02-20 14:59:19 -0800

    refactor(redux reducers): replace secretkey reducer with useQuery calls

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,19 +1,16 @@
 import { UserState, userReducer } from '../reducers/user';
 import { CurrentUserActionEnum } from '../reducers/user/currentUser';
 import { combineReducers } from 'redux';
-import { SecretKeyState, secretKeyReducer } from './secretKey';
 import { SidebarState, sidebarSlice } from '../sidebar-state';
 
 export type ReduxState = {
   user: UserState;
-  secretKey: SecretKeyState;
   drawer: SidebarState;
 };
 
 const createRootReducer = () => {
   return combineReducers({
     user: userReducer,
-    secretKey: secretKeyReducer,
     sidebar: sidebarSlice.reducer,
   });
 };
