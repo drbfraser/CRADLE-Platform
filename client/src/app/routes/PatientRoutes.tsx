e315cd24009827316470f8afc449a56f4f4ecac3
Harry Nguyen <hln5@sfu.ca>
2025-02-20 11:51:13 -0800

    refactor(patient routes): split route components up

--- client/src/app/routes/PatientRoutes.tsx
+++ client/src/app/routes/PatientRoutes.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { PatientPage } from 'src/pages/patient';
 import { PatientFormPage } from 'src/pages/patientForm';
 import EditMedicalRecordForm from 'src/pages/patientForm/components/EditMedicalRecordForm';
@@ -9,44 +9,57 @@ import { NewPatientForm } from 'src/pages/patientForm/components/NewPatientForm'
 import NewPregnancyForm from 'src/pages/patientForm/components/NewPregnancyForm';
 import { PatientsPage } from 'src/pages/patients';
 
+const PatientFormRoutes = () => {
+  const PatientFormNewRoutes = () => (
+    <Routes>
+      <Route
+        path="medicalHistory"
+        element={<NewMedicalRecordForm isDrugHistory={false} />}
+      />
+      <Route
+        path="drugHistory"
+        element={<NewMedicalRecordForm isDrugHistory={true} />}
+      />
+      <Route path="pregnancy" element={<NewPregnancyForm />} />
+    </Routes>
+  );
+  const PatientFormEditRoutes = () => (
+    <Routes>
+      <Route path="personalInfo" element={<EditPersonalInfoForm />} />
+      <Route
+        path="medicalHistory/:recordId"
+        element={<EditMedicalRecordForm isDrugHistory={false} />}
+      />
+      <Route
+        path="drugHistory/:recordId"
+        element={<EditMedicalRecordForm isDrugHistory={true} />}
+      />
+      <Route path="pregnancy/:recordId" element={<EditPregnancyForm />} />
+    </Routes>
+  );
+
+  return (
+    <Routes>
+      <Route element={<PatientFormPage />}>
+        <Route path="new/*" element={<PatientFormNewRoutes />} />
+        <Route path="edit/*" element={<PatientFormEditRoutes />} />
+      </Route>
+    </Routes>
+  );
+};
+
 const PatientRoutes = () => {
   return (
     <Routes>
-      <Route element={<Outlet />}>
-        <Route index element={<PatientsPage />} />
+      <Route index element={<PatientsPage />} />
 
-        <Route path="new" element={<PatientFormPage />}>
-          <Route index element={<NewPatientForm />} />
-        </Route>
+      <Route path="new" element={<PatientFormPage />}>
+        <Route index element={<NewPatientForm />} />
+      </Route>
 
-        <Route path=":patientId/" element={<Outlet />}>
-          <Route index element={<PatientPage />} />
-
-          <Route path="new/" element={<PatientFormPage />}>
-            <Route
-              path="medicalHistory"
-              element={<NewMedicalRecordForm isDrugHistory={false} />}
-            />
-            <Route
-              path="drugHistory"
-              element={<NewMedicalRecordForm isDrugHistory={true} />}
-            />
-            <Route path="pregnancy" element={<NewPregnancyForm />} />
-          </Route>
-
-          <Route path="edit/" element={<PatientFormPage />}>
-            <Route path="personalInfo" element={<EditPersonalInfoForm />} />
-            <Route
-              path="medicalHistory/:recordId"
-              element={<EditMedicalRecordForm isDrugHistory={false} />}
-            />
-            <Route
-              path="drugHistory/:recordId"
-              element={<EditMedicalRecordForm isDrugHistory={true} />}
-            />
-            <Route path="pregnancy/:recordId" element={<EditPregnancyForm />} />
-          </Route>
-        </Route>
+      <Route path=":patientId/">
+        <Route index element={<PatientPage />} />
+        <Route path="*" element={<PatientFormRoutes />} />
       </Route>
     </Routes>
   );
