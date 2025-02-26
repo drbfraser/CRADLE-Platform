d726da07258f01623a62fb4f928c6aa58a3e3d37
Ryan Milligan <rcm9@sfu.ca>
2025-02-25 16:04:54 -0800

    test: add check for referral card

--- client/playwright/createReferral.spec.ts
+++ client/playwright/createReferral.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { TEST_PATIENTS } from './constants';
+import moment from 'moment';
 
 test('Create Referral', async ({ page, browserName }) => {
   const patient = TEST_PATIENTS.AA;
+  const referralComment = `referral-test | patient_id=${
+    patient.id
+  } | browser=${browserName} | dateTime=${moment().format()}`;
+
   await page.goto(`/patients/${patient.id}`);
   await page.waitForURL(`/patients/${patient.id}`);
 
@@ -11,13 +16,16 @@ test('Create Referral', async ({ page, browserName }) => {
 
   await page.getByRole('combobox', { name: 'Refer To' }).click();
   await page.getByText('H1000').click();
-  await page
-    .getByRole('textbox', { name: 'Comments' })
-    .fill(`referral-test | patient_id=${patient.id} | browser=${browserName}`);
+  await page.getByRole('textbox', { name: 'Comments' }).fill(referralComment);
 
   await page.getByRole('button', { name: 'Submit' }).click();
 
   await expect(page).toHaveURL(`/patients/${patient.id}`);
+
   const successAlert = await page.getByRole('alert').getByText('success');
   await expect(successAlert).toBeVisible();
+
+  const referralCard = await page.getByText(referralComment);
+  await referralCard.scrollIntoViewIfNeeded();
+  await expect(referralCard).toContainText(referralComment);
 });
