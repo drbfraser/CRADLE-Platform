53b29758ea44536315b07f3d7f83ace478ad0423
Ryan Milligan <rcm9@sfu.ca>
2025-03-02 12:52:23 -0800

    test: add PatientSummaryPage fixture

---
+++ client/playwright/pages/patient-summary-page.ts
@@ -0,0 +1,24 @@
+import { type Page, type Locator } from '@playwright/test';
+
+export class PatientSummaryPage {
+  readonly page: Page;
+  readonly url: string;
+
+  private readonly createReferralButton: Locator;
+
+  constructor(page: Page, patientId: string) {
+    this.page = page;
+    this.url = `/patients/${patientId}`;
+    this.createReferralButton = page.getByRole('button', {
+      name: 'Create Referral',
+    });
+  }
+
+  async goto() {
+    await this.page.goto(this.url);
+  }
+
+  async clickCreateReferralButton() {
+    await this.createReferralButton.click();
+  }
+}
