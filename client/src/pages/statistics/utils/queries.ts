b3b4df71374fe487b1b683948b1c49499928da5c
Ryan Milligan <rcm9@sfu.ca>
2025-03-04 22:45:19 -0800

    rename user to userId

--- client/src/pages/statistics/utils/queries.ts
+++ client/src/pages/statistics/utils/queries.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUserStatisticsAsync } from 'src/shared/api/apiStatistics';
 
-export const useUserStatsQuery = (user: string, from: number, to: number) => {
+export const useUserStatsQuery = (userId: string, from: number, to: number) => {
   return useQuery({
-    queryKey: ['user', user, from, to],
-    queryFn: () => getUserStatisticsAsync(user, from, to),
-    enabled: !!user,
+    queryKey: ['user', userId, from, to],
+    queryFn: () => getUserStatisticsAsync(userId, from, to),
+    enabled: !!userId,
   });
 };
