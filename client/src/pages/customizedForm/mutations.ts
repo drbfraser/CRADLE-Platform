29512f3b07bd76b566fbe5adeac9fa4c19953110
Harry Nguyen <hln5@sfu.ca>
2025-03-14 09:39:17 -0700

    refactor(custom forms): restructure folders, use tanstack query for mutations and queries, and create layout file

---
+++ client/src/pages/customizedForm/mutations.ts
@@ -0,0 +1,14 @@
+import { useMutation } from '@tanstack/react-query';
+import { saveFormResponseAsync } from 'src/shared/api/api';
+import { PostBody } from './handlers';
+
+export const useSubmitCustomForm = () => {
+  return useMutation({
+    mutationFn: (values: {
+      formId: string | undefined;
+      postBody: PostBody;
+    }) => {
+      return saveFormResponseAsync(values.postBody, values.formId);
+    },
+  });
+};
