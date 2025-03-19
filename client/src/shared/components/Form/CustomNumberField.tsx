dc3f92af2d4b675051e2c6d2e3cef7797ced824d
Zhengying Sun <zhengying_sun@sfu.ca>
2025-03-19 03:38:42 -0700

    Update CustomNumberField.tsx
    Add new props (units, required, min, max) to handle these values.
    Apply these props to the NumericFormat component.

--- client/src/shared/components/Form/CustomNumberField.tsx
+++ client/src/shared/components/Form/CustomNumberField.tsx
@@ -5,13 +5,31 @@ type CustomNumberFieldProps = Omit<NumericFormatProps, 'customInput'> &
   Omit<TextFieldProps, 'defaultValue' | 'type'> & {
     value?: string | number | null;
     step?: number;
+    required?: boolean;
+    units?: string;
+    min?: number;
+    max?: number;
   };
 
 const CustomNumberField = ({
   variant = 'outlined',
+  required = false,
+  units = "",
+  min,
+  max,
   ...props
 }: CustomNumberFieldProps) => {
-  return <NumericFormat customInput={TextField} {...props} />;
+  return (
+    <div className="custom-number-field">
+      <NumericFormat
+        customInput={TextField}
+        required={required}
+        inputProps={{ min, max }}
+        {...props}
+      />
+      {units && <span className="units-label">{units}</span>}
+    </div>
+  );
 };
 
 export default CustomNumberField;
