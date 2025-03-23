cc24b2b7db4a0e21e56e2aa003ba9b60331ef2f4
Ryan Milligan <rcm9@sfu.ca>
2025-03-23 16:24:32 -0700

    fix: set default limit to None

--- server/api/resources/patients.py
+++ server/api/resources/patients.py
@@ -483,6 +483,7 @@ class AdminPatientList(RootModel):
 
 class GetAllPatientsAdminQuery(SearchFilterQueryParams):
     include_archived: Optional[bool] = Field(True, alias="includeArchived")
+    limit: Optional[int] = None
 
 
 # /api/patients/admin
