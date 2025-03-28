3453bcc77e9b1c113d9be930414cafa3c45d10d2
Paul Atwal <paulatwal_@hotmail.com>
2025-03-28 10:43:27 -0700

    Updated based on Ryan's feedback

--- client/playwright/page-object-models/admin-patients-page-model.ts
+++ client/playwright/page-object-models/admin-patients-page-model.ts
@@ -2,48 +2,39 @@ import { type Page, type Locator, expect } from '@playwright/test';
 import { PageObjectModel } from './page-object-model';
 
 export class AdminPatientsPageModel extends PageObjectModel {
-  private readonly patientRow: (name: string) => Locator;
-  private readonly archiveButton: (name: string) => Locator;
-
   constructor(page: Page) {
     super(page, '/admin/patients');
+  }
 
-    this.patientRow = (patientName) =>
-      page.getByRole('gridcell', { name: patientName });
-
-    this.archiveButton = (patientName) =>
-      page
-        .getByRole('row', { name: patientName, exact: false })
-        .getByRole('button');
+  private async waitForPatientsApiResponse() {
+    await this.page.waitForResponse(
+      (response) =>
+        response.url().includes('/api/patients/admin') &&
+        response.status() === 200
+    );
   }
 
   async goto() {
-    const responsePromise = this.page.waitForResponse(
-      (response) =>
-        response.url().includes('includeArchived=true') &&
-        response.status() === 200
-    );
+    const responsePromise = this.waitForPatientsApiResponse();
     await super.goto();
     await responsePromise;
   }
 
   async getPatientRowByName(name: string) {
-    return this.patientRow(name);
+    return this.page.getByRole('gridcell', { name });
   }
 
   // can also be used to unarchive patient
   async archivePatientByName(name: string) {
-    const button = this.archiveButton(name);
-    await button.click();
+    const archiveButton = this.page
+      .getByRole('row', { name, exact: false })
+      .getByRole('button');
+    await archiveButton.click();
 
-    const confirm = this.page.getByRole('button', { name: 'Yes' });
-    await expect(confirm).toBeVisible();
-    await confirm.click();
+    const confirmButton = this.page.getByRole('button', { name: 'Yes' });
+    await expect(confirmButton).toBeVisible();
+    await confirmButton.click();
 
-    await this.page.waitForResponse(
-      (response) =>
-        response.url().includes('/admin?includeArchived=true') &&
-        response.status() === 200
-    );
+    await this.waitForPatientsApiResponse();
   }
 }
--- client/playwright/page-object-models/patients-page-model.ts
+++ client/playwright/page-object-models/patients-page-model.ts
@@ -2,16 +2,11 @@ import { type Page, type Locator, expect } from '@playwright/test';
 import { PageObjectModel } from './page-object-model';
 
 export class PatientsPageModel extends PageObjectModel {
-  private readonly patientRow: (name: string) => Locator;
-
   constructor(page: Page) {
     super(page, '/patients');
-
-    this.patientRow = (patientName) =>
-      page.getByRole('cell', { name: patientName });
   }
 
   async getPatientRowByName(name: string) {
-    return this.patientRow(name);
+    return this.page.getByRole('cell', { name });
   }
 }
