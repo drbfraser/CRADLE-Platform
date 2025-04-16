02cfa6caad3c6d263e4de9e3ee247f57ba8eebe7
Ryan Milligan <rcm9@sfu.ca>
2025-04-16 14:51:05 -0700

    fix: pass algorithm to jwt.decode()

--- server/authentication/sms_auth.py
+++ server/authentication/sms_auth.py
@@ -13,6 +13,7 @@ server once the sender has been authenticated via their SMS secret key.
 """
 
 CRADLE_SMS_ISSUER = "CRADLE-SMS"
+ALGORITHM = "HS256"
 
 
 def _get_sms_secret_key(user_id: int) -> str:
@@ -34,6 +35,7 @@ def create_sms_access_token(user_id: int):
     sms_access_token = jwt.encode(
         payload={"iss": CRADLE_SMS_ISSUER, "sub": str(user_id), "username": username},
         key=sms_secret_key,
+        algorithm=ALGORITHM,
     )
     return sms_access_token
 
@@ -52,7 +54,9 @@ def decode_sms_access_token(access_token: str) -> dict:
         raise ValueError(f"No SMS secret key found for user with ID: {user_id}")
 
     try:
-        payload = jwt.decode(access_token, sms_secret_key)
+        payload = jwt.decode(
+            jwt=access_token, key=sms_secret_key, algorithms=[ALGORITHM]
+        )
     except Exception as err:
         print(err)
         raise ValueError(str(err))
