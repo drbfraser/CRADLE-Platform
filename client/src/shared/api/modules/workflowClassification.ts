af830c5ebaab26bc2d59725bfa64ebb6e90fc693
Ellie Neufeld <ellie_neufeld@sfu.ca>
2025-06-30 16:42:42 -0700

    made workflow classifications API

---
+++ client/src/shared/api/modules/workflowClassification.ts
@@ -0,0 +1,45 @@
+import { axiosFetch } from '../core/http';
+import { EndpointEnum } from '../../enums';
+import { ID } from '../../constants';
+import { WorkflowClassification } from '../../types/workflow/workflowTypes';
+
+// full path
+const CLASSIFICATIONS = EndpointEnum.WORKFLOW_CLASSIFICATIONS;
+
+// specific classification path
+const classificationPath = (id: ID) => `${CLASSIFICATIONS}/${id}`;
+
+// GET /workflow/classifications
+export const listWorkflowClassifications = async (): Promise<
+  WorkflowClassification[]
+> => {
+  const response = await axiosFetch.get<WorkflowClassification[]>(
+    CLASSIFICATIONS
+  );
+  return response.data;
+};
+
+// GET /workflow/classifications/{classificationId}
+export const getWorkflowClassification = async (
+  classificationId: ID
+): Promise<WorkflowClassification> => {
+  const response = await axiosFetch.get<WorkflowClassification>(
+    classificationPath(classificationId)
+  );
+  return response.data;
+};
+
+// POST /workflow/classifications
+export const createClassification = (payload: WorkflowClassification) =>
+  axiosFetch
+    .post<WorkflowClassification>(CLASSIFICATIONS, payload)
+    .then((r) => r.data);
+
+// PUT /workflow/classifications
+export const updateClassification = (
+  classificationId: ID,
+  payload: Partial<WorkflowClassification>
+) =>
+  axiosFetch
+    .put<WorkflowClassification>(classificationPath(classificationId), payload)
+    .then((r) => r.data);
