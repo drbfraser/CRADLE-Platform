1ab90e9c35f7600a721edeaf99c9bc7de7f1901d
James Chuong <jca543@sfu.ca>
2025-07-10 14:58:42 -0700

    Implement function to check if branch conditions exist for POST and PUT requests

--- server/api/resources/workflow_template_steps.py
+++ server/api/resources/workflow_template_steps.py
@@ -31,6 +31,18 @@ api_workflow_template_steps = APIBlueprint(
 )
 
 
+def check_branch_conditions(template_step: dict) -> None:
+    for branch in template_step["branches"]:
+        if branch["condition"] is None and branch["condition_id"] is not None:
+            branch_condition = crud.read(RuleGroupOrm, id=branch["condition_id"])
+
+            if branch_condition is None:
+                return abort(
+                    code=404,
+                    description=f"Branch condition with ID: ({branch['condition_id']}) not found.",
+                )
+
+
 # /api/workflow/template/steps [POST]
 @api_workflow_template_steps.post("", responses={201: WorkflowTemplateStepModel})
 def create_workflow_template_step(body: WorkflowTemplateStepUploadModel):
@@ -59,15 +71,7 @@ def create_workflow_template_step(body: WorkflowTemplateStepUploadModel):
 
     assign_step_ids(WorkflowTemplateStepOrm, template_step, workflow_template.id)
 
-    for branch in template_step["branches"]:
-        if branch["condition"] is None and branch["condition_id"] is not None:
-            branch_condition = crud.read(RuleGroupOrm, id=branch["condition_id"])
-
-            if branch_condition is None:
-                return abort(
-                    code=404,
-                    description=f"Branch condition with ID: ({branch['condition_id']}) not found.",
-                )
+    check_branch_conditions(template_step)
 
     try:
         if template_step["form"] is not None:
@@ -163,6 +167,10 @@ def update_workflow_template_step(
 
     except ValueError:
         return abort(code=404, description="User not found.")
+
+    check_branch_conditions(
+        workflow_template_step_changes
+    )  # If new branches are being added to the step
 
     crud.update(
         WorkflowTemplateStepOrm,
