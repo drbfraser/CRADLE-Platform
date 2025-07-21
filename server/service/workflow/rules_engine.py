f8b112f9b22c7e92cf227928d88fd32f17eec227
jerrydngzh <jerrydeng67@gmail.com>
2025-07-21 01:12:47 -0700

    clean up rule engine definitions and layout logic, add typings

--- server/service/workflow/rules_engine.py
+++ server/service/workflow/rules_engine.py
@@ -1,31 +1,32 @@
+import json
+from typing import Dict, Any
 from json_logic import jsonLogic
 
-class RulesEngineFacade:
+class RulesEngine:
     """
     An abstraction layer for the underlying Rules Engine Implementation
     """
-    def __init__(self, args, ruleGroup, rules):
+    def __init__(self, args: Dict[str, Any], rule_group: str):
         """
         Initializes the rules engine
  
-        :param a: list of data args used in the rules
-        :param b: a json object defining how we want to combine given list of rules
-        :param c: list of rules
-        :rtype: RulesEngineFacade
+        :param args: dict of resolved datasources used in the rules
+        :param rule_group: a json object defining how we want to combine given list of rules
+        :rtype: RulesEngine
         """
-        self.rulesEngine = RulesEngineImpl(args, ruleGroup, rules)
+        self.rulesEngine = _RulesEngineImpl(args, rule_group)
 
-    def evaluate(self, input):
+    def evaluate(self, input: Dict[str, Any]):
         """
         Evaluate the given rules
 
-        :param a: an input data object
-        :rtype: bool
+        :param input: an input data object
+        :rtype: Result object
         """
         return self.rulesEngine.evaluate(input)
 
 
-class RulesEngineImpl:    
+class _RulesEngineImpl:
     """
     example 1
     {
@@ -40,7 +41,7 @@ class RulesEngineImpl:
             { "<" : [{"var": "age"}, 18]},
             {
                 "or": [
-                    {"==": [{"var": "b"}, "$table.column$"]},
+                    {"==": [{"var": "b"}, "$table.column"]},
                     {"!=": [{"var": "a"}, 15]}
                 ]
             },
@@ -48,18 +49,17 @@ class RulesEngineImpl:
         ]
     }
     """
-    def __init__(self, args, rg, _):
-        self.parsed_rules = self._parse_rules(args, rg, _)
+    def __init__(self, args: Dict[str, Any], rg: str):
+        self.parsed_rules = self._parse_rules(args, rg)
         
-    def _parse_rules(self, args, rg, _):
+    def _parse_rules(self, args: Dict[str, Any], rg: str) -> Dict:
         """
         processes given input arguments and rule group 
         into a rule object ready for evaluation 
         
-        :param a: list of data source args
-        :param b: a rule group object combining the rules
-        :param c: unsused
-        :rtype: a json object representing a formed rule
+        :param args: a dict of resolved datasource args
+        :param rg: a rule group object
+        :rtype: a dict representing a formed rule
         """
 
         # method 1
@@ -70,11 +70,14 @@ class RulesEngineImpl:
 
         # method 2
         # deserialize into a dict
-        # recursively search this dict until we find datasource strings (assume format: $table.column$)
+        rule = json.loads(rg)
+
+        # TODO
+        # recursively search this dict until we find datasource strings (assume format: "$table.column")
         # update the value with list of args
         # repeat until all args are completed
-        return ""
+        
+        return rule
         
     def evaluate(self, input):
-        res = jsonLogic(self.parsed_rules, input)
-        return True
+        return jsonLogic(self.parsed_rules, input)
