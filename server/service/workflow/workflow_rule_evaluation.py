68209729694d9a66b3efcf9f5a7db23ccc998ee6
jerrydngzh <jerrydeng67@gmail.com>
2025-07-21 01:14:34 -0700

    define out and clean up service on data orchetration, add typings to function declarations

--- server/service/workflow/workflow_rule_evaluation.py
+++ server/service/workflow/workflow_rule_evaluation.py
@@ -1,5 +1,14 @@
 # TODO: create an interface out of this?
 #       -> can make mocks for testing (but idk if it is "pythonic")
+from typing import Dict, Any
+from rules_engine import RulesEngine
+from workflow_datasources import WorkflowDatasourcing
+import data.crud as dl
+
+class EvaluteResult:
+    value: bool
+    details: str
+
 class WorkflowEvaluationService:
     """
     responsible for:
@@ -33,28 +42,46 @@ class WorkflowEvaluationService:
         self.datasource_service = ds
         self.rule_engine_service = re 
     
-    def get_data(self):
+    def get_data(self, wf_instance_step_id: str) -> tuple[str, dict[str]]:
         """
-        get workflow data
-        - rule group
-        - rules
+        Retrieve relevant information required for a rule evaluation
+        - workflow_instance_step object
+        - w/ the object, get the condition_id, fetch a rule_group object
 
-        :param a: rule group id
-        :rtype: a rule group object
-        """        
-        pass
+        get the datasource strings in the rule_group object, resolve the 
+        datasource strings via the datasource service
+        
+        :param wf_instance_step_id: an id for a workflow instance step
+        :rtype: a tuple of rule_group and resolved datasource strings 
+        """
+        # TODO: Get workflow_instance_step object
+        # dl.read_workflow_instance_step(wf_instance_step_id)
 
-    def _process_arguments(self):
-        """
-        get and process arguments
-        """
-        pass
+        # TODO: Get rule_group object
+        # dl.read_rule_group()
+        rule_group = ""
+        datasources = [""]
 
-    def evaluate_rule_engine(self):
-        """
-        call the rules engine
-        """
-        pass
+        # NOTE stubbed
+        # Resolve the datasource strings
+        # returns a dict { "datasource_key":"value" }
+        resolved_data = WorkflowDatasourcing.resolve_datasources(datasources)
+        
+        return (rule_group, resolved_data)
 
-    def _handle_result(self):
-        pass
+    def evaluate_rule_engine(self, input_data: str, rule_group: str, datasources: Dict[str, Any]) -> EvaluteResult:
+        """
+        Call the engine to evalaute a rule
+        - get a scoped instance of the rule engine
+        - give it a rule group and the list of datasource args
+        - handle any results if needed
+
+        :param input_data: an json object representing input data from a form
+        :param rule_group: a rule group json string
+        :rtype: a EvalauteResult object containing the evalauted result
+        """
+        # "instantiate"
+        re = RulesEngine(datasources, rule_group)
+
+        # evaluate
+        return re.evaluate(input_data)
