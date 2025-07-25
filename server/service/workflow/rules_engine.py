97c5f79566fce89b4e70455ca6fdd1d8137a5e44
jerrydngzh <jerrydeng67@gmail.com>
2025-07-24 19:47:37 -0700

    add some examples for other structures to support, re-approach processing for rule engine

--- server/service/workflow/rules_engine.py
+++ server/service/workflow/rules_engine.py
@@ -32,31 +32,71 @@ class RulesEngine:
 
 class _RulesEngineImpl:
     """
-    example 1
+    example 1 -- fixed data
     {
         "and": [
             { ">" : [ {"var": "age"}, 18 ] },
             { "==" : [ {"var": "reading.status" }, "yellow"] }
         ]
     }
-    example 2
+    example 2 -- complex: fixed, lists, datasourcing w/ default values
+    - every datasource string would be in the form: {"var": ["$table.column", <default value>]}
+    - resolved datasource dne -> don't need to include it / give it a value of None
     {
         "and": [
             { "<" : [{"var": "age"}, 18]},
             {
                 "or": [
-                    {"==": [{"var": "b"}, "$table.column"]},
+                    {"==": [{"var": "b"}, {"var": ["$table.column"}, <default value>]]},
                     {"!=": [{"var": "a"}, 15]}
                 ]
             },
             {"in": ["banana", {"var": "fruits_list"}]}
         ]
     }
+
+    -- if we want date eval, FE will need to handle logic around getting this
+
+    example 3 -- dates
+    - predicates/input data we expect to be dates is a value in a `{"date": ... }` dict
+    - applies for variables and constant data
+    - data format: a date string: "yyyy-mm-dd"
+    - {"today": []} operator gets current date (replace w/ a `{"date": ...}` obj)
+    {
+        "<=": [
+            {"date": {"var": "testDate"}}, {"date": "2021-01-01"}
+        ]
+    }
+    
+    example 4 -- date times
+    - same idea as example 3, datetimes are in a `{"datetime": ... }` dict
+    - data format: a datetime string
+    - some special work will need to be done if we want to get current datetime
+       - i.e. introduce our own operator (switch to another implementation)
+       - learn and experiment w/ the datetime operations to compose a way to compare
+         e.g. on a patients age, but computing it via rules
+    {
+        "<=": [
+            {"datetime": {"var": "testDatetime"}},
+            {"datetime": "2022-12-01T10:00:00.000+02:00"},
+        ]
+    }
+    e.g.: 
+    {
+        "-": [
+            {"datetime": 2022-12-01T12:00:00.000+02:00},
+            {"datetime": "2022-11-01T10:00:00.000+01:00"},
+        ]
+    }
+    - generates a ISO Duration result format, some way to interpret a result, or find another
+      way to compute the given example
     """
 
-    def __init__(self, args: Dict[str, Any], rg: str):
-        self.parsed_rules = self._parse_rules(args, rg)
+    def __init__(self, args: Dict[str, Any], rule: str):
+        self.args: Dict[str, Any] = args
+        self.rule = rule
 
+    # not needed anymore
     def _parse_rules(self, args: Dict[str, Any], rule: str) -> Dict:
         """
         processes given input arguments and rule
@@ -92,4 +132,10 @@ class _RulesEngineImpl:
         :returns: the result from jsonLogic
         :rtype: a boolean
         """
+        
+        # inject datasources into the input data object
+        # let jsonlogic do resolution for us 
+        for key, value in self.args.items():
+            input[key] = value 
+        
         return jsonLogic(self.parsed_rules, input)
