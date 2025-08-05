e056d41242641c1ad8fde3818de16e96b093f7ca
James Chuong <jca543@sfu.ca>
2025-08-04 21:33:25 -0700

    Implement DB model for workflow collection, add foreign key and relationship between workflow classifications and collections

--- server/models.py
+++ server/models.py
@@ -698,10 +698,32 @@ class SmsSecretKeyOrm(db.Model):
         return SmsSecretKeySchema
 
 
+class WorkflowCollectionOrm(db.Model):
+    __tablename__ = "workflow_collection"
+    id = db.Column(db.String(50), primary_key=True, nullable=False, default=get_uuid)
+    name = db.Column(db.String(200), index=True, nullable=False)
+
+    @staticmethod
+    def schema():
+        return WorkflowCollectionSchema
+
+
 class WorkflowClassificationOrm(db.Model):
     __tablename__ = "workflow_classification"
     id = db.Column(db.String(50), primary_key=True, nullable=False, default=get_uuid)
     name = db.Column(db.String(200), index=True, nullable=False)
+
+    # FOREIGN KEYS
+    collection_id = db.Column(
+        db.ForeignKey(WorkflowCollectionOrm.id, ondelete="SET NULL"), nullable=True
+    )
+
+    # RELATIONSHIPS
+    collection = db.relationship(
+        WorkflowCollectionOrm,
+        backref=db.backref("workflow_classifications", lazy=True),
+        passive_deletes=True,
+    )
 
     @staticmethod
     def schema():
@@ -1154,6 +1176,14 @@ class RuleGroupSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         include_fk = True
         model = RuleGroupOrm
+        load_instance = True
+        include_relationships = True
+
+
+class WorkflowCollectionSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        include_fk = True
+        model = WorkflowCollectionOrm
         load_instance = True
         include_relationships = True
 
