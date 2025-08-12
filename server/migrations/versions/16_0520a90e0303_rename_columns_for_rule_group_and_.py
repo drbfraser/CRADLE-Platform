0dfa6825f8262da2d88b29483a63acee64a9d113
jerrydngzh <jerrydeng67@gmail.com>
2025-08-12 01:26:39 -0700

    some reason files didnt save in commit

--- server/migrations/versions/16_0520a90e0303_rename_columns_for_rule_group_and_.py
+++ server/migrations/versions/16_0520a90e0303_rename_columns_for_rule_group_and_.py
@@ -1,6 +1,6 @@
 """Rename columns for rule_group and update types from TEXT to JSON
 
-Revision ID: 0520a90e0303
+Revision ID: 16_0520a90e0303
 Revises: 15_bf923c5cb3c3
 Create Date: 2025-08-12 01:24:32.468246
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0520a90e0303'
+revision = '16_0520a90e0303'
 down_revision = '15_bf923c5cb3c3'
 branch_labels = None
 depends_on = None
