0f4c18c330284c59eb7eaf44da44d8f510bc4fd9
Ilia Fatemi <ifa6@sfu.ca>
2025-09-25 15:20:28 -0700

    Updated function call to the refactored module

--- server/api/resources/assessments.py
+++ server/api/resources/assessments.py
@@ -5,6 +5,7 @@ from flask_openapi3.models.tag import Tag
 from common.api_utils import AssessmentIdPath
 from common.user_utils import get_current_user_from_jwt
 from data import crud, marshal
+from data.db_operations import common_crud
 from models import AssessmentOrm
 from validation.assessments import (
     AssessmentList,
@@ -40,7 +41,7 @@ def create_new_assessment(body: AssessmentPostBody):
     if body.healthcare_worker_id is None:
         body.healthcare_worker_id = get_current_user_from_jwt()["id"]
     assessment = marshal.unmarshal(AssessmentOrm, body.model_dump())
-    crud.create(assessment, refresh=True)
+    common_crud.create(assessment, refresh=True)
     return marshal.marshal(assessment), 201
 
 
