79123d352479bdd98fbc94dd899b0121e83e0c96
Maria Zaman <maria_zaman@sfu.ca>
2025-10-31 11:26:25 -0700

    912: Integrate WorkflowPlanner with WorkflowService (#916)
    - Add `WorkflowAction` type
    - Add `WorkflowOp` type
    - Add methods to `WorkflowService` interface:
      - `get_available_workflow_actions()`
      - `apply_workflow_action()`
    - Change `WorkflowPlanner` interface a bit:
      -  remove `plan()`
      - add `get_available_actions()`
      - add `get_operations()`
    - Add and update workflow unit tests

--- server/service/workflow/workflow_planner.py
+++ server/service/workflow/workflow_planner.py
@@ -1,76 +1,21 @@
-from dataclasses import dataclass
 from typing import Optional
 
-from enums import StrEnum, WorkflowStatusEnum, WorkflowStepStatusEnum
-from validation.workflow_models import (
-    WorkflowInstanceModel,
-    WorkflowInstanceStepModel,
-    WorkflowTemplateModel,
-    WorkflowTemplateStepModel,
+from enums import WorkflowStatusEnum, WorkflowStepStatusEnum
+from service.workflow.workflow_errors import InvalidWorkflowActionError
+from service.workflow.workflow_operations import (
+    UpdateCurrentStepOp,
+    UpdateStepStatusOp,
+    UpdateWorkflowStatusOp,
+    WorkflowOp,
 )
-
-
-class WorkflowOpName(StrEnum):
-    START_WORKFLOW = "start_workflow"
-    COMPLETE_WORKFLOW = "complete_workflow"
-    START_STEP = "start_step"
-    COMPLETE_STEP = "complete_step"
-    TRANSITION_TO_STEP = "transition_to_step"
-
-
-@dataclass
-class WorkflowOp:
-    name: WorkflowOpName
-    params: dict = None
-
-    def __repr__(self):
-        params_str = f", params={self.params}" if self.params else ""
-        return f"WorkflowOp(name='{self.name}'{params_str})"
-
-
-class WorkflowView:
-    """
-    A read-only view over a workflow instance and its template that provides
-    convenient access to workflow steps.
-
-    Should be stable because:
-        - It holds references to the workflow models, so changes to state made
-          externally are reflected automatically
-        - Step ID mappings should be fixed for a given template-instance pair
-    """
-
-    def __init__(
-        self, template: WorkflowTemplateModel, instance: WorkflowInstanceModel
-    ):
-        self.template = template
-        self.instance = instance
-
-        self._instance_steps_by_id = {step.id: step for step in instance.steps}
-        self._template_steps_by_id = {step.id: step for step in template.steps}
-        self._template_step_id_to_instance_step_id = {
-            step.workflow_template_step_id: step.id for step in instance.steps
-        }
-
-    def get_instance_step(self, step_id: str) -> WorkflowInstanceStepModel:
-        return self._instance_steps_by_id[step_id]
-
-    def get_template_step(self, step_id: str) -> WorkflowTemplateStepModel:
-        return self._template_steps_by_id[step_id]
-
-    def get_instance_step_for_template_step(
-        self, template_step_id: str
-    ) -> WorkflowInstanceStepModel:
-        return self.get_instance_step(
-            self._template_step_id_to_instance_step_id[template_step_id]
-        )
-
-    def get_starting_step(self) -> WorkflowTemplateStepModel:
-        return self._template_steps_by_id[self.template.starting_step_id]
-
-    def get_current_step(self) -> Optional[WorkflowInstanceStepModel]:
-        if self.instance.current_step_id:
-            return self._instance_steps_by_id[self.instance.current_step_id]
-        return None
+from service.workflow.workflow_view import WorkflowView
+from validation.workflow_models import (
+    CompleteStepActionModel,
+    StartStepActionModel,
+    StartWorkflowActionModel,
+    WorkflowActionModel,
+    WorkflowInstanceStepModel,
+)
 
 
 class WorkflowPlanner:
@@ -79,8 +24,9 @@ class WorkflowPlanner:
     a workflow. Doesn't mutate any workflow state.
     """
 
+    @staticmethod
     def _eval_next_step_from_this_step(
-        self, ctx: WorkflowView, step: WorkflowInstanceStepModel
+        ctx: WorkflowView, step: WorkflowInstanceStepModel
     ) -> Optional[WorkflowInstanceStepModel]:
         """
         Evaluate the next step to go to from this step.
@@ -99,81 +45,83 @@ class WorkflowPlanner:
 
         return next_step
 
-    def _plan_start_workflow(self, ctx: WorkflowView) -> list[WorkflowOp]:
-        assert ctx.instance.status == WorkflowStatusEnum.PENDING
-        assert ctx.get_current_step() is None
+    @staticmethod
+    def _is_last_step(ctx: WorkflowView, step: WorkflowInstanceStepModel) -> bool:
+        next_step = WorkflowPlanner._eval_next_step_from_this_step(ctx, step)
+        return next_step is None
 
-        start_step = ctx.get_instance_step_for_template_step(ctx.get_starting_step().id)
-        assert start_step.status == WorkflowStepStatusEnum.PENDING
-
-        return [
-            WorkflowOp(name=WorkflowOpName.START_WORKFLOW),
-            WorkflowOp(
-                name=WorkflowOpName.TRANSITION_TO_STEP,
-                params={"from_step_id": None, "to_step_id": start_step.id},
-            ),
-            WorkflowOp(
-                name=WorkflowOpName.START_STEP, params={"step_id": start_step.id}
-            ),
-        ]
-
-    def _plan_complete_step(self, ctx: WorkflowView, step_id: str) -> list[WorkflowOp]:
+    @staticmethod
+    def get_available_actions(ctx: WorkflowView) -> list[WorkflowActionModel]:
+        """
+        Returns the actions available to take as the next action in the workflow.
+        """
         current_step = ctx.get_current_step()
-        assert (
-            current_step and current_step.id == step_id
-        ), "Step ID must be the current step"
 
-        ops = [
-            WorkflowOp(
-                name=WorkflowOpName.COMPLETE_STEP, params={"step_id": current_step.id}
-            ),
-        ]
+        if ctx.instance.status == WorkflowStatusEnum.PENDING:
+            return [StartWorkflowActionModel()]
 
-        # Determine the next step
-        next_step = self._eval_next_step_from_this_step(ctx, current_step)
+        if current_step:
+            if current_step.status == WorkflowStepStatusEnum.PENDING:
+                return [StartStepActionModel(step_id=current_step.id)]
 
-        ops.append(
-            WorkflowOp(
-                name=WorkflowOpName.TRANSITION_TO_STEP,
-                params={
-                    "from_step_id": current_step.id,
-                    "to_step_id": next_step.id if next_step else None,
-                },
-            )
-        )
+            if current_step.status == WorkflowStepStatusEnum.ACTIVE:
+                return [CompleteStepActionModel(step_id=current_step.id)]
 
-        if next_step:
-            ops.append(
-                WorkflowOp(
-                    name=WorkflowOpName.START_STEP, params={"step_id": next_step.id}
+            if current_step.status == WorkflowStepStatusEnum.COMPLETED:
+                next_step = WorkflowPlanner._eval_next_step_from_this_step(
+                    ctx=ctx, step=current_step
                 )
-            )
 
-        return ops
+                if next_step:
+                    # Currently, step should always be in PENDING state, so the only available
+                    # action is to start it
+                    return [StartStepActionModel(step_id=next_step.id)]
 
-    def plan(
-        self,
-        ctx: WorkflowView,
-        operation: Optional[WorkflowOp],
+        return []
+
+    @staticmethod
+    def get_operations(
+        ctx: WorkflowView, action: WorkflowActionModel
     ) -> list[WorkflowOp]:
-        ops = []
+        """
+        Translate an action into the workflow operations that should all be applied to
+        the workflow instance to apply that action.
+        """
+        valid_actions = WorkflowPlanner.get_available_actions(ctx)
 
-        if operation:
-            if operation.name == WorkflowOpName.START_WORKFLOW:
-                ops.extend(self._plan_start_workflow(ctx))
-            elif operation.name == WorkflowOpName.COMPLETE_STEP:
-                step_id = operation.params.get("step_id")
-                ops.extend(self._plan_complete_step(ctx, step_id))
-            else:
-                raise NotImplementedError(
-                    f"Operation '{operation}' not supported by planner"
+        if action not in valid_actions:
+            raise InvalidWorkflowActionError(action, valid_actions)
+
+        if isinstance(action, StartWorkflowActionModel):
+            starting_step = ctx.get_instance_step_for_template_step(
+                ctx.get_starting_step().id
+            )
+
+            return [
+                UpdateWorkflowStatusOp(WorkflowStatusEnum.ACTIVE),
+                UpdateCurrentStepOp(starting_step.id),
+                UpdateStepStatusOp(starting_step.id, WorkflowStepStatusEnum.ACTIVE),
+            ]
+
+        if isinstance(action, StartStepActionModel):
+            return [
+                UpdateCurrentStepOp(new_current_step_id=action.step_id),
+                UpdateStepStatusOp(action.step_id, WorkflowStepStatusEnum.ACTIVE),
+            ]
+
+        if isinstance(action, CompleteStepActionModel):
+            ops = [UpdateStepStatusOp(action.step_id, WorkflowStepStatusEnum.COMPLETED)]
+
+            step = ctx.get_instance_step(action.step_id)
+
+            if WorkflowPlanner._is_last_step(ctx, step):
+                ops.extend(
+                    [
+                        UpdateCurrentStepOp(new_current_step_id=None),
+                        UpdateWorkflowStatusOp(WorkflowStatusEnum.COMPLETED),
+                    ]
                 )
 
-        all_steps_completed = all(
-            step.status == WorkflowStepStatusEnum.COMPLETED
-            for step in ctx.instance.steps
-        )
-        if all_steps_completed and ctx.instance.status != WorkflowStatusEnum.COMPLETED:
-            ops.append(WorkflowOp(name=WorkflowOpName.COMPLETE_WORKFLOW))
+            return ops
 
-        return ops
+        raise ValueError(f"Action '{action}' is not supported")
