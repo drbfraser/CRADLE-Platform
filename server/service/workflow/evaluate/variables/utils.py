b1703334438b5e1cce4c66d7b9e4f39bee6fd83a
Ilia Fatemi <ifa6@sfu.ca>
2025-11-07 00:10:05 -0800

    More ruff fix

--- server/service/workflow/evaluate/variables/utils.py
+++ server/service/workflow/evaluate/variables/utils.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 import json
 import logging
-from typing import Any, Dict, Iterable, List, Union
+from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Union
 
-from validation.workflow_models import WorkflowTemplateModel
+if TYPE_CHECKING:
+    from validation.workflow_models import WorkflowTemplateModel
 
 Json = Dict[str, Any]
 TemplateLike = Union[Json, Any]
