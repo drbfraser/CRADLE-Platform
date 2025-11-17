4cb9ec2e8bbfb6503713a2aa3e654a7c2ac2ddd0
Poorvi <poorvi.b26@gmail.com>
2025-11-17 00:05:31 -0800

    feat: upload new form template body + refactor pydantic models

--- server/validation/formsV2_models.py
+++ server/validation/formsV2_models.py
@@ -1,12 +1,12 @@
-from typing import Any, List, Optional
+from typing import List, Optional, Union
 
-from pydantic import Field
+from pydantic import Field, RootModel
 
 from common.api_utils import (
     FormTemplateIdPath,
 )
 from common.commonUtil import get_current_time
-from enums import QuestionTypeEnum
+from enums import QRelationalEnum, QuestionTypeEnum
 from validation import CradleBaseModel
 
 
@@ -47,39 +47,86 @@ class LangVersion(CradleBaseModel):
     text: str
 
 
-class LangVersionResponse(CradleBaseModel):
-    id: Optional[int] = None
-    lang: str
-    questionId: str
-    questionText: str
+class MultiLangText(RootModel[dict[str, str]]):
+    """
+    Represents multilingual text like:
+    {"english": "Hello", "french": "Bonjour"}
+    """
+
+
+
+class NumberAnswer(CradleBaseModel):
+    number: float
+
+
+class TextAnswer(CradleBaseModel):
+    text: str
+
+
+class OptionAnswer(CradleBaseModel):
+    options: List[str]
+
+
+class DateAnswer(CradleBaseModel):
+    date: str
+
+
+AnswerType = Union[
+    NumberAnswer,
+    TextAnswer,
+    OptionAnswer,
+    DateAnswer,
+]
+
+
+class VisibleCondition(CradleBaseModel, use_enum_values=True):
+    """
+    Valid example:
+        {
+            "question_index": 1,
+            "relation": "EQUAL_TO",
+            "answers": {
+                "number": 5
+            }
+        }
+    """
+
+    question_index: int
+    answers: AnswerType
+    relation: QRelationalEnum
 
 
 class QuestionResponse(CradleBaseModel):
     id: str
-    formTemplateId: str
-    questionType: QuestionTypeEnum
+    form_template_id: str
+    question_type: QuestionTypeEnum
     order: int
-    questionText: str = ""
-    categoryIndex: Optional[int] = None
+
+    # can be either {"english": "..."} or "Basic Info"
+    question_text: Union[MultiLangText, str]
+
+    category_index: Optional[int] = None
     required: bool
-    hasCommentAttached: bool
-    allowFutureDates: bool
-    allowPastDates: bool
-    visibleCondition: List[Any] = []
-    mcOptions: List[str] = []
-    langVersions: List[LangVersionResponse]
+    has_comment_attached: bool
+    allow_future_dates: bool
+    allow_past_dates: bool
+    visible_condition: Optional[list[VisibleCondition]] = []
+
+    # can be ["Male", "Female"] or [{"english": "Male"}, {...}]
+    mc_options: Optional[List[Union[MultiLangText, str]]] = None
 
 
 class FormClassification(CradleBaseModel):
-    id: str
-    name: str
+    id: Optional[str] = None
+    name: MultiLangText
+    name_string_id: Optional[str] = None
 
 
 class FormTemplateResponse(CradleBaseModel):
     id: str
     archived: bool
     classification: FormClassification
-    dateCreated: int
+    date_created: int
     version: str
     questions: List[QuestionResponse]
 
@@ -107,3 +154,57 @@ class ArchiveFormTemplateQuery(CradleBaseModel):
         True,
         description="If true, the Form Template will be archived. If false, the Form Template will be unarchived.",
     )
+
+
+# class MCOption(CradleBaseModel):
+#     string_id: Optional[str] = None
+#     translations: Dict[str, str] = Field(default_factory=dict)
+
+#     class Config:
+#         extra = "allow"
+
+
+class MCOption(RootModel[dict[str, str]]):
+    """
+    Represents multilingual text like:
+    {"english": "Hello", "french": "Bonjour"}
+    """
+
+
+
+class FormTemplateUploadQuestion(CradleBaseModel):
+    id: Optional[str] = None
+    form_template_id: Optional[str] = None
+    question_type: QuestionTypeEnum
+    order: int
+
+    question_text: MultiLangText
+    question_string_id: Optional[str] = None
+    required: bool
+    has_comment_attached: Optional[bool] = False
+    allow_future_dates: Optional[bool] = None
+    allow_past_dates: Optional[bool] = None
+
+    visible_condition: Optional[list[VisibleCondition]] = []
+
+    category_index: Optional[int] = None
+
+    string_max_length: Optional[int] = None
+    string_max_lines: Optional[int] = None
+
+    num_min: Optional[float] = None
+    num_max: Optional[float] = None
+    units: Optional[str] = None
+
+    user_question_id: Optional[str] = None
+
+    mc_options: Optional[List[MCOption]] = None
+
+
+class FormTemplateUploadRequest(CradleBaseModel):
+    id: Optional[str] = None
+    version: Optional[int]
+    archived: bool = False
+    classification: FormClassification
+    date_created: int = get_current_time()
+    questions: List[FormTemplateUploadQuestion]
