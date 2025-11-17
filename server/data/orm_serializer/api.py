85f7313f20228724a049ee16a5c601d9aa77451d
Ilia Fatemi <ifa6@sfu.ca>
2025-11-16 17:14:58 -0800

    removed __marshal_form_classification_v2 since there is no function definition

--- server/data/orm_serializer/api.py
+++ server/data/orm_serializer/api.py
@@ -34,7 +34,6 @@ from models import (
 from .forms import (
     __marshal_form,
     __marshal_form_answer_v2,
-    __marshal_form_classification_v2,
     __marshal_form_question_template_v2,
     __marshal_form_submission_v2,
     __marshal_form_template,
@@ -142,8 +141,6 @@ def marshal(obj: Any, shallow: bool = False, if_include_versions: bool = False) 
         return __marshal_workflow_collection(obj, shallow)
     if isinstance(obj, LangVersionOrmV2):
         return __marshal_lang_version_v2(obj)
-    if isinstance(obj, FormClassificationOrmV2):
-        return __marshal_form_classification_v2(obj, if_include_versions)
     if isinstance(obj, FormTemplateOrmV2):
         return __marshal_form_template_v2(obj, shallow)
     if isinstance(obj, FormQuestionTemplateOrmV2):
