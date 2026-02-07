a51407a8ed312a9170fbe8ae2cfbaf7269180f1d
Zoe Wong <zoelevelup@gmail.com>
2026-02-07 14:45:18 -0800

    Removed add branch option and default add button to insert a new node

--- client/src/shared/components/workflow/workflowTemplate/NodeHandler.tsx
+++ client/src/shared/components/workflow/workflowTemplate/NodeHandler.tsx
@@ -1,51 +1,27 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import { Handle, Position } from '@xyflow/react';
-import { Menu, MenuItem, ListItemIcon, ListItemText, Box } from '@mui/material';
+import { Box } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import AccountTreeIcon from '@mui/icons-material/AccountTree';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 interface NodeHandlerProps {
   isEditMode: boolean;
   onInsertNode: () => void;
-  onAddBranch: () => void;
 }
 
 export const NodeHandler: React.FC<NodeHandlerProps> = ({
   isEditMode,
   onInsertNode,
-  onAddBranch,
+
 }) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const handleRef = useRef<HTMLDivElement>(null);
-  const open = Boolean(anchorEl);
 
   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
     if (!isEditMode) return;
     event.stopPropagation();
-    setAnchorEl(handleRef.current);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleInsertNode = (event: React.MouseEvent) => {
-    event.stopPropagation();
     onInsertNode();
-    handleClose();
-  };
-
-  const handleAddBranch = (event: React.MouseEvent) => {
-    event.stopPropagation();
-    onAddBranch();
-    handleClose();
   };
 
   return (
-    <>
       <Box
-        ref={handleRef}
         onClick={handleClick}
         sx={{
           position: 'absolute',
@@ -87,37 +63,5 @@ export const NodeHandler: React.FC<NodeHandlerProps> = ({
           />
         )}
       </Box>
-      {isEditMode && (
-        <Menu
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          onClick={(e) => e.stopPropagation()}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-          sx={{
-            mt: 1,
-          }}>
-          <MenuItem onClick={handleInsertNode}>
-            <ListItemIcon>
-              <AddCircleOutlineIcon fontSize="small" />
-            </ListItemIcon>
-            <ListItemText>Insert node</ListItemText>
-          </MenuItem>
-          <MenuItem onClick={handleAddBranch}>
-            <ListItemIcon>
-              <AccountTreeIcon fontSize="small" />
-            </ListItemIcon>
-            <ListItemText>Add branch</ListItemText>
-          </MenuItem>
-        </Menu>
-      )}
-    </>
   );
 };
