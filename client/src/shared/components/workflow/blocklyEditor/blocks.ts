9f1f646f290c3c7c133dd1a39c817e1eb61eb333
asa522 <asa522@sfu.ca>
2026-04-15 14:34:33 -0400

    convert blocks.ts to exported registerBlocks function

--- client/src/shared/components/workflow/blocklyEditor/blocks.ts
+++ client/src/shared/components/workflow/blocklyEditor/blocks.ts
@@ -1,103 +1,130 @@
 import * as Blockly from 'blockly';
+import { WorkflowVariable } from 'src/shared/api/modules/workflowVariables';
 
-// TODO: Replace with dynamic fetch from GET /api/workflow/variables.
-const VARIABLE_OPTIONS: [string, string, string][] = [
-  ["Patient's Age", 'patient.age', 'Number'],
-];
+function blocklyTypeFromVariableType(
+  type: WorkflowVariable['type']
+): string | null {
+  switch (type) {
+    case 'integer':
+    case 'double':
+      return 'Number';
+    case 'boolean':
+      return 'Boolean';
+    case 'string':
+    case 'date':
+      return 'String';
+    default:
+      return null;
+  }
+}
 
-const VARIABLE_DROPDOWN: [string, string][] = VARIABLE_OPTIONS.map(
-  ([display, value]) => [display, value]
-);
+function variableDisplayName(v: WorkflowVariable): string {
+  return v.description ?? v.tag;
+}
 
-const VARIABLE_TYPE_MAP = new Map(
-  VARIABLE_OPTIONS.map(([, value, type]) => [value, type])
-);
+export function registerBlocks(variables: WorkflowVariable[]): void {
+  const options = variables.map(
+    (v) =>
+      [variableDisplayName(v), v.tag, blocklyTypeFromVariableType(v.type)] as [
+        string,
+        string,
+        string | null,
+      ]
+  );
 
-Blockly.Blocks['app_variable'] = {
-  init: function (this: Blockly.Block) {
-    this.appendDummyInput().appendField(
-      new Blockly.FieldDropdown(VARIABLE_DROPDOWN),
-      'VAR_NAME'
-    );
-    this.setOutput(true, VARIABLE_OPTIONS[0]?.[2] ?? null);
-    this.setColour(230);
-  },
-  onchange: function (this: Blockly.Block) {
-    const varName = this.getFieldValue('VAR_NAME');
-    const type = VARIABLE_TYPE_MAP.get(varName) ?? null;
-    this.outputConnection?.setCheck(type);
-  },
-};
+  const dropdown: [string, string][] = options.map(([display, value]) => [
+    display,
+    value,
+  ]);
 
-Blockly.Blocks['comparison'] = {
-  init: function (this: Blockly.Block) {
-    this.appendValueInput('LEFT');
-    this.appendDummyInput().appendField(
-      new Blockly.FieldDropdown([
-        ['<', '<'],
-        ['>', '>'],
-        ['=', '=='],
-        ['≤', '<='],
-        ['≥', '>='],
-        ['≠', '!='],
-      ]),
-      'OP'
-    );
-    this.appendValueInput('RIGHT');
-    this.setInputsInline(true);
-    this.setOutput(true, 'Boolean');
-    this.setColour(210);
-  },
-  onchange: function (this: Blockly.Block) {
-    const leftBlock = this.getInput('LEFT')?.connection?.targetBlock();
-    const typeCheck = leftBlock?.outputConnection?.getCheck() ?? null;
-    this.getInput('RIGHT')?.connection?.setCheck(typeCheck);
-  },
-};
+  const typeMap = new Map(options.map(([, value, type]) => [value, type]));
 
-Blockly.Blocks['logic_op'] = {
-  init: function (this: Blockly.Block) {
-    this.appendValueInput('A').setCheck('Boolean');
-    this.appendDummyInput().appendField(
-      new Blockly.FieldDropdown([
-        ['AND', 'and'],
-        ['OR', 'or'],
-      ]),
-      'OP'
-    );
-    this.appendValueInput('B').setCheck('Boolean');
-    this.setInputsInline(true);
-    this.setOutput(true, 'Boolean');
-    this.setColour(120);
-  },
-};
+  Blockly.Blocks['app_variable'] = {
+    init: function (this: Blockly.Block) {
+      this.appendDummyInput().appendField(
+        new Blockly.FieldDropdown(dropdown),
+        'VAR_NAME'
+      );
+      this.setOutput(true, options[0]?.[2] ?? null);
+      this.setColour(230);
+    },
+    onchange: function (this: Blockly.Block) {
+      const varName = this.getFieldValue('VAR_NAME');
+      const type = typeMap.get(varName) ?? null;
+      this.outputConnection?.setCheck(type);
+    },
+  };
 
-Blockly.Blocks['logic_negate'] = {
-  init: function (this: Blockly.Block) {
-    this.appendValueInput('VALUE').setCheck('Boolean').appendField('NOT');
-    this.setOutput(true, 'Boolean');
-    this.setColour(120);
-  },
-};
+  Blockly.Blocks['comparison'] = {
+    init: function (this: Blockly.Block) {
+      this.appendValueInput('LEFT');
+      this.appendDummyInput().appendField(
+        new Blockly.FieldDropdown([
+          ['<', '<'],
+          ['>', '>'],
+          ['=', '=='],
+          ['≤', '<='],
+          ['≥', '>='],
+          ['≠', '!='],
+        ]),
+        'OP'
+      );
+      this.appendValueInput('RIGHT');
+      this.setInputsInline(true);
+      this.setOutput(true, 'Boolean');
+      this.setColour(210);
+    },
+    onchange: function (this: Blockly.Block) {
+      const leftBlock = this.getInput('LEFT')?.connection?.targetBlock();
+      const typeCheck = leftBlock?.outputConnection?.getCheck() ?? null;
+      this.getInput('RIGHT')?.connection?.setCheck(typeCheck);
+    },
+  };
 
-Blockly.Blocks['number_value'] = {
-  init: function (this: Blockly.Block) {
-    this.appendDummyInput().appendField(new Blockly.FieldNumber(0), 'NUM');
-    this.setOutput(true, 'Number');
-    this.setColour(160);
-  },
-};
+  Blockly.Blocks['logic_op'] = {
+    init: function (this: Blockly.Block) {
+      this.appendValueInput('A').setCheck('Boolean');
+      this.appendDummyInput().appendField(
+        new Blockly.FieldDropdown([
+          ['AND', 'and'],
+          ['OR', 'or'],
+        ]),
+        'OP'
+      );
+      this.appendValueInput('B').setCheck('Boolean');
+      this.setInputsInline(true);
+      this.setOutput(true, 'Boolean');
+      this.setColour(120);
+    },
+  };
 
-Blockly.Blocks['boolean_value'] = {
-  init: function (this: Blockly.Block) {
-    this.appendDummyInput().appendField(
-      new Blockly.FieldDropdown([
-        ['true', 'TRUE'],
-        ['false', 'FALSE'],
-      ]),
-      'BOOL'
-    );
-    this.setOutput(true, 'Boolean');
-    this.setColour(160);
-  },
-};
+  Blockly.Blocks['logic_negate'] = {
+    init: function (this: Blockly.Block) {
+      this.appendValueInput('VALUE').setCheck('Boolean').appendField('NOT');
+      this.setOutput(true, 'Boolean');
+      this.setColour(120);
+    },
+  };
+
+  Blockly.Blocks['number_value'] = {
+    init: function (this: Blockly.Block) {
+      this.appendDummyInput().appendField(new Blockly.FieldNumber(0), 'NUM');
+      this.setOutput(true, 'Number');
+      this.setColour(160);
+    },
+  };
+
+  Blockly.Blocks['boolean_value'] = {
+    init: function (this: Blockly.Block) {
+      this.appendDummyInput().appendField(
+        new Blockly.FieldDropdown([
+          ['true', 'TRUE'],
+          ['false', 'FALSE'],
+        ]),
+        'BOOL'
+      );
+      this.setOutput(true, 'Boolean');
+      this.setColour(160);
+    },
+  };
+}
