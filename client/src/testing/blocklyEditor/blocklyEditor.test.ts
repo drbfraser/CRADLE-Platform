e513a0726dc11cd2b8d9d0f5dd99f0f7078ab7c9
Jayden Brown <jayden_brown@sfu.ca>
2026-08-19 13:20:47 -0700

    Updated tests to match updated missing variables

--- client/src/testing/blocklyEditor/blocklyEditor.test.ts
+++ client/src/testing/blocklyEditor/blocklyEditor.test.ts
@@ -336,7 +336,9 @@ describe('Blockly workspace rules', () => {
       expect(roots).toHaveLength(1);
       const root = roots[0]!;
       expect(root.type).toBe('string_comparison');
-      expect(root.getInputTargetBlock('LEFT')).toBeNull();
+      const left = root.getInputTargetBlock('LEFT');
+      expect(left?.type).toBe('app_variable_missing');
+      expect(left?.getFieldValue('VAR_NAME')).toBe('forms[latest].missing_q');
       const right = root.getInputTargetBlock('RIGHT');
       expect(right?.type).toBe('string_value');
       expect(right?.getFieldValue('TEXT')).toBe('Yes');
@@ -356,7 +358,9 @@ describe('Blockly workspace rules', () => {
       expect(roots).toHaveLength(1);
       const root = roots[0]!;
       expect(root.type).toBe('number_comparison');
-      expect(root.getInputTargetBlock('LEFT')).toBeNull();
+      const left = root.getInputTargetBlock('LEFT');
+      expect(left?.type).toBe('app_variable_missing');
+      expect(left?.getFieldValue('VAR_NAME')).toBe('forms[latest].patient_age');
       expect(root.getFieldValue('OP')).toBe('>=');
       const right = root.getInputTargetBlock('RIGHT');
       expect(right?.type).toBe('number_value');
